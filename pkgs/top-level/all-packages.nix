444738d55d309fb301c920725c2a92c569635170
Peter Hoeg <peter@hoeg.com>
2018-03-19 18:50:00 +0800

    py-wmi-client: init at unstable-20160601
    (cherry picked from commit ffe12af49a1dfa8a9b18cddb665b1b824736493d)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24671,6 +24671,8 @@ in
 
   qMasterPassword = libsForQt5.callPackage ../applications/misc/qMasterPassword { };
 
+  py-wmi-client = callPackage ../tools/networking/py-wmi-client { };
+
   redprl = callPackage ../applications/science/logic/redprl { };
 
   retroarchBare = callPackage ../misc/emulators/retroarch {
