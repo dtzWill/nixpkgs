d1999ce48418fa525c001a65fa54f2fe37d0b82c
Will Dietz <w@wdtz.org>
2018-03-26 14:29:57 -0500

    xfsprogs: 4.14.0 -> 4.15.1?

--- pkgs/tools/filesystems/xfsprogs/default.nix
+++ pkgs/tools/filesystems/xfsprogs/default.nix
@@ -1,36 +1,36 @@
-{ stdenv, fetchpatch, fetchgit, autoconf, automake, gettext, libtool, readline, utillinux }:
+{ stdenv, fetchpatch, fetchgit, autoconf, automake, gettext, libtool, readline, utillinux, libunistring, pkgconfig }:
 
 let
   gentooPatch = name: sha256: fetchpatch {
-    url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/sys-fs/xfsprogs/files/${name}?id=f4055adc94e11d182033a71e32f97b357c034aff";
+    url = "https://gitweb.gentoo.org/repo/gentoo.git/plain/sys-fs/xfsprogs/files/${name}?id=2517dd766cf84d251631f4324f7ec4bce912abb9";
     inherit sha256;
   };
 in
 
 stdenv.mkDerivation rec {
   name = "xfsprogs-${version}";
-  version = "4.14.0";
+  version = "4.15.1";
 
   src = fetchgit {
     url = "git://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git";
     rev = "refs/tags/v${version}";
-    sha256 = "19mg3avm188xz215hqbbh7251q27qwm7g1xr8ffrjwvzmdq55rxj";
+    sha256 = "0s5hisnb2qww1lljyfjvwgqld0a9g3g18bgbvdcgyn80sx5l66l4";
   };
 
   outputs = [ "bin" "dev" "out" "doc" ];
 
-  nativeBuildInputs = [ autoconf automake libtool gettext ];
+  nativeBuildInputs = [ autoconf automake libtool gettext pkgconfig ];
   propagatedBuildInputs = [ utillinux ]; # Dev headers include <uuid/uuid.h>
-  buildInputs = [ readline ];
+  buildInputs = [ readline libunistring ];
 
   enableParallelBuilding = true;
 
   # Why is all this garbage needed? Why? Why?
   patches = [
-    (gentooPatch "xfsprogs-4.12.0-sharedlibs.patch" "1i081749x91jvlrw84l4a3r081vqcvn6myqhnqbnfcfhd64h12bq")
-    (gentooPatch "xfsprogs-4.7.0-libxcmd-link.patch" "1lvy1ajzml39a631a7jqficnzsd40bzkca7hkxv1ybiqyp8sf55s")
     (gentooPatch "xfsprogs-4.9.0-underlinking.patch" "1r7l8jphspy14i43zbfnjrnyrdm4cpgyfchblascxylmans0gci7")
-    ./glibc-2.27.patch
+    (gentooPatch "xfsprogs-4.15.0-sharedlibs.patch" "0bv2naxpiw7vcsg8p1v2i47wgfda91z1xy1kfwydbp4wmb4nbyyv")
+    (gentooPatch "xfsprogs-4.15.0-docdir.patch" "1srgdidvq2ka0rmfdwpqp92fapgh53w1h7rajm4nnby5vp2v8dfr")
+    #./glibc-2.27.patch
   ];
 
   preConfigure = ''
