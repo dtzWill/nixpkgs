af47d228992c764c6274b250d93137b4646ccc0c
Will Dietz <w@wdtz.org>
2018-05-01 17:24:15 -0500

    musl: local git again, fixes?

--- pkgs/os-specific/linux/musl/default.nix
+++ pkgs/os-specific/linux/musl/default.nix
@@ -32,12 +32,12 @@ stdenv.mkDerivation rec {
   name    = "musl-${version}";
   version = "1.1.19";
 
-  #src = builtins.fetchGit ~/cur/musl;
+  src = builtins.fetchGit ~/cur/musl;
   #src = builtins.fetchGit git://git.musl-libc.org/musl;
-  src = fetchurl {
-    url    = "http://www.musl-libc.org/releases/musl-${version}.tar.gz";
-    sha256 = "1nf1wh44bhm8gdcfr75ayib29b99vpq62zmjymrq7f96h9bshnfv";
-  };
+  #src = fetchurl {
+  #  url    = "http://www.musl-libc.org/releases/musl-${version}.tar.gz";
+  #  sha256 = "1nf1wh44bhm8gdcfr75ayib29b99vpq62zmjymrq7f96h9bshnfv";
+  #};
 
   enableParallelBuilding = true;
 
