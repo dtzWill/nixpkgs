21e4495902883061432f22eb3bf51f51bc0840bd
Will Dietz <w@wdtz.org>
2018-05-01 14:17:49 -0500

    musl: local repo

--- pkgs/os-specific/linux/musl/default.nix
+++ pkgs/os-specific/linux/musl/default.nix
@@ -32,7 +32,8 @@ stdenv.mkDerivation rec {
   name    = "musl-${version}";
   version = "1.1.19-git";
 
-  src = builtins.fetchGit git://git.musl-libc.org/musl;
+  src = builtins.fetchGit ~/cur/musl;
+  #src = builtins.fetchGit git://git.musl-libc.org/musl;
   #src = fetchurl {
   #  url    = "http://www.musl-libc.org/releases/musl-${version}.tar.gz";
   #  sha256 = "1nf1wh44bhm8gdcfr75ayib29b99vpq62zmjymrq7f96h9bshnfv";
