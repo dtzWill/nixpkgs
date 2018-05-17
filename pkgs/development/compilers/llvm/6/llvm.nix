b11edd8518f38093b65e1b24873b936d1c7b6968
Will Dietz <w@wdtz.org>
2018-05-17 10:45:48 -0500

    slightly better paxmark

--- pkgs/development/compilers/llvm/6/llvm.nix
+++ pkgs/development/compilers/llvm/6/llvm.nix
@@ -170,13 +170,14 @@ in stdenv.mkDerivation (rec {
 
   postBuild = ''
     rm -fR $out
+
+    paxmark m bin/{lli,llvm-rtdyld}
+    paxmark m bin/lli-child-target
+  '' + stdenv.lib.optionalString (stdenv.lib.hostPlatform == stdenv.lib.buildPlatform) ''
+    paxmark m unittests/ExecutionEngine/MCJIT/MCJITTests
+    paxmark m unittests/ExecutionEngine/Orc/OrcJITTests
+    paxmark m unittests/Support/SupportTests
   '';
-#
-#    paxmark m bin/{lli,llvm-rtdyld}
-#    paxmark m unittests/ExecutionEngine/MCJIT/MCJITTests
-#    paxmark m unittests/ExecutionEngine/Orc/OrcJITTests
-#    paxmark m unittests/Support/SupportTests
-#    paxmark m bin/lli-child-target
 
   preCheck = ''
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib
