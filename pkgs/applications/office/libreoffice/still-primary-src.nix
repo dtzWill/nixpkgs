eaf74cf563d7e7e043334b29cd0b2cc9a3f2c62c
Will Dietz <w@wdtz.org>
2018-09-03 15:07:20 -0500

    wip

--- pkgs/applications/office/libreoffice/still-primary-src.nix
+++ pkgs/applications/office/libreoffice/still-primary-src.nix
@@ -1,9 +1,9 @@
 { fetchurl }:
 
 rec {
-  major = "5";
-  minor = "4";
-  patch = "7";
+  major = "6";
+  minor = "0";
+  patch = "6";
   tweak = "2";
 
   subdir = "${major}.${minor}.${patch}";
@@ -12,6 +12,6 @@ rec {
 
   src = fetchurl {
     url = "https://download.documentfoundation.org/libreoffice/src/${subdir}/libreoffice-${version}.tar.xz";
-    sha256 = "0s9s4nhp2whwxis54jbxrf1dwpnpl95b9781d1pdj4xk5z9v90fv";
+    sha256 = "0s9s6nhp2whwxis54jbxrf1dwpnpl95b9781d1pdj4xk5z9v90fv";
   };
 }
