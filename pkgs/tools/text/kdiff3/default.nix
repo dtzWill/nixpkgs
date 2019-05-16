e313dc575734de5af481f849cf53fdf71463ea96
Will Dietz <w@wdtz.org>
2019-05-16 09:09:08 -0500

    kdiff3: clean, assume no longer needed

--- pkgs/tools/text/kdiff3/default.nix
+++ pkgs/tools/text/kdiff3/default.nix
@@ -13,20 +13,6 @@ mkDerivation rec {
     sha256 = "083pz5c1w7l9h4sb8zz8a763yph5sk3mxnhpdykz1rrggy9f8p54";
   };
 
-  patches = [
-    # TODO: is this still relevant?
-    #(fetchpatch {
-    #  name = "git-mergetool.diff"; # see https://gitlab.com/tfischer/kdiff3/merge_requests/2
-    #  url = "https://gitlab.com/vcunat/kdiff3/commit/6106126216.patch";
-    #  sha256 = "16xqc24y8bg8gzkdbwapiwi68rzqnkpz4hgn586mi01ngig2fd7y";
-    #})
-  ];
-  #patchFlags = "-p 2";
-
-  #postPatch = ''
-  #  sed -re "s/(p\\[[^]]+] *== *)('([^']|\\\\')+')/\\1QChar(\\2)/g" -i src/diff.cpp
-  #'';
-
   nativeBuildInputs = [ extra-cmake-modules kdoctools wrapGAppsHook ];
 
   propagatedBuildInputs = [ kconfig kcrash kinit kparts ];
