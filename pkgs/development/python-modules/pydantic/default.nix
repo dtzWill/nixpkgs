a458c9c20eeec267068ce9d1cb815413cb94d623
Will Dietz <w@wdtz.org>
2019-05-16 10:33:03 -0500

    pythonPackages.pydantic: 0.23 -> 0.25

--- pkgs/development/python-modules/pydantic/default.nix
+++ pkgs/development/python-modules/pydantic/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pydantic";
-  version = "0.23";
+  version = "0.25";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "13k8mysyrqp2jsd23sjv9r4lpkzrq2lbrnxkz36f3d56x421idsq";
+    sha256 = "2203e01c1d87a3d964aa0db56efdb1b89a90eca610ab3f0ddea396e2a5fa4cc4";
   };
 
   buildInputs = [ ujson email_validator ];
