be8b2057ea1c7185298ef28f208d258521713b41
Will Dietz <w@wdtz.org>
2019-05-16 10:00:20 -0500

    quaternion-git: bump to use merged nick coloring PR instead of patches

--- pkgs/applications/networking/instant-messengers/quaternion/default.nix
+++ pkgs/applications/networking/instant-messengers/quaternion/default.nix
@@ -50,16 +50,23 @@ let
     };
   };
 
+  # This is the version from the submodule in latest quaternion
   qmtx_colors = libqmatrixclient_0_5.overrideAttrs(o: {
-    patches = (o.patches or []) ++ [
-      (fetchpatch {
-        url = https://github.com/QMatrixClient/libqmatrixclient/pull/298.patch;
-        sha256 = "05h0wvx45rlqyzz1zfj24l7cwmxrxwkv5f88fzcvqbfq1hwm0542";
-      })
-    ];
+    src = fetchFromGitHub {
+      owner = "QMatrixClient";
+      repo = "libqmatrixclient";
+      rev = "d6f39dcb0de69322479f287514a8c36afcb3fe7b";
+      sha256 = "00n5qz8ahjvcwn3ssz7vdpglwx8gfmjp7yc72hpq5pcj9dh72xka";
+    };
+    #patches = (o.patches or []) ++ [
+    #  (fetchpatch {
+    #    url = https://github.com/QMatrixClient/libqmatrixclient/pull/298.patch;
+    #    sha256 = "05h0wvx45rlqyzz1zfj24l7cwmxrxwkv5f88fzcvqbfq1hwm0542";
+    #  })
+    #];
   });
 
 in rec {
   quaternion     = generic "0.0.9.4"     "12mkwiqqbi4774kwl7gha72jyf0jf547acy6rw8ry249zl4lja54" "" qmtx_colors; # libqmatrixclient_0_5;
-  quaternion-git = quaternion;
+  quaternion-git = generic "4c999db5da4b56a2cac25fcb6a881abcfac673c2" "0ql883422ygm9i881clfn1p85br7lhzdalbz8di4zafhndwsdxr8" "" qmtx_colors;
 }
