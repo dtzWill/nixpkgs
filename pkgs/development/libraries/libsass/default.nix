4128da8171aff5c509a5ddf590748663a4d3ab63
Will Dietz <w@wdtz.org>
2019-05-18 14:47:03 -0500

    libsass: drop included patch (for CVE)

--- pkgs/development/libraries/libsass/default.nix
+++ pkgs/development/libraries/libsass/default.nix
@@ -9,14 +9,6 @@ stdenv.mkDerivation rec {
     sha256 = "0sai6wfw38nradzj64lrq6qi4ip93dzar3ia2vqav6fd1fin5fdl";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "CVE-2018-19827.patch";
-      url = "https://github.com/sass/libsass/commit/b21fb9f84096d9927780b86fa90629a096af358d.patch";
-      sha256 = "0ix12x9plmpgs3xda2fjdcykca687h16qfwqr57i5qphjr9vp33l";
-    })
-  ];
-
   preConfigure = ''
     export LIBSASS_VERSION=${version}
   '';
