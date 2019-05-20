d4d5a8f892355bfcc7fb34e1caebf90a9480342d
Will Dietz <w@wdtz.org>
2019-05-19 19:44:44 -0500

    garcon: 0.6.1 -> 0.6.2, drop patches included

--- pkgs/desktops/xfce4-13/garcon/default.nix
+++ pkgs/desktops/xfce4-13/garcon/default.nix
@@ -3,10 +3,9 @@
 mkXfceDerivation rec {
   category = "xfce";
   pname = "garcon";
-  version = "0.6.1";
+  version = "0.6.2";
 
-  sha256 = "19k8bwn29x7hkg882pxv5xxmrbfagdqgkxg166pwz2k0prab6hl8";
+  sha256 = "0gmvi6m3iww7m3xxx5wiqd8vsi18igzhcpjfzknfc8z741vc38yj";
 
-  patches = [ ./12700.patch ./13785.patch ];
   buildInputs = [ gtk2 gtk3 libxfce4ui libxfce4util ];
 }
