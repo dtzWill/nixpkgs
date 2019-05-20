10019385e0e5e418ba7ed455226a9fbc920d9317
Will Dietz <w@wdtz.org>
2019-05-19 19:12:31 -0500

    xfconf: 4.13.5 -> 4.13.7

--- pkgs/desktops/xfce4-13/xfconf/default.nix
+++ pkgs/desktops/xfce4-13/xfconf/default.nix
@@ -3,9 +3,9 @@
 mkXfceDerivation rec {
   category = "xfce";
   pname = "xfconf";
-  version = "4.13.5";
+  version = "4.13.7";
 
-  sha256 = "0xpnwb04yw5qdn0bj8b740a7rmiy316vhja5pp8p6sdiqm32yi8a";
+  sha256 = "0n9cjiz3mj011p3w4jv0n2ifz38whmykdl888mczc26l1gflxnr3";
 
   buildInputs = [ libxfce4util ];
 }
