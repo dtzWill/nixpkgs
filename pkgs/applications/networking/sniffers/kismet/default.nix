283302fc8c1269e836a5103bed75a600ada02477
Will Dietz <w@wdtz.org>
2019-05-21 08:14:39 -0500

    kismet: 2016-07-R1 -> 2019-04-R1

--- pkgs/applications/networking/sniffers/kismet/default.nix
+++ pkgs/applications/networking/sniffers/kismet/default.nix
@@ -1,23 +1,25 @@
-{ stdenv, fetchurl, pkgconfig, libpcap, ncurses, expat, pcre, libnl }:
+{ stdenv, fetchurl, pkgconfig
+, libpcap, ncurses, expat, pcre, libmicrohttpd, libnl, sqlite, zlib }:
 
 stdenv.mkDerivation rec {
   name = "kismet-${version}";
-  version = "2016-07-R1";
+  version = "2019-04-R1";
 
   src = fetchurl {
     url = "https://www.kismetwireless.net/code/${name}.tar.xz";
-    sha256 = "0dz28y4ay4lskhl0lawqy2dkcrhgfkbg06v22qxzzw8i6caizcmx";
+    sha256 = "143p19cya38kwsfsnh01f97gaiy5hviv641sb06adhmbfcs5wmv0";
   };
 
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ libpcap ncurses expat pcre libnl ];
-  postConfigure = ''
-    sed -e 's/-o $(INSTUSR)//' \
-        -e 's/-g $(INSTGRP)//' \
-        -e 's/-g $(MANGRP)//' \
-        -e 's/-g $(SUIDGROUP)//' \
-        -i Makefile
-  '';
+  buildInputs = [ libpcap ncurses expat pcre libmicrohttpd libnl sqlite zlib ];
+  configureFlags = [ "--disable-python-tools" /* TODO */ ];
+  #postConfigure = ''
+  #  sed -e 's/-o $(INSTUSR)//' \
+  #      -e 's/-g $(INSTGRP)//' \
+  #      -e 's/-g $(MANGRP)//' \
+  #      -e 's/-g $(SUIDGROUP)//' \
+  #      -i Makefile
+  #'';
 
   meta = with stdenv.lib; {
     description = "Wireless network sniffer";
