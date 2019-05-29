fe9a86549a6167e2ca58758a8a8be732fd0f8b39
Will Dietz <w@wdtz.org>
2019-05-28 20:38:37 -0500

    dedup: 2.0

--- pkgs/tools/backup/dedup/default.nix
+++ pkgs/tools/backup/dedup/default.nix
@@ -5,18 +5,18 @@
 
 stdenv.mkDerivation rec {
   pname = "dedup";
-  #version = "1.0";
-  version = "2019-05-22";
+  version = "2.0";
+#  version = "2019-05-22";
 
-  #src = fetchurl {
-  #  url = "https://dl.2f30.org/releases/${pname}-${version}.tar.gz";
-  #  sha256 = "0wd4cnzhqk8l7byp1y16slma6r3i1qglwicwmxirhwdy1m7j5ijy";
-  #};
-  src = fetchgit {
-    url =  git://git.2f30.org/dedup.git;
-    rev = "edba7a881758ccfa9cd11ba4a4b60fe83446701e";
-    sha256 = "12k7k6fnhdhhaklhhli8rsh0g1zdgd5a4y1rc3s44m76k9ln4yv0";
+  src = fetchurl {
+    url = "https://dl.2f30.org/releases/${pname}-${version}.tar.gz";
+    sha256 = "0n5kkni4d6blz3s94y0ddyhijb74lxv7msr2mvdmj8l19k0lrfh1";
   };
+  #src = fetchgit {
+  #  url =  git://git.2f30.org/dedup.git;
+  #  rev = "edba7a881758ccfa9cd11ba4a4b60fe83446701e";
+  #  sha256 = "12k7k6fnhdhhaklhhli8rsh0g1zdgd5a4y1rc3s44m76k9ln4yv0";
+  #};
 
   makeFlags = [
     "CC:=$(CC)"
