3e2c375b30a496ae1c1fb5e5d6dcefe82c323b44
Will Dietz <w@wdtz.org>
2019-05-30 09:45:42 -0500

    libstatgrab: fix w/musl and such

--- pkgs/development/libraries/libstatgrab/default.nix
+++ pkgs/development/libraries/libstatgrab/default.nix
@@ -1,15 +1,26 @@
-{ stdenv, fetchurl
+{ stdenv, fetchurl, autoreconfHook, pkgconfig
+, ncurses
 , IOKit ? null }:
 
 stdenv.mkDerivation rec {
   name = "libstatgrab-0.91";
 
   src = fetchurl {
-    url = "https://ftp.i-scream.org/pub/i-scream/libstatgrab/${name}.tar.gz";
+    url = "http://ftp.i-scream.org/pub/i-scream/libstatgrab/${name}.tar.gz";
     sha256 = "1azinx2yzs442ycwq6p15skl3mscmqj7fd5hq7fckhjp92735s83";
   };
 
-  buildInputs = [] ++ stdenv.lib.optional stdenv.isDarwin IOKit;
+  buildInputs = [ ncurses ] ++ stdenv.lib.optional stdenv.isDarwin IOKit;
+
+  # perhaps limit this to musl
+  NIX_CFLAGS_COMPILE = [ "-DLINUX" "-DHAVE_PROCFS" ];
+
+  nativeBuildInputs = [ autoreconfHook pkgconfig ];
+
+  patches = [ ./configure-musl.patch ./os_info-musl.patch ];
+  patchFlags = [ "-p0" ];
+
+  configureFlags = [ "ac_cv_header_sys_sysinfo_h=no" ];
 
   meta = with stdenv.lib; {
     homepage = https://www.i-scream.org/libstatgrab/;
