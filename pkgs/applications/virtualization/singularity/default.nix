48c9c8dc1b702a7ad4578f5c3ae03d19ffbb3201
Will Dietz <w@wdtz.org>
2019-06-01 01:17:55 -0500

    singularity: 3.0.1 -> 3.2.1
    https://github.com/sylabs/singularity/releases/tag/v3.2.1

--- pkgs/applications/virtualization/singularity/default.nix
+++ pkgs/applications/virtualization/singularity/default.nix
@@ -17,13 +17,13 @@ with lib;
 
 buildGoPackage rec {
   name = "singularity-${version}";
-  version = "3.0.1";
+  version = "3.2.1";
 
   src = fetchFromGitHub {
     owner = "sylabs";
     repo = "singularity";
     rev = "v${version}";
-    sha256 = "1wpsd0il2ipa2n5cnbj8dzs095jycdryq2rx62kikbq7ahzz4fsi";
+    sha256 = "14lhxwy21s7q081x7kbnvkjsbxgsg2f181qlzmlxcn6n7gfav3kj";
   };
 
   goPackagePath = "github.com/sylabs/singularity";
