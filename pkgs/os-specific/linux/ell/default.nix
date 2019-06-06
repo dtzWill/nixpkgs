d3824ba91570175bcfd30938c03dbec1bb12d1ee
Will Dietz <w@wdtz.org>
2019-06-06 16:08:23 -0500

    ell: git bump

--- pkgs/os-specific/linux/ell/default.nix
+++ pkgs/os-specific/linux/ell/default.nix
@@ -3,13 +3,13 @@
 stdenv.mkDerivation rec {
   pname = "ell";
   #version = "0.20";
-  version = "2019-06-04";
+  version = "2019-06-06";
 
   src = fetchgit {
      url = https://git.kernel.org/pub/scm/libs/ell/ell.git;
      #rev = version;
-     rev = "2652cba364aa2e18c39a23ba00fa5ca88f120730";
-     sha256 = "1sq1b5d4zbk3ggii96bh80a3xgznj99y9j0c0p5pljs10savbg3v";
+     rev = "7c4e38f83bb90ebb5b0cffb7328d176928c39c64";
+     sha256 = "1119d0jmhn03y51aq9lplhhb2hh92balf9wkfvnrjk8hj9vsfxfa";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
