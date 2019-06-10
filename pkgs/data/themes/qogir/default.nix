08431478b7967351f643b705b701f56c7d1f7862
Will Dietz <w@wdtz.org>
2019-06-09 21:14:25 -0500

    qogir-theme: git bump

--- pkgs/data/themes/qogir/default.nix
+++ pkgs/data/themes/qogir/default.nix
@@ -8,8 +8,8 @@ stdenv.mkDerivation rec {
     owner = "vinceliuice";
     repo = pname;
     #rev = version;
-    rev = "a0188de8cc703ae72df4b29dead5f646a131aaa0";
-    sha256 = "1sac1k6r3ax18dwn2d5j55mpv1f8s9gnw5rr0jh1waax8swjaq1p";
+    rev = "50703180932e603eede09122090a4551d5237918";
+    sha256 = "10z5alja7k6b5gbyg82h7pp6h6jbxmicnww4p8w0pk1gw03k540r";
   };
 
   buildInputs = [ gdk_pixbuf librsvg ];
