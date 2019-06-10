bad70e5963643cdc258ad0756a5c6bbbd628b228
Will Dietz <w@wdtz.org>
2019-06-09 21:10:19 -0500

    ipfs: buildGoModule, drop deps.nix

--- pkgs/applications/networking/ipfs/default.nix
+++ pkgs/applications/networking/ipfs/default.nix
@@ -1,13 +1,11 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   name = "ipfs-${version}";
   version = "0.4.21";
   rev = "v${version}";
 
   goPackagePath = "github.com/ipfs/go-ipfs";
-
-  goDeps = ./deps.nix;
 
   src = fetchFromGitHub {
     owner = "ipfs";
@@ -15,6 +13,8 @@ buildGoPackage rec {
     inherit rev;
     sha256 = "0jlj89vjy4nw3x3j45r16y8bph5ss5lp907pjgqvad0naxbf99b0";
   };
+
+  modSha256 = "0d9rq0hig9jwv9jfajfyj2111arikqzdnyhf5aqkwahcblpx54iy";
 
   meta = with stdenv.lib; {
     description = "A global, versioned, peer-to-peer filesystem";
