611711da504e16347907da8593ef4f0125d5b7bd
Will Dietz <w@wdtz.org>
2019-06-10 20:49:44 -0500

    requests-async: loosen ver req on httpcore, fingers crossed

--- pkgs/development/python-modules/requests-async/default.nix
+++ pkgs/development/python-modules/requests-async/default.nix
@@ -9,5 +9,11 @@ buildPythonPackage rec {
     sha256 = "0vyy9mwdg6c4i22s3awhv8x11rcazhmhi5pxybn9cc9qa4244cc7";
   };
 
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace "httpcore==0.3.*" \
+                "httpcore==0.*"
+  '';
+
   propagatedBuildInputs = [ requests httpcore ];
 }
