66042c289b2b80c03735918abf269d92181c1024
Will Dietz <w@wdtz.org>
2019-06-11 17:48:39 -0500

    scite: 4.0.5 -> 4.1.6, touchup a bit

--- pkgs/applications/editors/scite/default.nix
+++ pkgs/applications/editors/scite/default.nix
@@ -1,26 +1,23 @@
 { stdenv, fetchurl, pkgconfig, gtk2 }:
 
 stdenv.mkDerivation rec {
-  name = "scite-${version}";
-  version = "4.0.5";
+  pname = "scite";
+  version = "4.1.6";
 
   src = fetchurl {
-    url = https://www.scintilla.org/scite405.tgz;
-    sha256 = "0h16wk2986nkkhhdv5g4lxlcn02qwyja24x1r6vf02r1hf46b9q2";
+    url = "https://www.scintilla.org/scite${builtins.replaceStrings ["."][""] version}.tgz";
+    sha256 = "0k10qjil101mxlmd39sdz1n9yaif0x5q6vl03is48jrh4lvvlfyy";
   };
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ gtk2 ];
   sourceRoot = "scintilla/gtk";
 
+
+  makeFlags = [ "prefix=${placeholder "out"}" ];
   buildPhase = ''
     make
     cd ../../scite/gtk
-    make prefix=$out/
-  '';
-
-  installPhase = ''
-    make install prefix=$out/
   '';
 
   meta = with stdenv.lib; {
