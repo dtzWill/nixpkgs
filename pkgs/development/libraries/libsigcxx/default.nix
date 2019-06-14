0ec70b569f3b371a96367e0eb1e118366f6e7144
Will Dietz <w@wdtz.org>
2019-06-14 17:29:21 -0500

    libsigcxx: 2.10.1 -> 2.10.2
    https://github.com/libsigcplusplus/libsigcplusplus/releases/tag/2.10.2

--- pkgs/development/libraries/libsigcxx/default.nix
+++ pkgs/development/libraries/libsigcxx/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libsigc++";
-  version = "2.10.1";
+  version = "2.10.2";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "00v08km4wwzbh6vjxb21388wb9dm6g2xh14rgwabnv4c2wk5z8n9";
+    sha256 = "163s14d1rqp82gc1vsm3q0wzsbdicb9q6307kz0zk5lm6x9h5jmi";
   };
 
   nativeBuildInputs = [ pkgconfig gnum4 ];
