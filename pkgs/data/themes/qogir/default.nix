68eeac119e9fae69bc2271110f8da28033c4ea10
Will Dietz <w@wdtz.org>
2019-06-17 17:36:57 -0500

    qogir-theme: 2019-05-07 -> 2019-06-17

--- pkgs/data/themes/qogir/default.nix
+++ pkgs/data/themes/qogir/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "qogir-theme";
-  version = "2019-05-07";
+  version = "2019-06-17";
 
   src = fetchFromGitHub {
     owner = "vinceliuice";
     repo = pname;
     #rev = version;
-    rev = "50703180932e603eede09122090a4551d5237918";
-    sha256 = "10z5alja7k6b5gbyg82h7pp6h6jbxmicnww4p8w0pk1gw03k540r";
+    rev = "4dc272d120491709a49c01e0e858b462eda9344d";
+    sha256 = "10z5alja8k6b5gbyg82h7pp6h6jbxmicnww4p9w0pk1gw03k540r";
   };
 
   buildInputs = [ gdk_pixbuf librsvg ];
