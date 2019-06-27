13de1d240f01d2ee16ce67dcf2029537475216a6
Will Dietz <w@wdtz.org>
2019-06-27 17:49:13 -0500

    minizinc: touchups in prep for update, NFCI

--- pkgs/development/tools/minizinc/default.nix
+++ pkgs/development/tools/minizinc/default.nix
@@ -1,16 +1,18 @@
 { stdenv, fetchFromGitHub, cmake, flex, bison }:
-let
-  version = "2.2.3";
-in
-stdenv.mkDerivation {
-  name = "minizinc-${version}";
 
+stdenv.mkDerivation rec {
+  pname = "minizinc";
+  version = "2.2.3";
+
+  nativeBuildInputs = [ cmake flex bison ];
   buildInputs = [ cmake flex bison ];
 
   src = fetchFromGitHub {
     owner = "MiniZinc";
     repo = "libminizinc";
-    rev = "3d66971a0cad6edbe796f4dd940229d38e5bfe3d"; # tags on the repo are disappearing: See https://github.com/MiniZinc/libminizinc/issues/257
+    # tags on the repo are disappearing: See https://github.com/MiniZinc/libminizinc/issues/257
+    # optimistically, let's see if it happens again:
+    rev = "refs/tags/${version}";
     sha256 = "1q31y9131aj2lsm34srm8i1s0271qcaaknzvym3r8awynm14saq5";
   };
 
