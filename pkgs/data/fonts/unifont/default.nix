4d2d39c453f11715765f810636f6e425fd08980b
Will Dietz <w@wdtz.org>
2019-06-27 11:50:33 -0500

    unifont: 12.1.01 -> 12.1.02
    https://lists.gnu.org/archive/html/info-gnu/2019-06/msg00000.html

--- pkgs/data/fonts/unifont/default.nix
+++ pkgs/data/fonts/unifont/default.nix
@@ -2,16 +2,16 @@
 
 stdenv.mkDerivation rec {
   name = "unifont-${version}";
-  version = "12.1.01";
+  version = "12.1.02";
 
   ttf = fetchurl {
     url = "mirror://gnu/unifont/${name}/${name}.ttf";
-    sha256 = "05knv3vlnk8ahaybwz6r95d3a1h7h7q9ll6ij2jl7zgjhcx4zy5d";
+    sha256 = "0jd5kh5pvax8zlhy63mjc31k7chdfgdj8nbmp0gy00lx1da62jfs";
   };
 
   pcf = fetchurl {
     url = "mirror://gnu/unifont/${name}/${name}.pcf.gz";
-    sha256 = "0q7dlnfzk49m4pgf2s7jv05jysa6sfxx3w0y17yis9j7g18lyw1b";
+    sha256 = "1p9vgf74qc6qdr8g9w983jgh4b96z1p6y78sxi48sql1mw3izjkg";
   };
 
   nativeBuildInputs = [ mkfontscale mkfontdir ];
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
 
   outputHashAlgo = "sha256";
   outputHashMode = "recursive";
-  outputHash = "0sgdr9dma4hkda3siydfvjrnzrpri8r7iqs2zqf77z9n4zn90qp5";
+  outputHash = "0sgdr9dma5hkda3siydfvjrnzrpri8r7iqs2zqf77z9n4zn90qp5";
 
   meta = with stdenv.lib; {
     description = "Unicode font for Base Multilingual Plane";
