12fc3089b9164d41041e2e1246ef552c1adc23f0
Will Dietz <w@wdtz.org>
2019-06-27 18:29:25 -0500

    wtf: 0.11.0 -> 0.12.0

--- pkgs/applications/misc/wtf/default.nix
+++ pkgs/applications/misc/wtf/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "wtf";
-  version = "0.11.0";
+  version = "0.12.0";
 
   src = fetchFromGitHub {
     owner = "wtfutil";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1b671jhf3xaaisgpiad5apmvwkp40qr2hm4n21m0ya7k5ckps09z";
+    sha256 = "0m0hrn1iqikp2pc16264ibsdw3nra4ba8r711mn1r2n15241x03v";
   };
 
   modSha256 = "0as736nnx7ci4w9gdp27g55g6dny9bh1fryz3g89gxm2sa2nlb9l";
