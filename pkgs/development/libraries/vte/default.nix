cec9a91de01fb51ec7fde2115add001284302c62
Will Dietz <w@wdtz.org>
2019-07-02 20:08:46 -0500

    Revert "vte: 0.57.3 (dev, odd)"
    only care about vte-ng these days anyway, and via ALLVM
    
    This reverts commit 7f46cccd2a0a15957b69ede2bd550052e724ed1c.

--- pkgs/development/libraries/vte/default.nix
+++ pkgs/development/libraries/vte/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   pname = "vte";
-  version = "0.57.3";
+  version = "0.56.3";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0dqfb086smwdj7d8mas9sn9px4c7lfm3iz176qajpc087rh974qn";
+    sha256 = "0j166gic5znssdb9r45qazq4kb4v9fial82czand5wa8i2yd988p";
   };
 
   passthru = {
