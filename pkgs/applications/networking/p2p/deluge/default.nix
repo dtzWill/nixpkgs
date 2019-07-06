380042ed2d486fa516a3ed5eddd947b610a5be76
Will Dietz <w@wdtz.org>
2019-07-05 19:43:54 -0500

    deluge: 1.3.15 -> 2.0.3
    * let's try 2.0 version now
    * bz2 -> xz
    * maybe python3
    * temp disable pyGtkGlade for deps

--- pkgs/applications/networking/p2p/deluge/default.nix
+++ pkgs/applications/networking/p2p/deluge/default.nix
@@ -1,24 +1,16 @@
 { stdenv, fetchurl, fetchpatch, intltool, libtorrentRasterbar, pythonPackages }:
 
 pythonPackages.buildPythonPackage rec {
-  name = "deluge-${version}";
-  version = "1.3.15";
+  pname = "deluge";
+  version = "2.0.3";
 
   src = fetchurl {
-    url = "http://download.deluge-torrent.org/source/${name}.tar.bz2";
-    sha256 = "1467b9hmgw59gf398mhbf40ggaka948yz3afh6022v753c9j7y6w";
+    url = "http://download.deluge-torrent.org/source/2.0/${pname}-${version}.tar.xz";
+    sha256 = "14d8kn2pvr1qv8mwqrxmj85jycr73vwfqz12hzag0ararbkfhyky";
   };
 
-  patches = [
-    # Fix preferences when built against libtorrent >=0.16
-    (fetchpatch {
-      url = "https://git.deluge-torrent.org/deluge/patch/?id=38d7b7cdfde3c50d6263602ffb03af92fcbfa52e";
-      sha256 = "0la3i0lkj6yv4725h4kbd07mhfwcb34w7prjl9gxg12q7px6c31d";
-    })
-  ];
-
   propagatedBuildInputs = with pythonPackages; [
-    pyGtkGlade twisted Mako chardet pyxdg pyopenssl service-identity
+   /* pyGtkGlade*/ twisted Mako chardet pyxdg pyopenssl service-identity
     libtorrentRasterbar.dev libtorrentRasterbar.python
   ];
 
