918e826b194cdace9ac025a2eec540b68f66aff7
Will Dietz <w@wdtz.org>
2019-07-09 14:26:59 -0500

    libmicrohttpd: 0.9.64 -> 0.9.65

--- pkgs/development/libraries/libmicrohttpd/default.nix
+++ pkgs/development/libraries/libmicrohttpd/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, libgcrypt, curl, gnutls, pkgconfig, libiconv, libintl }:
 
 stdenv.mkDerivation rec {
-  name = "libmicrohttpd-${version}";
-  version = "0.9.64";
+  pname = "libmicrohttpd";
+  version = "0.9.65";
 
   src = fetchurl {
-    url = "mirror://gnu/libmicrohttpd/${name}.tar.gz";
-    sha256 = "03imzkd1hl2mkkpi84vg5xq9x6b58gwsv86ym85km0lhb7nxi4p7";
+    url = "mirror://gnu/${pname}/${pname}-${version}.tar.gz";
+    sha256 = "1jdk6wigvnkh5bi9if4rik8i9sbvdql61lm8ipgpypyxqmcpjipj";
   };
 
   outputs = [ "out" "dev" "devdoc" "info" ];
