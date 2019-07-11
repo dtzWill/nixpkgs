23a633a1797ba459355d9d2416c40714187de694
Will Dietz <w@wdtz.org>
2019-07-10 19:35:55 -0500

    Revert "ffmpeg_4: 4.1.3 -> 4.1.4"
    This reverts commit eeec0e18b306b8ebcd2fa154ad674a3024fd2f26.

--- pkgs/development/libraries/ffmpeg/4.nix
+++ pkgs/development/libraries/ffmpeg/4.nix
@@ -6,7 +6,7 @@
 
 callPackage ./generic.nix (args // rec {
   version = "${branch}";
-  branch = "4.1.4";
-  sha256 = "1qd7a10gs12ifcp31gramcgqjl77swskjfp7cijibgyg5yl4kw7i";
+  branch = "4.1.3";
+  sha256 = "0gdnprc7gk4b7ckq8wbxbrj7i00r76r9a5g9mj7iln40512j0c0c";
   darwinFrameworks = [ Cocoa CoreMedia VideoToolbox ];
 })
