3b3e6b9d886964c250f84ac6f1f7467e2c6eb644
Mario Rodas <marsam@users.noreply.github.com>
2019-07-15 21:49:38 -0500

    wtf: 0.11.0 -> 0.17.1
    (cherry picked from commit 50ba9f4385f25875a1e2904fdc0610680e7792ac)

--- pkgs/applications/misc/wtf/default.nix
+++ pkgs/applications/misc/wtf/default.nix
@@ -1,26 +1,24 @@
-{ buildGoModule
+{ buildGoPackage
 , fetchFromGitHub
 , lib
 }:
 
-buildGoModule rec {
+buildGoPackage rec {
   pname = "wtf";
-  version = "0.13.0";
+  version = "0.17.1";
 
   src = fetchFromGitHub {
     owner = "wtfutil";
     repo = pname;
     rev = "v${version}";
-    sha256 = "13izb0v0b30v0xv3yg082n0zsk4v57x6vx3m7ln4wril762ab784";
+    sha256 = "1qiwl6z5rraspjqry8dwnx8fgl9vv70sn5kgvh8074vl651yjq8c";
   };
 
-  modSha256 = "0921d2qy0jcnih7hlda0i5q45b350yp9frh3hhh91f1g0j7vn1y6";
-
-  buildFlagsArray = [ "-ldflags=" "-X main.version=${version}" ];
+  goPackagePath = "github.com/wtfutil/wtf";
 
   meta = with lib; {
     description = "The personal information dashboard for your terminal";
-    homepage = http://wtfutil.com/;
+    homepage = "https://wtfutil.com/";
     license = licenses.mpl20;
     maintainers = with maintainers; [ kalbasit ];
     platforms = platforms.linux ++ platforms.darwin;
