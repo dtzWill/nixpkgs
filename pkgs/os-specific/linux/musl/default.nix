91c3608bc23e03c043baa7c77837aa7419cc80f5
Will Dietz <w@wdtz.org>
2019-07-16 18:49:37 -0500

    musl: 1.1.22 -> 1.1.23
    https://git.musl-libc.org/cgit/musl/commit/?id=b07d45eb01e900f0176894fdedab62285f5cb8be

--- pkgs/os-specific/linux/musl/default.nix
+++ pkgs/os-specific/linux/musl/default.nix
@@ -29,11 +29,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "musl";
-  version = "1.1.22";
+  version = "1.1.23";
 
   src = fetchurl {
     url    = "https://www.musl-libc.org/releases/${pname}-${version}.tar.gz";
-    sha256 = "1qr9xqdzziy5bsyyqlh6k8yz056ll55d5yvc0gbhz61ginj422cb";
+    sha256 = "1ahlpmwnpbysdrpkbhdvjk03bcwvigk19h42wgfrfv7jrr0yn3wa";
   };
 
   enableParallelBuilding = true;
