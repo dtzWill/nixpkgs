fdda11567da3d7e5a4cc17c39dd3de09e37b482e
Frederik Rietdijk <fridh@fridh.nl>
2019-07-17 20:36:47 +0200

    Split buildPythonPackage into setup hooks
    This commit splits the `buildPythonPackage` into multiple setup hooks.
    
    Generally, Python packages are built from source to wheels using `setuptools`.
    The wheels are then installed with `pip`. Tests were often called with
    `python setup.py test` but this is less common nowadays. Most projects
    now use a different entry point for running tests, typically `pytest`
    or `nosetests`.
    
    Since the wheel format was introduced more tools were built to generate these,
    e.g. `flit`. Since PEP 517 is provisionally accepted, defining a build-system
    independent format (`pyproject.toml`), `pip` can now use that format to
    execute the correct build-system.
    
    In the past I've added support for PEP 517 (`pyproject`) to the Python
    builder, resulting in a now rather large builder. Furthermore, it was not possible
    to reuse components elsewhere. Therefore, the builder is now split into multiple
    setup hooks.
    
    The `setuptoolsCheckHook` is included now by default but in time it should
    be removed from `buildPythonPackage` to make it easier to use another hook
    (curently one has to pass in `dontUseSetuptoolsCheck`).
    
    (cherry picked from commit f7e28bf5d8181926e600a222cb70180519d09726)

--- pkgs/development/python-modules/pip/default.nix
+++ pkgs/development/python-modules/pip/default.nix
@@ -1,11 +1,15 @@
 { lib
+, python
 , buildPythonPackage
+, bootstrapped-pip
 , fetchPypi
 , mock
 , scripttest
 , virtualenv
 , pretend
 , pytest
+, setuptools
+, wheel
 }:
 
 buildPythonPackage rec {
@@ -18,9 +22,11 @@ buildPythonPackage rec {
     sha256 = "e7a31f147974362e6c82d84b91c7f2bdf57e4d3163d3d454e6c3e71944d67135";
   };
 
+  nativeBuildInputs = [ bootstrapped-pip ];
+
   # pip detects that we already have bootstrapped_pip "installed", so we need
   # to force it a little.
-  installFlags = [ "--ignore-installed" ];
+  pipInstallFlags = [ "--ignore-installed" ];
 
   checkInputs = [ mock scripttest virtualenv pretend pytest ];
   # Pip wants pytest, but tests are not distributed
--- pkgs/development/python-modules/pytest/default.nix
+++ pkgs/development/python-modules/pytest/default.nix
@@ -1,8 +1,9 @@
 { stdenv, buildPythonPackage, pythonOlder, fetchPypi, attrs, hypothesis, py
 , setuptools_scm, setuptools, six, pluggy, funcsigs, isPy3k, more-itertools
-, atomicwrites, mock, writeText, pathlib2, wcwidth, packaging, isPyPy
+, atomicwrites, mock, writeText, pathlib2, wcwidth, packaging, isPyPy, python
 }:
 buildPythonPackage rec {
+  # TODO: sync w/nixpkgs master for pytest 5+
   version = "4.6.5";
   pname = "pytest";
 
@@ -17,12 +18,14 @@ buildPythonPackage rec {
   };
 
   checkInputs = [ hypothesis mock ];
-  buildInputs = [ setuptools_scm ];
+  nativeBuildInputs = [ setuptools_scm ];
   propagatedBuildInputs = [ attrs py setuptools six pluggy more-itertools atomicwrites wcwidth packaging ]
     ++ stdenv.lib.optionals (!isPy3k) [ funcsigs ]
     ++ stdenv.lib.optionals (pythonOlder "3.6") [ pathlib2 ];
 
   doCheck = !isPyPy; # https://github.com/pytest-dev/pytest/issues/3460
+
+  # Ignored file https://github.com/pytest-dev/pytest/pull/5605#issuecomment-522243929
   checkPhase = ''
     runHook preCheck
     $out/bin/py.test -x testing/ -k "not test_collect_pyargs_with_testpaths"
@@ -34,15 +37,17 @@ buildPythonPackage rec {
     pytestcachePhase() {
         find $out -name .pytest_cache -type d -exec rm -rf {} +
     }
-
     preDistPhases+=" pytestcachePhase"
   '';
+
+  pythonImportsCheck = [
+    "pytest"
+  ];
 
   meta = with stdenv.lib; {
     homepage = https://docs.pytest.org;
     description = "Framework for writing tests";
     maintainers = with maintainers; [ domenkozar lovek323 madjar lsix ];
     license = licenses.mit;
-    platforms = platforms.unix;
   };
 }
