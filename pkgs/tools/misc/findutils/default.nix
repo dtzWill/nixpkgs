6ac345dd3205f6c9e09129a09289ede1bf39f2c5
Will Dietz <w@wdtz.org>
2019-07-16 21:38:47 -0500

    findutils: patches to fix w/latest musl

--- pkgs/tools/misc/findutils/default.nix
+++ pkgs/tools/misc/findutils/default.nix
@@ -17,6 +17,12 @@ stdenv.mkDerivation rec {
     # Prevent tests from failing on old kernels (2.6x)
     # getdtablesize reports incorrect values if getrlimit() fails
     ./disable-getdtablesize-test.patch
+
+    # gnulib patches from void, fix w/musl but generally applicible */
+    ./gnulib-freadahead.patch
+    ./gnulib-fseeko.patch
+    ./gnulib-mountlist.patch
+    ./gnulib-stdio-impl.patch
   ];
 
   buildInputs = [ coreutils ]; # bin/updatedb script needs to call sort
