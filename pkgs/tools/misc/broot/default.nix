0c2ad5e021d48d886ad2fb0e7aaa93fbb5fe6309
Will Dietz <w@wdtz.org>
2019-07-19 18:01:36 -0500

    boot: 0.8.5 -> 0.9.0, wip

--- pkgs/tools/misc/broot/default.nix
+++ pkgs/tools/misc/broot/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "broot";
-  version = "0.8.5";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "Canop";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0sq7p9qx2d2h047l5q9kwl5g5qi43d2xzn3mm7jdidbrh2ay1s3g";
+    sha256 = "0i6ayp295xnppq92lc1fsfyrjkxrkvsva07yby45qa0l92nihqpy";
   };
 
-  cargoSha256 = "0fsdkdckn3f8hz0xp33r2sx81n7h4qjbjg5dbxnzwk6xv75fbkfx";
+  cargoSha256 = "0fsdkdckn4f8hz0xp33r2sx81n7h4qjbjg5dbxnzwk6xv75fbkfx";
 
   meta = with stdenv.lib; {
     description = "An interactive tree view, a fuzzy search, a balanced BFS descent and customizable commands";
