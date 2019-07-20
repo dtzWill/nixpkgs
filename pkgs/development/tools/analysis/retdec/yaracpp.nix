597f50c962fd52cf5c9c2211c6b27d42c3f90c01
Will Dietz <w@wdtz.org>
2019-07-19 23:32:53 -0500

    retdec: dep fixups, let's see

--- pkgs/development/tools/analysis/retdec/yaracpp.nix
+++ pkgs/development/tools/analysis/retdec/yaracpp.nix
@@ -15,7 +15,7 @@ in stdenv.mkDerivation rec {
   # does not build anything
   name = "yaracpp-src-${version}";
   version = "2018-10-09";
-  rev = "b92bde0e59e3b75bc445227e04b71105771dee8b"; # as specified in retdec/deps/yaracpp/CMakeLists.txt
+  rev = "1ba9a78e0a46260a77c5a34a141ecaab66298192"; # as specified in retdec/deps/yaracpp/CMakeLists.txt
 
   src = fetchFromGitHub {
     inherit rev;
