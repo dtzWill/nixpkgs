ccb032bfbf924b575a2c865316d04d7906c72116
Will Dietz <w@wdtz.org>
2019-07-23 20:29:48 -0500

    Revert "exiv2: 0.27.2-RC3, fix"
    This reverts commit 016a43796a18f532d11886946ab5202eabfe9c2b.

--- pkgs/development/libraries/exiv2/default.nix
+++ pkgs/development/libraries/exiv2/default.nix
@@ -14,14 +14,19 @@
 
 stdenv.mkDerivation rec {
   pname = "exiv2";
-  version = "0.27.2-RC3";
+  version = "0.27.1";
 
   src = fetchFromGitHub rec {
-    owner = pname;
-    repo  = pname;
-    rev = "v${version}";
-    sha256 = "0l82zrnx48knjmybxm1y9i792gmaafdp5k458886gipgc11v6hfz";
+    owner = "exiv2";
+    repo  = "exiv2";
+    rev = version;
+    sha256 = "0b5m921070fkyif0zlyb49gly3p6xd0hv1jyym4j25hx12gzbx0c";
   };
+
+  patches = [
+    # https://github.com/Exiv2/exiv2/commit/aae88060ca85a483cd7fe791ba116c04d96c0bf9#comments
+    ./fix-cmake.patch
+  ];
 
   cmakeFlags = [
     "-DEXIV2_BUILD_PO=ON"
