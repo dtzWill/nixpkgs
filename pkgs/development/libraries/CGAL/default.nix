3f600eab6f1267e43157ff3215871eec1d7623bd
Will Dietz <w@wdtz.org>
2019-08-01 18:53:05 -0500

    Revert "cgal: try downgrading to 4.13.1, see if fixes graph-tool?" (nope)
    This reverts commit d4796df9f487cc07da139faf35ab233f22bff4be.

--- pkgs/development/libraries/CGAL/default.nix
+++ pkgs/development/libraries/CGAL/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchFromGitHub, cmake, boost, gmp, mpfr }:
 
 stdenv.mkDerivation rec {
-  pname = "cgal";
-  version = "4.13.1";
+  version = "4.14";
+  name = "cgal-" + version;
 
   src = fetchFromGitHub {
     owner = "CGAL";
     repo = "releases";
     rev = "CGAL-${version}";
-    sha256 = "0qvlv0xgh45kwxc677jfnjvwr8vc3wkdbk2fv7ybmz7piwahszff";
+    sha256 = "0p0s1dl5a261zwy0hxa7ylkypk45rwc6n84lx507dwdhfz4ihv12";
   };
 
   # note: optional component libCGAL_ImageIO would need zlib and opengl;
