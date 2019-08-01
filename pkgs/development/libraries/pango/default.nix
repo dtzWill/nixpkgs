9110e5ea0b18389b548eb99fc446d1e4bb207210
Will Dietz <w@wdtz.org>
2019-07-31 22:28:10 -0500

    pango: find fix

--- pkgs/development/libraries/pango/default.nix
+++ pkgs/development/libraries/pango/default.nix
@@ -58,8 +58,8 @@ in stdenv.mkDerivation rec {
   };
 
   # Requires.private workaround
-  postInstall = ''
-    find $dev -type f -name "*.pc" -exec sed -i -e 's/^Requires.private/Requires' \;
+  postFixup = ''
+    find $dev -type f -name "*.pc" -exec sed -i -e 's/^Requires.private/Requires/' '{}' \;
   '';
 
   meta = with stdenv.lib; {
