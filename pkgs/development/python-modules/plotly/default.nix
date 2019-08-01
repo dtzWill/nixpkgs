59bdf72356d4396aa1e5dbb13ac930c7316406fc
Will Dietz <w@wdtz.org>
2019-08-01 15:14:11 -0500

    plotly: add most of the optional requirements too

--- pkgs/development/python-modules/plotly/default.nix
+++ pkgs/development/python-modules/plotly/default.nix
@@ -7,15 +7,38 @@
 , requests
 , retrying
 , six
+# Test inputs
+, numpy
+, matplotlib
+, coverage
+, mock
+, nose
+, pytest
+, backports_tempfile
+, xarray
+, scipy
+# and quite a few others for various features I don't know are needed "yet".
+# https://github.com/plotly/plotly.py/blob/master/packages/python/plotly/optional-requirements.txt
+# Adding a few I'm interested in
+, pandas
+, psutil
+, inflect
+, colorcet
+, ipython
+# (TODO: jupyter bits, skipping for now)
+, pyshp
+, geopandas
+, shapely
+, pillow
 }:
 
 buildPythonPackage rec {
   pname = "plotly";
-  version = "3.10.0";
+  version = "4.0.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "164aav7i3ann1lv3xbb76ylpph4hissl0wsnmil1s3m0r7sk7jsx";
+    sha256 = "0iw0j2jwlbzknpbdpaqrjjlbycbwqhavp1crblvihf03knn7nkxz";
   };
 
   propagatedBuildInputs = [
@@ -25,14 +48,40 @@ buildPythonPackage rec {
     requests
     retrying
     six
+
+    # Extras/optional
+    pandas
+    psutil
+    inflect
+    colorcet
+    ipython
+    # (TODO: jupyter bits, skipping for now)
+    pyshp
+    geopandas
+    shapely
+    pillow
+  ];
+
+  checkInputs = [
+    numpy
+    matplotlib
+    coverage
+    mock
+    nose
+    pytest
+    backports_tempfile
+    xarray
+    pytz
+    scipy
   ];
 
   # No tests in archive
-  doCheck = false;
+  #doCheck = false;
 
-  meta = {
+  meta = with lib; {
     description = "Python plotting library for collaborative, interactive, publication-quality graphs";
     homepage = https://plot.ly/python/;
-    license = with lib.licenses; [ mit ];
+    license = licenses.mit;
+    maintainers = with maintainers; [ dtzWill ];
   };
 }
