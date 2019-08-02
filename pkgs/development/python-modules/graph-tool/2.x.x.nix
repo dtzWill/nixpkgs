27ed34cf741d6c005a8a15949461b4007b1e2e59
Will Dietz <w@wdtz.org>
2019-08-01 19:58:25 -0500

    graph-tool: gcc7 should work, maybe patch is applied too fuzzily?

--- pkgs/development/python-modules/graph-tool/2.x.x.nix
+++ pkgs/development/python-modules/graph-tool/2.x.x.nix
@@ -2,7 +2,6 @@
 , pkgconfig, boost, expat, scipy, cgal, gmp, mpfr
 , gobject-introspection, pygobject3, gtk3, matplotlib, ncurses
 , buildPythonPackage
-, fetchpatch
 , pythonAtLeast
 , lib
 }:
@@ -23,15 +22,6 @@ buildPythonPackage rec {
     url = "https://downloads.skewed.de/graph-tool/graph-tool-${version}.tar.bz2";
     sha256 = "0ykzcnqc5bhqb4xlf9ahpp807vj5868xdrmcj6fggqnnpqv4633c";
   };
-
-  patches = [
-    # fix build with cgal 4.13 (https://git.skewed.de/count0/graph-tool/issues/509)
-    (fetchpatch {
-      name = "cgal-4.13.patch";
-      url = "https://git.skewed.de/count0/graph-tool/commit/aa39e4a6b42d43fac30c841d176c75aff92cc01a.patch";
-      sha256 = "1578inb4jqwq2fhhwscn5z95nzmaxvmvk30nzs5wirr26iznap4m";
-    })
-  ];
 
   configureFlags = [
     "--with-python-module-path=$(out)/${python.sitePackages}"
