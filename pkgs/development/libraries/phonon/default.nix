6b6554bdd6839b8451324c213cd087a1588ef3d3
R. RyanTM <ryantm-bot@ryantm.com>
2019-08-04 23:30:44 -0700

    libsForQt5.phonon: 4.10.2 -> 4.10.3
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/phonon-qt5/versions
    
    (cherry picked from commit 403d64a705300180517241691266bcb68beb8748)

--- pkgs/development/libraries/phonon/default.nix
+++ pkgs/development/libraries/phonon/default.nix
@@ -8,7 +8,7 @@ with lib;
 assert withQt5; # XXX: DTZ: I want to know if/when/how my configs try qt4 instead
 
 let
-  v = "4.10.2";
+  v = "4.10.3";
 
   soname = if withQt5 then "phonon4qt5" else "phonon";
   buildsystemdir = "share/cmake/${soname}";
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://kde/stable/phonon/${v}/phonon-${v}.tar.xz";
-    sha256 = "02c8fyyvg5qb0lxwxmnxc5grkg6p3halakjf02vmwmvqaycb3v9l";
+    sha256 = "15f2vndpqfcivifzl1s07r0wkavpfrjln1p46cwfk85gd5b192rf";
   };
 
   buildInputs =
