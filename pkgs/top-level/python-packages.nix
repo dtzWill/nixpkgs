b2dcd3f7c2e650f375b319462ae4ce77bbf6d46f
Jonathan Ringer <jonringer117@gmail.com>
2019-08-05 18:45:12 -0700

    pythonPackages.delegator-py: init at 0.1.1
    (cherry picked from commit fcd2858063ae98eb266a73f921130aa976c35554)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1433,6 +1433,8 @@ in {
     gui = false;
   };
 
+  delegator-py = callPackage ../development/python-modules/delegator-py { };
+
   deluge-client = callPackage ../development/python-modules/deluge-client { };
 
   arrow = callPackage ../development/python-modules/arrow { };
