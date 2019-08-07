5235856072d6332f187fbc0bf85c355f65dd4770
Will Dietz <w@wdtz.org>
2019-08-06 21:12:17 -0500

    vit2: git bump

--- pkgs/applications/misc/vit/python.nix
+++ pkgs/applications/misc/vit/python.nix
@@ -4,14 +4,14 @@ with python3Packages;
 
 buildPythonApplication rec {
   pname = "vit";
-  version = "2.0.0b1";
+  version = "2.0.0b1.1";
 
   src = fetchFromGitHub {
     owner = "scottkosty";
     repo = pname;
     #rev = "v${version}";
-    rev = "db1a4a08900728647db44b2b6a3cc40196c85b98";
-    sha256 = "12i35fa7fymz74ljnw1ypns2l61p95d3a7lrplvb0fmmi341kcp9";
+    rev = "9f274bb24cc74911ae9f5c75d756e0a98ae97f52";
+    sha256 = "0ybai7r7hwvjy8lrwmv1j9r2gh93lz6amsj6v1akvhiayv5aiqpp";
   };
 
   propagatedBuildInputs = [
