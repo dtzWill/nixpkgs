1efb77c6e8b8571a6db7abf47aee289526f7e47f
Will Dietz <w@wdtz.org>
2019-08-09 13:34:54 -0500

    nodejs-12_x: 12.5.0 -> 12.8.0
    https://nodejs.org/en/blog/release/v12.8.0/
    https://nodejs.org/en/blog/release/v12.7.0/
    https://nodejs.org/en/blog/release/v12.6.0/

--- pkgs/development/web/nodejs/v12.nix
+++ pkgs/development/web/nodejs/v12.nix
@@ -5,6 +5,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "12.5.0";
-    sha256 = "08haqs104lw44l92bxfii18sdn7y1k07cz3p0ni9bhw7kh4vf5c7";
+    version = "12.8.0";
+    sha256 = "1914i1hm59adbv8jzn46cvf9pmcvs1gclkm95f4rkdgafqimaywr";
   }
