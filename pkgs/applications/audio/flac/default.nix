2ad362e41b91018c9100d2a415c407a7b0c8a310
Will Dietz <w@wdtz.org>
2019-08-13 11:14:54 -0500

    flac: pname-ify

--- pkgs/applications/audio/flac/default.nix
+++ pkgs/applications/audio/flac/default.nix
@@ -1,10 +1,11 @@
 { stdenv, fetchurl, libogg }:
 
 stdenv.mkDerivation rec {
-  name = "flac-1.3.2";
+  pname = "flac";
+  version = "1.3.2";
 
   src = fetchurl {
-    url = "http://downloads.xiph.org/releases/flac/${name}.tar.xz";
+    url = "http://downloads.xiph.org/releases/${pname}/${pname}-${version}.tar.xz";
     sha256 = "0gymm2j3276kr9nz6vmgfwsdfrq6c449n40a0mzz8h6wc7nw7kwi";
   };
 
