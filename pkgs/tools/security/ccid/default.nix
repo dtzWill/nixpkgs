d6b319dcfdb777d0836cc44f872306bbccb85737
Will Dietz <w@wdtz.org>
2019-08-13 17:42:44 -0500

    ccid: 1.4.30 -> 1.4.31

--- pkgs/tools/security/ccid/default.nix
+++ pkgs/tools/security/ccid/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, pcsclite, pkgconfig, libusb1, perl }:
 
 stdenv.mkDerivation rec {
-  version = "1.4.30";
-  name = "ccid-${version}";
+  version = "1.4.31";
+  pname = "ccid";
 
   src = fetchurl {
-    url = "https://ccid.apdu.fr/files/${name}.tar.bz2";
-    sha256 = "0z7zafdg75fr1adlv2x0zz34s07gljcjg2lsz76s1048w1xhh5xc";
+    url = "https://ccid.apdu.fr/files/${pname}-${version}.tar.bz2";
+    sha256 = "1xz8ikr6vk73w3xnwb931yq8lqc1zrj8c3v34n6h63irwjvdfj3b";
   };
 
   postPatch = ''
@@ -14,9 +14,7 @@ stdenv.mkDerivation rec {
     substituteInPlace src/Makefile.in --replace /bin/echo echo
   '';
 
-  preConfigure = ''
-    configureFlagsArray+=("--enable-usbdropdir=$out/pcsc/drivers")
-  '';
+  configureFlags = [ "--enable-usbdropdir=${placeholder "out"}/pcsc/drivers" ];
 
   nativeBuildInputs = [ pkgconfig perl ];
   buildInputs = [ pcsclite libusb1 ];
