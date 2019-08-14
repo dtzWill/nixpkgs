88f8b7dedd03e72433fe5ca578da83f702e45cd6
Will Dietz <w@wdtz.org>
2019-08-14 13:54:22 -0500

    Revert "wayland-protocols: 1.17 -> 1.18" (rebuild later)
    This reverts commit 9edc40cb26adac967cb5361045670092ae7e6ae8.

--- pkgs/development/libraries/wayland/protocols.nix
+++ pkgs/development/libraries/wayland/protocols.nix
@@ -4,11 +4,11 @@
 
 stdenv.mkDerivation rec {
   name = "wayland-protocols-${version}";
-  version = "1.18";
+  version = "1.17";
 
   src = fetchurl {
     url = "https://wayland.freedesktop.org/releases/${name}.tar.xz";
-    sha256 = "1cvl93h83ymbfhb567jv5gzyq08181w7c46rsw4xqqqpcvkvfwrx";
+    sha256 = "0bw1sqixqk2a7mqw630cs4dlgcp5yib90vyikzm3lr05jz7ij4yz";
   };
 
   nativeBuildInputs = [ pkgconfig ];
