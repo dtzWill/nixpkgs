35a786e1ed71362ac9dc752b86ed7530d1b80d66
Will Dietz <w@wdtz.org>
2019-08-15 15:53:54 -0500

    taskell: 1.5.1 -> 1.6.0
    https://github.com/smallhadroncollider/taskell/releases/tag/1.6.0

--- pkgs/applications/misc/taskell/default.nix
+++ pkgs/applications/misc/taskell/default.nix
@@ -1,8 +1,8 @@
 { lib, haskellPackages, fetchFromGitHub }:
 
 let
-  version = "1.5.1";
-  sha256  = "1z17f1nzn65nixdhrxhcjnhrv2wrr79hl742jyxkfxl8mf4dyf0i";
+  version = "1.6.0";
+  sha256  = "1yq7lbqg759i3hyxcskx3924b7xmw6i4ny6n8yq80k4hikw2k6mf";
 
 in (haskellPackages.mkDerivation {
   pname = "taskell";
