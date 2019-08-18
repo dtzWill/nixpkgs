e01ba294b15bc03eddd4b488d7bd56ed80a110fa
Frederik Rietdijk <fridh@fridh.nl>
2019-08-18 10:54:46 +0200

    pythonPackages.bleach: disable a test
    (cherry picked from commit 57ef737629c3b88606345f51d5b87ef34158123a)

--- pkgs/development/python-modules/bleach/default.nix
+++ pkgs/development/python-modules/bleach/default.nix
@@ -24,6 +24,12 @@ buildPythonPackage rec {
     substituteInPlace setup.py --replace ",<3dev" ""
   '';
 
+  # Disable a test
+  # https://github.com/mozilla/bleach/issues/467
+  checkPhase = ''
+    pytest -k "not test_only_text_is_cleaned"
+  '';
+
   meta = {
     description = "An easy, HTML5, whitelisting HTML sanitizer";
     longDescription = ''
