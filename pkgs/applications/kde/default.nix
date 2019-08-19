83e88d880a178f662023681d674806110fe86356
Peter Hoeg <peter@hoeg.com>
2019-08-19 22:43:10 +0800

    kolf: init at 19.08.0
    (cherry picked from commit d3fd2c4128dcd7dba46ab4c9115537896d826b52)

--- pkgs/applications/kde/default.nix
+++ pkgs/applications/kde/default.nix
@@ -132,6 +132,7 @@ let
       kmix = callPackage ./kmix.nix {};
       kmplot = callPackage ./kmplot.nix {};
       knotes = callPackage ./knotes.nix {};
+      kolf = callPackage ./kolf.nix {};
       kolourpaint = callPackage ./kolourpaint.nix {};
       kompare = callPackage ./kompare.nix {};
       konsole = callPackage ./konsole.nix {};
