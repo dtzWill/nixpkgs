81829983dc09836229393e63b934ccb4fcadf6d7
Will Dietz <w@wdtz.org>
2019-08-19 16:43:36 -0500

    nextcloud: 16.0.3 -> 16.0.4

--- pkgs/servers/nextcloud/default.nix
+++ pkgs/servers/nextcloud/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchzip }:
 
 let
-  version = "16.0.3";
+  version = "16.0.4";
 in fetchzip {
   name = "nextcloud-${version}";
 
   url = "https://download.nextcloud.com/server/releases/nextcloud-${version}.tar.bz2";
-  sha256 = "1j3zzl2rlxkxx5f349ksw2acw42cbf2c3vpxsq03k1nwi99y6269";
+  sha256 = "02k6bhwhx6zd156j0gxrfr0xg6pgzdq0z9la1rb8mpzj69yg19gf";
 
   meta = {
     description = "Sharing solution for files, calendars, contacts and more";
