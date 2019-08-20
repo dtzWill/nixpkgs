b94e6e7fe494e9f41ba1586ee1e09091e1ca253c
Will Dietz <w@wdtz.org>
2019-08-20 00:00:57 -0500

    blueman: 2.0.8 -> 2.1.1, 2.1 is big, wip

--- pkgs/tools/bluetooth/blueman/default.nix
+++ pkgs/tools/bluetooth/blueman/default.nix
@@ -1,19 +1,19 @@
 { config, stdenv, lib, fetchurl, intltool, pkgconfig, python3Packages, bluez, gtk3
-, obex_data_server, xdg_utils, libnotify, dnsmasq, dhcp
+, obex_data_server, xdg_utils, dnsmasq, dhcp, iproute
 , hicolor-icon-theme, librsvg, wrapGAppsHook, gobject-introspection
 , withPulseAudio ? config.pulseaudio or stdenv.isLinux, libpulseaudio }:
 
 let
   pythonPackages = python3Packages;
-  binPath = lib.makeBinPath [ xdg_utils dnsmasq dhcp ];
+  binPath = lib.makeBinPath [ xdg_utils dnsmasq dhcp iproute ];
 
 in stdenv.mkDerivation rec {
-  name = "blueman-${version}";
-  version = "2.0.8";
+  pname = "blueman";
+  version = "2.1.1";
 
   src = fetchurl {
-    url = "https://github.com/blueman-project/blueman/releases/download/${version}/${name}.tar.xz";
-    sha256 = "0kkh6jppqcn3yf70vnny1l015kxrz3dxw4g774gl02lh9ixx1bq4";
+    url = "https://github.com/blueman-project/${pname}/releases/download/${version}/${pname}-${version}.tar.xz";
+    sha256 = "1hyvc5x97j8b4kvwzh58zzlc454d0h0hk440zbg8f5as9qrv5spi";
   };
 
   nativeBuildInputs = [
@@ -21,7 +21,7 @@ in stdenv.mkDerivation rec {
     pythonPackages.wrapPython wrapGAppsHook
   ];
 
-  buildInputs = [ bluez gtk3 pythonPackages.python libnotify librsvg hicolor-icon-theme ]
+  buildInputs = [ bluez gtk3 pythonPackages.python librsvg hicolor-icon-theme iproute ]
                 ++ pythonPath
                 ++ lib.optional withPulseAudio libpulseaudio;
 
@@ -29,11 +29,15 @@ in stdenv.mkDerivation rec {
     sed -i 's,CDLL(",CDLL("${libpulseaudio.out}/lib/,g' blueman/main/PulseAudioUtils.py
   '';
 
-  pythonPath = with pythonPackages; [ dbus-python pygobject3 pycairo ];
+  pythonPath = with pythonPackages; [ pygobject3 pycairo ];
 
   propagatedUserEnvPkgs = [ obex_data_server ];
 
-  configureFlags = [ (lib.enableFeature withPulseAudio "pulseaudio") ];
+  configureFlags = [
+    (lib.enableFeature withPulseAudio "pulseaudio")
+    "--with-systemdsystemunitdir=${placeholder "out"}/etc/systemd/system"
+    "--with-systemduserunitdir=${placeholder "out"}/etc/systemd/user"
+  ];
 
   postFixup = ''
     makeWrapperArgs="--prefix PATH ':' ${binPath}"
