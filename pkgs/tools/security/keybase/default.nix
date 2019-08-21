f27cee34b34a392ce52ed9ddf61e7afed3af96ed
Will Dietz <w@wdtz.org>
2019-08-21 17:57:53 -0500

    keybase: 4.3.1 -> 4.3.2

--- pkgs/tools/security/keybase/default.nix
+++ pkgs/tools/security/keybase/default.nix
@@ -10,7 +10,7 @@
 
 buildGoPackage rec {
   pname = "keybase";
-  version = "4.3.1";
+  version = "4.3.2";
 
   goPackagePath = "github.com/keybase/client";
   subPackages = [
@@ -33,7 +33,7 @@ buildGoPackage rec {
     owner = "keybase";
     repo = "client";
     rev = "v${version}";
-    sha256 = "1743d7a7ix882yxz9pk230vdvdj46sbscqv4wqyhb0la2pl9jqdp";
+    sha256 = "1g1ay3iaghj3xfk4h9hr9vkialf5fdbakld6m9527bn9snjz5dzd";
   };
 
   nativeBuildInputs = [ makeWrapper ]; # TODO: patch paths instead?
