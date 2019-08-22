4c5e00a2751eef9a3e447b29ebba167fdf2f2ef5
Will Dietz <w@wdtz.org>
2019-08-22 18:38:38 -0500

    fortune: cleanup bits put in $out/local, per reviewer feedback thanks!

--- pkgs/tools/misc/fortune/default.nix
+++ pkgs/tools/misc/fortune/default.nix
@@ -13,10 +13,26 @@ stdenv.mkDerivation rec {
 
   buildInputs = [ recode ];
 
-  postInstall = ''
-    mv -v $out/games/fortune $out/bin/fortune
-    rmdir -v $out/games
-  '';
+  cmakeFlags = [
+    "-DLOCALDIR=${placeholder "out"}/share/fortunes"
+  ];
+
+  patches = [ (builtins.toFile "not-a-game.patch" ''
+    diff --git a/CMakeLists.txt b/CMakeLists.txt
+    index 865e855..5a59370 100644
+    --- a/CMakeLists.txt
+    +++ b/CMakeLists.txt
+    @@ -154,7 +154,7 @@ ENDMACRO()
+     my_exe(
+         "fortune"
+         "fortune/fortune.c"
+    -    "games"
+    +    "bin"
+     )
+
+     my_exe(
+    -- 
+  '') ];
 
   meta = with stdenv.lib; {
     description = "A program that displays a pseudorandom message from a database of quotations";
