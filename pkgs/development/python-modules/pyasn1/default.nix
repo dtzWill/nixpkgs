c2ea3fe36d9722c56e6558bc3f77d53a748495c6
Will Dietz <w@wdtz.org>
2019-08-23 21:30:34 -0500

    Revert "Revert "pythonPackages.pyasn1: 0.4.5 -> 0.4.6""
    This reverts commit 1db4b2c253eb7b45fdc7ed088b30f63bac9bd879.

--- pkgs/development/python-modules/pyasn1/default.nix
+++ pkgs/development/python-modules/pyasn1/default.nix
@@ -2,11 +2,11 @@
 
 buildPythonPackage rec {
   pname = "pyasn1";
-  version = "0.4.5";
+  version = "0.4.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "da2420fe13a9452d8ae97a0e478adde1dee153b11ba832a95b223a2ba01c10f7";
+    sha256 = "11mwdsvrbwvjmny40cxa76h81bbc8jfr1prvw6hw7yvg374xawxp";
   };
 
   meta = with stdenv.lib; {
