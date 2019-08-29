81362837e3761c72de8d27d42bd5c4f2edc9bfa6
tobias pflug <tobias.pflug@gmail.com>
2019-08-29 11:52:31 +0200

    hydra-cli: init at 0.2.0
    (cherry picked from commit 11ec70c0ff305520ec2ba83bd852e138f4cf4ccf)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11453,6 +11453,8 @@ in
 
   hydra = callPackage ../development/tools/misc/hydra { };
 
+  hydra-cli = callPackage ../development/tools/misc/hydra-cli { };
+
   hydraAntLogger = callPackage ../development/libraries/java/hydra-ant-logger { };
 
   hyena = callPackage ../development/libraries/hyena { };
