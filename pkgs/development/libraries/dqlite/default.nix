eac9e9ee0d24cd1249bdafec3f4213e997111974
Will Dietz <w@wdtz.org>
2019-08-29 22:39:17 -0500

    dqlite: 0.2.6 -> 1.0.0

--- pkgs/development/libraries/dqlite/default.nix
+++ pkgs/development/libraries/dqlite/default.nix
@@ -4,13 +4,13 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   pname = "dqlite";
-  version = "0.2.6";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "CanonicalLtd";
     repo = pname;
     rev = "v${version}";
-    sha256 = "13l7na5858v2ah1vim6lafmzajgkymfi5rd6bk14cm4vcnxc40wb";
+    sha256 = "0670c1c84lcf5vl3h6mlff00fz2fnm766bzlk526sjjzysx3zjya";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig ];
