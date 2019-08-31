9b43d1f4faab4af40d38b7ed4ca6294c83a97753
Danielle Lancashire <dani@builds.terrible.systems>
2019-08-31 10:09:32 +0200

    gotestsum: Init at 0.3.5
    (cherry picked from commit 1c9253ce2b512bbe625a6fafbae58228498f42fb)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16727,6 +16727,8 @@ in
 
   gotests = callPackage ../development/tools/gotests { };
 
+  gotestsum = callPackage ../development/tools/gotestsum { };
+
   impl = callPackage ../development/tools/impl { };
 
   quicktemplate = callPackage ../development/tools/quicktemplate { };
