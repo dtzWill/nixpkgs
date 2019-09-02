b71e914cfab8a5e5f2b4e7c4e691b5b864345684
Will Dietz <w@wdtz.org>
2019-09-02 18:23:15 -0500

    v4l-utils: 1.16.6 -> 1.16.7

--- pkgs/os-specific/linux/v4l-utils/default.nix
+++ pkgs/os-specific/linux/v4l-utils/default.nix
@@ -7,12 +7,12 @@
 # See libv4l in all-packages.nix for the libs only (overrides alsa, libX11 & QT)
 
 mkDerivation rec {
-  name = "v4l-utils-${version}";
-  version = "1.16.6";
+  pname = "v4l-utils";
+  version = "1.16.7";
 
   src = fetchurl {
-    url = "https://linuxtv.org/downloads/v4l-utils/${name}.tar.bz2";
-    sha256 = "1bkqlrizx0j2rd6ybam2x17bjrpwzl4v4szmnzm3cmixis3w3npr";
+    url = "https://linuxtv.org/downloads/${pname}/${pname}-${version}.tar.bz2";
+    sha256 = "1ng0x3wj3a1ckfd00yxa4za43xms92gdp7rdag060b7p39z7m4gf";
   };
 
   outputs = [ "out" "dev" ];
