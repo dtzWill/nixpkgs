101ef667ed2f5e33100627be0d5cfa25775feeb2
Maximilian Bosch <maximilian@mbosch.me>
2019-09-05 17:43:10 +0200

    weechatScripts.weechat-xmpp: remove
    This plugin is fairly outdated and depends on python2 libraries that
    don't receive any updates either (xmpppy for instance[1]).
    
    [1] https://pypi.org/project/xmpppy/
    
    (cherry picked from commit e4bc0e2b5f6bf96dfd3c69799918ddf7f67155cf)

--- pkgs/applications/networking/irc/weechat/scripts/default.nix
+++ pkgs/applications/networking/irc/weechat/scripts/default.nix
@@ -1,10 +1,6 @@
 { callPackage, luaPackages, python3Packages }:
 
 {
-  weechat-xmpp = callPackage ./weechat-xmpp {
-    inherit (pythonPackages) pydns;
-  };
-
   weechat-matrix-bridge = callPackage ./weechat-matrix-bridge {
     inherit (luaPackages) cjson luaffi;
   };
