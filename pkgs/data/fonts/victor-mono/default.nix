79659bbb9ecae4abd6ee3ae76c42e73b7007c365
Will Dietz <w@wdtz.org>
2019-09-04 20:25:42 -0500

    victor-mono: 1.2.5 -> 1.2.6, temporarily no opentype variant

--- pkgs/data/fonts/victor-mono/default.nix
+++ pkgs/data/fonts/victor-mono/default.nix
@@ -2,7 +2,7 @@
 
 let
   pname = "victor-mono";
-  version = "1.2.5";
+  version = "1.2.6";
 in fetchFromGitHub rec {
   name = "${pname}-${version}";
 
@@ -12,21 +12,22 @@ in fetchFromGitHub rec {
 
   # Upstream prefers we download from the website,
   # but we really insist on a more versioned resource.
-  # Happily, tagged releases on github contain the same
-  # file `VictorMonoAll.zip` as from the website,
-  # so we extract it from the tagged release.
-  # Both methods produce the same file, but this way
-  # we can safely reason about what version it is.
+  # The github source usually contains a copy of this
+  # in `public/VictorMonoAll.zip` but 1.2.6 didn't update this.
+  # Looks like the ttf files were regenerated, use those for now.
+  # (following no longer applies, but hopefully does soon)
+  ## # Happily, tagged releases on github contain the same
+  ## # file `VictorMonoAll.zip` as from the website,
+  ## # so we extract it from the tagged release.
+  ## # Both methods produce the same file, but this way
+  ## # we can safely reason about what version it is.
   postFetch = ''
-    tar xvf $downloadedFile --strip-components=2 ${name}/public/VictorMonoAll.zip
-
     mkdir -p $out/share/fonts/{true,open}type/${pname}
 
-    unzip -j VictorMonoAll.zip \*.ttf -d $out/share/fonts/truetype/${pname}
-    unzip -j VictorMonoAll.zip \*.otf -d $out/share/fonts/opentype/${pname}
+    tar xvf $downloadedFile --strip-components=2 --wildcards --no-wildcards-match-slash \*/src/assets/\*.ttf -C $out/share/fonts/truetype/${pname}
   '';
 
-  sha256 = "0dj5h45qk6abggj6mgm19sb0a7q0v4x41f2zds1ab79yd22gbjns";
+  sha256 = "044m7sd4rbl3dsim2013v8rz2s2nn31snlb7g2pgkgcgsnjymxjh";
 
   meta = with lib; {
     description = "Free programming font with cursive italics and ligatures";
