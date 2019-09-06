a5d290105bf5ec80b8d66428748df9b970b85ec6
worldofpeace <worldofpeace@protonmail.ch>
2019-09-06 12:08:23 -0400

    Merge pull request #67585 from worldofpeace/system-config-printer
    nixos/system-config-printer: init
    
    (cherry picked from commit 5d4890b58dcbe0a099cda233a259284a6e5ec9d4)

--- pkgs/tools/misc/system-config-printer/default.nix
+++ pkgs/tools/misc/system-config-printer/default.nix
@@ -54,6 +54,16 @@ stdenv.mkDerivation rec {
       find $out/share/system-config-printer -name \*.py -type f -perm -0100 -print0 | while read -d "" f; do
         patchPythonScript "$f"
       done
+
+      # The below line will be unneeded when the next upstream release arrives.
+      sed -i -e "s|/usr/local/bin|$out/bin|" "$out/share/dbus-1/services/org.fedoraproject.Config.Printing.service"
+
+      # Manually expand literal "$(out)", which have failed to expand
+      sed -e "s|ExecStart=\$(out)|ExecStart=$out|" \
+          -i "$out/etc/systemd/system/configure-printer@.service"
+
+      substituteInPlace $out/etc/udev/rules.d/70-printers.rules \
+        --replace "udev-configure-printer" "$out/etc/udev/udev-configure-printer"
     '';
 
   meta = {
