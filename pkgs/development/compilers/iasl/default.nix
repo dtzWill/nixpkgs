886e23330bd06acb94aefb5f24ac8ad3c64ae61a
Frederik Rietdijk <fridh@fridh.nl>
2019-09-06 23:25:18 +0200

    Merge remote-tracking branch 'upstream/gcc8' into staging-next
    (cherry picked from commit e68505e7355b0a76caac43783f59fae6474073dc)

--- pkgs/development/compilers/iasl/default.nix
+++ pkgs/development/compilers/iasl/default.nix
@@ -1,4 +1,4 @@
-{stdenv, fetchurl, bison, flex}:
+{stdenv, fetchurl, fetchpatch, bison, flex}:
 
 stdenv.mkDerivation rec {
   pname = "iasl";
@@ -16,6 +16,13 @@ stdenv.mkDerivation rec {
     "PREFIX=${placeholder "out"}"
     "DESTDIR="
   ];
+
+
+  patches = fetchpatch {
+    /* https://github.com/acpica/acpica/pull/389 */
+    url = "https://github.com/acpica/acpica/commit/935ca65f7806a3ef9bd02a947e50f3a1f586ac67.patch";
+    sha256 = "0jz4bakifphm425shbd1j99hldgy71m7scl8mwibm441d56l3ydf";
+  };
 
   meta = {
     description = "Intel ACPI Compiler";
