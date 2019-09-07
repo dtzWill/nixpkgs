50e8ca4b424697f6a37f7c50ebf97789f6e92516
Will Dietz <w@wdtz.org>
2019-09-07 12:51:07 -0500

    gnomecast: git

--- pkgs/applications/video/gnomecast/default.nix
+++ pkgs/applications/video/gnomecast/default.nix
@@ -1,14 +1,20 @@
-{ lib, python3Packages, gtk3, gobject-introspection, ffmpeg, which, wrapGAppsHook, gsettings-desktop-schemas, librsvg }:
+{ lib, fetchFromGitHub, python3Packages, gtk3, gobject-introspection, ffmpeg, which, wrapGAppsHook, gsettings-desktop-schemas, librsvg }:
 
 with python3Packages;
 buildPythonApplication rec {
   pname = "gnomecast";
-  version = "1.7.1";
+  version = "1.7.1-git";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0hss7m9chqjhdzbvxs8kr312izb5diz85nfv7c050gpslk5mkr0v";
+  src = fetchFromGitHub {
+    owner = "keredson";
+    repo = pname;
+    rev = "3ff24dbea65197c405391ffe6604c19084fb8e46";
+    sha256 = "0zmnpr98g4vjd3r13780glwfff02qhp8r4zd7w6cdjbq9hyk0mkw";
   };
+  #src = fetchPypi {
+  #  inherit pname version;
+  #  sha256 = "0hss7m9chqjhdzbvxs8kr312izb5diz85nfv7c050gpslk5mkr0v";
+  #};
 
   nativeBuildInputs = [ wrapGAppsHook gsettings-desktop-schemas librsvg ];
   propagatedBuildInputs = [
