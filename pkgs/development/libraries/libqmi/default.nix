66a7c82ef09871958fefb6b3b4cdacf46c31178c
Will Dietz <w@wdtz.org>
2019-09-07 13:06:16 -0500

    libqmi: git rev to fix w/new glib

--- pkgs/development/libraries/libqmi/default.nix
+++ pkgs/development/libraries/libqmi/default.nix
@@ -1,13 +1,20 @@
-{ stdenv, fetchurl, pkgconfig, glib, python3, libgudev, libmbim }:
+{ stdenv, fetchurl, fetchFromGitHub, pkgconfig, glib, python3, libgudev, libmbim }:
 
 stdenv.mkDerivation rec {
   pname = "libqmi";
-  version = "1.22.4";
+  #version = "1.22.4";
+  version = "1.22.4-git-2019-09-03";
 
-  src = fetchurl {
-    url = "https://www.freedesktop.org/software/libqmi/${pname}-${version}.tar.xz";
-    sha256 = "1wgrrb9vb3myl8xgck8ik86876ycbg8crylybs3ssi21vrxqwnsc";
+  src = fetchFromGitHub {
+    owner = "freedesktop";
+    repo = pname;
+    rev = "ef6f0038ddbbe9e5d8378625579013291b55e554";
+    sha256 = "0dzlawzfw8jb0jd15vrvls32d01a5bipjpn8f0rb0h5lars5gw6q";
   };
+  #src = fetchurl {
+  #  url = "https://www.freedesktop.org/software/libqmi/${pname}-${version}.tar.xz";
+  #  sha256 = "1wgrrb9vb3myl8xgck8ik86876ycbg8crylybs3ssi21vrxqwnsc";
+  #};
 
   outputs = [ "out" "dev" "devdoc" ];
 
