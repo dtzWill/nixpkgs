16e72db985520a1b2a32dd94ae2b78bbeed34458
Will Dietz <w@wdtz.org>
2019-09-07 13:18:00 -0500

    nodejs-12_x: 12.9.0 -> 12.10.0
    https://github.com/nodejs/node/blob/master/doc/changelogs/CHANGELOG_V12.md#12.10.0

--- pkgs/development/web/nodejs/v12.nix
+++ pkgs/development/web/nodejs/v12.nix
@@ -5,6 +5,6 @@ let
 in
   buildNodejs {
     inherit enableNpm;
-    version = "12.9.0";
-    sha256 = "1ia178ln79r0w592gnl06rxmdv1dgaxnx52xvcffan6z3fxfhpz0";
+    version = "12.10.0";
+    sha256 = "1k9hxqs23c3sxpr843ix8nidlca8wn30x0sq998j47wjc1ybh595";
   }
