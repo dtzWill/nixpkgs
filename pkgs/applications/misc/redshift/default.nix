977e75e20a9787f3e9b3fcbd15ce35379640d378
Riley Inman <rileyminman@gmail.com>
2019-09-07 13:45:28 -0400

    redshift-wlr: Init at 2019-04-17
    redshift-wlr is a fork of redshift that adds support for the
    wlr-gamma-control protocol. This supports sway and any other Wayland
    compositors that implement the standard in the future.
    
    (cherry picked from commit 3dfb476ddcdb04e56eb2892607120bf7a714ca53)

--- pkgs/applications/misc/redshift/default.nix
+++ pkgs/applications/misc/redshift/default.nix
@@ -11,103 +11,129 @@
 , withGeoclue ? withGeolocation && stdenv.isLinux, geoclue
 }:
 
-stdenv.mkDerivation rec {
-  pname = "redshift";
-  version = "1.12";
+let
+  mkRedshift =
+    { pname, version, src, meta }:
+    stdenv.mkDerivation rec {
+      inherit pname version src meta;
 
-  src = fetchFromGitHub {
-    owner = "jonls";
-    repo = "redshift";
-    rev = "v${version}";
-    sha256 = "12cb4gaqkybp4bkkns8pam378izr2mwhr2iy04wkprs2v92j7bz6";
+      patches = [
+        # https://github.com/jonls/redshift/pull/575
+        ./575.patch
+
+        (fetchpatch {
+          name = "docs-fix-config-file-path.patch";
+          url = "https://github.com/jonls/redshift/commit/001d4aeffadec94368cde5738b957740d2e36980.patch";
+          sha256 = "0dr09y1521jamh2p1r5vw23l9hv7yvnb396dghvdbp9glbappqls";
+        })
+        (fetchpatch {
+          name = "prefer-symbolic-icon-if-avail.patch";
+          url = "https://github.com/jonls/redshift/commit/dca8334646d8fefeb56332eb1535c595a6599c7d.patch";
+          sha256 = "1m0mv8imbikxybl79vm0d3yagr384j8sjv53ixj9wp37wf2a5sii";
+        })
+        (fetchpatch {
+          name = "allow-hash-comment-char.patch";
+          url = "https://github.com/jonls/redshift/commit/f7fbbb04c335cd844b97c80128145349854128f4.patch";
+          sha256 = "0h1zgdahg3h91gcv6w7migf1ja6k2p7r6l6bh870xqi8h71cpspq";
+        })
+        (fetchpatch {
+          name = "additional-suspend-durations.patch";
+          url = "https://github.com/jonls/redshift/commit/2e91da281e1161af594e07eed62b672f9b006a39.patch";
+          sha256 = "0dhf75l1x67g8nbwm70gsm5923diwvjlwhh5zyffn5ylbml81cg9";
+        })
+      ];
+
+      nativeBuildInputs = [
+        autoconf
+        automake
+        gettext
+        intltool
+        libtool
+        pkgconfig
+        wrapGAppsHook
+        wrapPython
+      ];
+
+      configureFlags = [
+        "--enable-randr=${if withRandr then "yes" else "no"}"
+        "--enable-geoclue2=${if withGeoclue then "yes" else "no"}"
+        "--enable-drm=${if withDrm then "yes" else "no"}"
+        "--enable-quartz=${if withQuartz then "yes" else "no"}"
+        "--enable-corelocation=${if withCoreLocation then "yes" else "no"}"
+      ];
+
+      buildInputs = [
+        gobject-introspection
+        gtk3
+        python
+      ] ++ stdenv.lib.optional  withRandr        libxcb
+        ++ stdenv.lib.optional  withGeoclue      geoclue
+        ++ stdenv.lib.optional  withDrm          libdrm
+        ++ stdenv.lib.optional  withQuartz       ApplicationServices
+        ++ stdenv.lib.optionals withCoreLocation [ CoreLocation Foundation Cocoa ]
+        ;
+
+      pythonPath = [ pygobject3 pyxdg ];
+
+      preConfigure = "./bootstrap";
+
+      postFixup = "wrapPythonPrograms";
+
+      # the geoclue agent may inspect these paths and expect them to be
+      # valid without having the correct $PATH set
+      postInstall = ''
+        substituteInPlace $out/share/applications/redshift.desktop \
+          --replace 'Exec=redshift' "Exec=$out/bin/redshift"
+        substituteInPlace $out/share/applications/redshift.desktop \
+          --replace 'Exec=redshift-gtk' "Exec=$out/bin/redshift-gtk"
+      '';
+
+      enableParallelBuilding = true;
+    };
+in
+rec {
+  redshift = mkRedshift rec {
+    pname = "redshift";
+    version = "1.12";
+
+    src = fetchFromGitHub {
+      owner = "jonls";
+      repo = "redshift";
+      rev = "v${version}";
+      sha256 = "12cb4gaqkybp4bkkns8pam378izr2mwhr2iy04wkprs2v92j7bz6";
+    };
+
+    meta = with stdenv.lib; {
+      description = "Screen color temperature manager";
+      longDescription = ''
+        Redshift adjusts the color temperature according to the position
+        of the sun. A different color temperature is set during night and
+        daytime. During twilight and early morning, the color temperature
+        transitions smoothly from night to daytime temperature to allow
+        your eyes to slowly adapt. At night the color temperature should
+        be set to match the lamps in your room.
+      '';
+      license = licenses.gpl3Plus;
+      homepage = http://jonls.dk/redshift;
+      platforms = platforms.unix;
+      maintainers = with maintainers; [ yegortimoshenko globin ];
+    };
   };
 
-  patches = [
-    # https://github.com/jonls/redshift/pull/575
-    ./575.patch
+  redshift-wlr = mkRedshift {
+    pname = "redshift-wlr";
+    version = "2019-04-17";
 
-    (fetchpatch {
-      name = "docs-fix-config-file-path.patch";
-      url = "https://github.com/jonls/redshift/commit/001d4aeffadec94368cde5738b957740d2e36980.patch";
-      sha256 = "0dr09y1521jamh2p1r5vw23l9hv7yvnb396dghvdbp9glbappqls";
-    })
-    (fetchpatch {
-      name = "prefer-symbolic-icon-if-avail.patch";
-      url = "https://github.com/jonls/redshift/commit/dca8334646d8fefeb56332eb1535c595a6599c7d.patch";
-      sha256 = "1m0mv8imbikxybl79vm0d3yagr384j8sjv53ixj9wp37wf2a5sii";
-    })
-    (fetchpatch {
-      name = "allow-hash-comment-char.patch";
-      url = "https://github.com/jonls/redshift/commit/f7fbbb04c335cd844b97c80128145349854128f4.patch";
-      sha256 = "0h1zgdahg3h91gcv6w7migf1ja6k2p7r6l6bh870xqi8h71cpspq";
-    })
-    (fetchpatch {
-      name = "additional-suspend-durations.patch";
-      url = "https://github.com/jonls/redshift/commit/2e91da281e1161af594e07eed62b672f9b006a39.patch";
-      sha256 = "0dhf75l1x67g8nbwm70gsm5923diwvjlwhh5zyffn5ylbml81cg9";
-    })
-  ];
+    src = fetchFromGitHub {
+      owner = "minus7";
+      repo = "redshift";
+      rev = "eecbfedac48f827e96ad5e151de8f41f6cd3af66";
+      sha256 = "0rs9bxxrw4wscf4a8yl776a8g880m5gcm75q06yx2cn3lw2b7v22";
+    };
 
-  nativeBuildInputs = [
-    autoconf
-    automake
-    gettext
-    intltool
-    libtool
-    pkgconfig
-    wrapGAppsHook
-    wrapPython
-  ];
-
-  configureFlags = [
-    "--enable-randr=${if withRandr then "yes" else "no"}"
-    "--enable-geoclue2=${if withGeoclue then "yes" else "no"}"
-    "--enable-drm=${if withDrm then "yes" else "no"}"
-    "--enable-quartz=${if withQuartz then "yes" else "no"}"
-    "--enable-corelocation=${if withCoreLocation then "yes" else "no"}"
-  ];
-
-  buildInputs = [
-    gobject-introspection
-    gtk3
-    python
-  ] ++ stdenv.lib.optional  withRandr        libxcb
-    ++ stdenv.lib.optional  withGeoclue      geoclue
-    ++ stdenv.lib.optional  withDrm          libdrm
-    ++ stdenv.lib.optional  withQuartz       ApplicationServices
-    ++ stdenv.lib.optionals withCoreLocation [ CoreLocation Foundation Cocoa ]
-    ;
-
-  pythonPath = [ pygobject3 pyxdg ];
-
-  preConfigure = "./bootstrap";
-
-  postFixup = "wrapPythonPrograms";
-
-  # the geoclue agent may inspect these paths and expect them to be
-  # valid without having the correct $PATH set
-  postInstall = ''
-    substituteInPlace $out/share/applications/redshift.desktop \
-      --replace 'Exec=redshift' "Exec=$out/bin/redshift"
-    substituteInPlace $out/share/applications/redshift-gtk.desktop \
-      --replace 'Exec=redshift-gtk' "Exec=$out/bin/redshift-gtk"
-  '';
-
-  enableParallelBuilding = true;
-
-  meta = with stdenv.lib; {
-    description = "Screen color temperature manager";
-    longDescription = ''
-      Redshift adjusts the color temperature according to the position
-      of the sun. A different color temperature is set during night and
-      daytime. During twilight and early morning, the color temperature
-      transitions smoothly from night to daytime temperature to allow
-      your eyes to slowly adapt. At night the color temperature should
-      be set to match the lamps in your room.
-    '';
-    license = licenses.gpl3Plus;
-    homepage = http://jonls.dk/redshift;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ yegortimoshenko globin ];
+    meta = redshift.meta // {
+      description = redshift.meta.description + "(with wlroots patches)";
+      homepage = https://github.com/minus7/redshift;
+    };
   };
 }
