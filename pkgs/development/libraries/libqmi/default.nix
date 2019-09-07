563d200ebf788b9f5b9e26524563a763fd090372
Will Dietz <w@wdtz.org>
2019-09-07 14:23:27 -0500

    libqmi: docbook bits

--- pkgs/development/libraries/libqmi/default.nix
+++ pkgs/development/libraries/libqmi/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchurl, fetchFromGitHub, autoreconfHook, pkgconfig, glib, python3, libgudev, libmbim, gtk-doc }:
+{ stdenv, fetchurl, fetchFromGitHub, autoreconfHook, pkgconfig, glib, python3, libgudev, libmbim
+, gtk-doc, docbook_xsl, docbook_xml_dtd_43 }:
 
 stdenv.mkDerivation rec {
   pname = "libqmi";
@@ -26,6 +27,7 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [
     autoreconfHook # needed when building from git
     gtk-doc # needed to build docs, but w/releases pre-gen'd docs can be used
+    docbook_xsl docbook_xml_dtd_43 # (also needed for building docs)
     pkgconfig
     python3
   ];
