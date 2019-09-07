33ee6f134c398235a7fd7038c1175c5f4f844fef
Will Dietz <w@wdtz.org>
2019-09-07 11:14:30 -0500

    glib: 2.60.7 -> 2.62.0
    https://gitlab.gnome.org/GNOME/glib/-/tags/2.62.0

--- pkgs/development/libraries/glib/default.nix
+++ pkgs/development/libraries/glib/default.nix
@@ -50,11 +50,11 @@ in
 
 stdenv.mkDerivation rec {
   pname = "glib";
-  version = "2.60.7";
+  version = "2.62.0";
 
   src = fetchurl {
     url = "mirror://gnome/sources/glib/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0433m0na8nc4cf0gidf4gfzz8k5d3dsssmh541qkpzcsaspw04lb";
+    sha256 = "046sqfmr84blxh4vkipmh8ff7wd19fxmh6lnr5ibchx3l02p49bc";
   };
 
   patches = optional stdenv.isDarwin ./darwin-compilation.patch
