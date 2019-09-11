c0a4761fafc92e540f973ba7c1e37fe4f90374f4
Will Dietz <w@wdtz.org>
2019-09-11 13:19:15 -0500

    intel-media-driver: 19.3.pre3 -> 19.3.pre4

--- pkgs/development/libraries/intel-media-driver/default.nix
+++ pkgs/development/libraries/intel-media-driver/default.nix
@@ -5,13 +5,13 @@
 
 stdenv.mkDerivation rec {
   name = "intel-media-driver-${version}";
-  version = "19.3.pre3";
+  version = "19.3.pre4";
 
   src = fetchFromGitHub {
     owner  = "intel";
     repo   = "media-driver";
     rev    = "intel-media-${version}";
-    sha256 = "0plqjbx8ffp65dgpms6ixvl7awqxilvsc4ihiw38c26yx7xr5ffb";
+    sha256 = "06pdvyhfyj3rxha287zdw90fv13vkr6dqrk66vng9ijgpk50k8ck";
   };
 
   cmakeFlags = [
