d69d8ad5bb45b3a520f8e8265dc293ec340a641c
Will Dietz <w@wdtz.org>
2019-09-13 13:25:21 -0500

    super-productivity: 2.10.9 -> 2.10.12
    2.11 tagged but doesn't have deb attached (yet?).

--- pkgs/applications/networking/super-productivity/default.nix
+++ pkgs/applications/networking/super-productivity/default.nix
@@ -7,7 +7,7 @@
 
 let
 
-  version = "2.10.9";
+  version = "2.10.12";
 
   rpath = stdenv.lib.makeLibraryPath [
     alsaLib
@@ -55,7 +55,7 @@ let
     if stdenv.hostPlatform.system == "x86_64-linux" then
       fetchurl {
         url = "https://github.com/johannesjo/super-productivity/releases/download/v${version}/superProductivity_${version}_amd64.deb";
-        sha256 = "0fxncpwbk3yv058c3sg9ykd6ij7wb4cmvh0vdac0n44y4nn4bf7a";
+        sha256 = "18sp2a0pxzh36fag93yv5g2xg9f502s4pbq90aqq0192rmw0q7iv";
       }
     else
       throw "super-productivity is not supported on ${stdenv.hostPlatform.system}";
