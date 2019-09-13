72c93d0be149f8162049d192aa80e0cd17f5a2ee
Will Dietz <w@wdtz.org>
2019-09-12 20:35:53 -0500

    cifs-utils: libcap so supports dropping capabilities

--- pkgs/os-specific/linux/cifs-utils/default.nix
+++ pkgs/os-specific/linux/cifs-utils/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, autoreconfHook, docutils, pkgconfig
-, kerberos, keyutils, pam, talloc }:
+, kerberos, keyutils, pam, talloc, libcap }:
 
 stdenv.mkDerivation rec {
   name = "cifs-utils-${version}";
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ autoreconfHook docutils pkgconfig ];
 
-  buildInputs = [ kerberos keyutils pam talloc ];
+  buildInputs = [ kerberos keyutils pam talloc libcap ];
 
   makeFlags = "root_sbindir=$(out)/sbin";
 
