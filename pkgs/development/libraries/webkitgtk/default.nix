df2f510ba8fa2b9a881a5c24c1cd4eb266097a3e
Will Dietz <w@wdtz.org>
2019-09-13 14:38:36 -0500

    webkitgtk: use wpe? :D

--- pkgs/development/libraries/webkitgtk/default.nix
+++ pkgs/development/libraries/webkitgtk/default.nix
@@ -6,7 +6,7 @@
 , enableGeoLocation ? true, geoclue2, sqlite
 , enableGtk2Plugins ? false, gtk2 ? null
 , gst-plugins-base, gst-plugins-bad, woff2
-, libwpe
+, libwpe, wpebackend-fdo
 , bubblewrap, libseccomp, xdg-dbus-proxy
 }:
 
@@ -94,8 +94,7 @@ stdenv.mkDerivation rec {
     libintl libwebp enchant2 libnotify gnutls pcre nettle libidn libgcrypt woff2
     libxml2 libsecret libxslt harfbuzz libpthreadstubs libtasn1 p11-kit openjpeg
     sqlite gst-plugins-base gst-plugins-bad libxkbcommon epoxy dbus at-spi2-core
-    # TODO: wpebackend-fdo
-    # libwpe
+    libwpe wpebackend-fdo
     bubblewrap libseccomp xdg-dbus-proxy
   ] ++ optional enableGeoLocation geoclue2
     ++ optional enableGtk2Plugins gtk2
