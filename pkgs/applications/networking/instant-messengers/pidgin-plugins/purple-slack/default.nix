62392d092b9510e450e7dab54254ad7270c959d8
eyjhbb@gmail.com <eyjhbb@gmail.com>
2019-09-15 17:33:17 +0200

    purple-slack: Init at 2019-08-18
    (cherry picked from commit 2179932043728448b054412f6103a3da22b9446d)

---
+++ pkgs/applications/networking/instant-messengers/pidgin-plugins/purple-slack/default.nix
@@ -0,0 +1,27 @@
+{ stdenv, fetchFromGitHub, pidgin }:
+
+stdenv.mkDerivation {
+  pname = "purple-slack";
+  version = "unstable-2019-08-18";
+
+  src = fetchFromGitHub {
+    owner = "dylex";
+    repo = "slack-libpurple";
+    rev = "be97802c7fd0b611722d2f551756e2a2672f6084";
+    sha256 = "0l6hwnnv6zzszxkk0a3nli88w5gijvlc9qgkkai0sb4v4c504y5v";
+  };
+
+  buildInputs = [ pidgin ];
+
+  PKG_CONFIG_PURPLE_PLUGINDIR = "${placeholder "out"}/lib/purple-2";
+  PKG_CONFIG_PURPLE_DATAROOTDIR = "${placeholder "out"}/share";
+
+  meta = with stdenv.lib; {
+    homepage = https://github.com/dylex/slack-libpurple;
+    description = "Slack plugin for Pidgin";
+    license = licenses.gpl2;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ eyjhb ];
+  };
+}
+
