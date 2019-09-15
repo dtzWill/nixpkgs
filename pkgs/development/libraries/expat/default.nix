6a13c0f6dcafe8f1b82084d2c13f8c02059b60f6
Will Dietz <w@wdtz.org>
2019-09-14 21:45:03 -0500

    expat: 2.2.7 -> 2.2.8 (security)
    https://blog.hartwork.org/posts/expat-228-with-security-fixes-has-been-released/
    
    CVE-2019-15903

--- pkgs/development/libraries/expat/default.nix
+++ pkgs/development/libraries/expat/default.nix
@@ -1,11 +1,12 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "expat-2.2.7";
+  pname = "expat";
+  version = "2.2.8";
 
   src = fetchurl {
-    url = "mirror://sourceforge/expat/${name}.tar.bz2";
-    sha256 = "067cfhqwiswm4zynw7xaxl59mrrimaiyjhnn8byxma1i98pi1jfb";
+    url = "mirror://sourceforge/${pname}/${pname}-${version}.tar.bz2";
+    sha256 = "00ji8zv33r2izb405bvp09ixj8fkwmd5y78pwhsdm0jsn140j4ws";
   };
 
   outputs = [ "out" "dev" ]; # TODO: fix referrers
