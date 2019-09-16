fd2dbbe962799d94343a6becdc233811d776520b
Roman Volosatovs <rvolosatovs@riseup.net>
2019-09-16 22:46:07 +0200

    all-packages: use go 1.12 for cri-o
    (cherry picked from commit 2f20deb2024bdcc051317f61263b865cd8d70954)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22777,7 +22777,9 @@ in
 
   crispyDoom = callPackage ../games/crispy-doom { };
 
-  cri-o = callPackage ../applications/virtualization/cri-o {};
+  cri-o = callPackage ../applications/virtualization/cri-o {
+    buildGoPackage = buildGo112Package;
+  };
 
   ckan = callPackage ../games/ckan { };
 
