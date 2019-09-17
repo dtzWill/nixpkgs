7fd40181abf5f64c3bd38a34527c6406e350dcb2
Will Dietz <w@wdtz.org>
2019-09-17 13:16:32 -0500

    libfido2: 1.2.0 -> git

--- pkgs/development/libraries/libfido2/default.nix
+++ pkgs/development/libraries/libfido2/default.nix
@@ -1,11 +1,18 @@
-{ stdenv, fetchurl, cmake, pkgconfig, libcbor, libressl, udev }:
+{ stdenv, fetchurl, fetchFromGitHub, cmake, pkgconfig, libcbor, libressl, udev }:
 
 stdenv.mkDerivation rec {
   pname = "libfido2";
-  version = "1.2.0";
-  src = fetchurl {
-    url = "https://developers.yubico.com/libfido2/Releases/libfido2-${version}.tar.gz";
-    sha256 = "1pbllhzcrzkgxad00bai7lna8dpkwiv8khx8p20miy661abv956v";
+  version = "2019-09-17";
+  #version = "1.2.0";
+  #src = fetchurl {
+  #  url = "https://developers.yubico.com/libfido2/Releases/libfido2-${version}.tar.gz";
+  #  sha256 = "1pbllhzcrzkgxad00bai7lna8dpkwiv8khx8p20miy661abv956v";
+  #};
+  src = fetchFromGitHub {
+    owner = "Yubico";
+    repo = pname;
+    rev = "5bcf1315b9d86cb5e40f14c04343aff4fc3917a3";
+    sha256 = "0nas7l29vfs249wsb0jiq6vrlyhs3frm2qpj5p69bf7zk7ir532r";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
