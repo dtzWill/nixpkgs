b77f737c108e1a023eda00b977362a67bbd733ab
Will Dietz <w@wdtz.org>
2019-09-18 01:45:08 -0500

    graphviz: 2.42.0 -> 2.42.2

--- pkgs/tools/graphics/graphviz/default.nix
+++ pkgs/tools/graphics/graphviz/default.nix
@@ -1,5 +1,5 @@
 import ./base.nix rec {
   rev = "stable_release_${version}";
-  version = "2.42.0";
-  sha256 = "0f315zkibgmj8cx6zbfp1zm2vhla1zasl7gm0rf3scssqw5s4yjx";
+  version = "2.42.2";
+  sha256 = "0lacl11amyvj04j78m63qifljl4c0nkyy50z4bkg8mg9j4hjdy0x";
  }
