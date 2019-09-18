3733fcab8677ffc4829d3b98a193ca9c9f1dd42c
Will Dietz <w@wdtz.org>
2019-09-17 20:00:32 -0500

    nm-tray: nvm longDescription

--- pkgs/tools/networking/network-manager/tray.nix
+++ pkgs/tools/networking/network-manager/tray.nix
@@ -23,12 +23,6 @@ mkDerivation rec {
 
   meta = with lib; {
     description = "Simple Network Manager frontend written in Qt";
-    longDescription = ''
-      nm-tray is a simple NetworkManager front end with information icon residing
-      in system tray (like e.g. nm-applet). It's a pure Qt application. For
-      interaction with NetworkManager it uses API provided by KF5::NetworkManagerQt
-      -> plain DBus communication.
-    '';
     homepage = "https://github.com/palinek/nm-tray";
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ dtzWill ];
