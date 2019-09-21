2a076ec8e71edc55ba870cea8ff36f394b4cd8e9
Will Dietz <w@wdtz.org>
2019-09-21 10:10:31 -0500

    qcad: deps, meta

--- pkgs/applications/graphics/qcad/default.nix
+++ pkgs/applications/graphics/qcad/default.nix
@@ -1,5 +1,7 @@
 { lib, mkDerivation, hostPlatform, fetchFromGitHub
-, qmake, qtbase, qtscript, qtxmlpatterns, qtmacextras }:
+, qmake, qttools
+, qtbase, qtmultimedia, qtscript, qtsvg, qtxmlpatterns
+, libGLU_combined, qtmacextras }:
 
 mkDerivation rec {
   pname = "qcad";
@@ -12,7 +14,18 @@ mkDerivation rec {
     sha256 = "16ihpr1j0z28zlmvr18zdnw4ba1wksq5rplmch2ffsk4ihg9d8k1";
   };
 
-  nativeBuildInputs = [ qmake ];
-  buildInputs = [ qtbase qtscript qtxmlpatterns ] ++ lib.optional hostPlatform.isDarwin qtmacextras;
-}
+  nativeBuildInputs = [ qmake qttools ];
+  buildInputs = [
+    libGLU_combined
+    qtbase qtmultimedia qtscript qtsvg qtxmlpatterns
+  ]
+  ++ lib.optional hostPlatform.isDarwin qtmacextras;
 
+  meta = with lib; {
+    description = "2D CAD package based upon Qt";
+    homepage = "https://qcad.org";
+    platforms = platforms.all;
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ dtzWill ];
+  };
+}
