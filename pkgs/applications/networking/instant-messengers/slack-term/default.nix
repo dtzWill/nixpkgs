fbbfb1c629697458d0b2314a79d16647ba325f69
Will Dietz <w@wdtz.org>
2019-09-21 18:00:24 -0500

    slack-term: bump

--- pkgs/applications/networking/instant-messengers/slack-term/default.nix
+++ pkgs/applications/networking/instant-messengers/slack-term/default.nix
@@ -11,8 +11,8 @@ buildGoModule rec {
     owner = "erroneousboat";
     repo = "slack-term";
     #rev = "v${version}";
-    rev = "f1d94f069af129a0c800fac0f9c71e4565176370";
-    sha256 = "1wskjivg3brryn6hl20nxq3n9b4dxa6cbw7vifa34p4kjkrgd07x";
+    rev = "d36cd2d125bc2e5037110cfd0a8ca0e3efa72642";
+    sha256 = "19k2rkfqqjz3jhi1bbrah9rwfqhnw13rf5xdy8rbgjv992d84vpv";
   };
 
   modSha256 = "0lw5j1nxbsc1clvwscyvpy8vkww4yk3qiffpnqqrn4sqz0m8cb83";
