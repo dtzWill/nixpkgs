0e2cc366562de3ca16f9a81ed660613e1369afa1
Will Dietz <w@wdtz.org>
2019-09-21 15:21:59 -0500

    drumstick: cleanup and use qt5's mkDerivation and callPackage

--- pkgs/development/libraries/drumstick/default.nix
+++ pkgs/development/libraries/drumstick/default.nix
@@ -1,13 +1,13 @@
-{ stdenv, fetchurl, alsaLib, cmake, docbook_xsl, docbook_xml_dtd_45, doxygen
-, fluidsynth, pkgconfig, qt5
+{ lib, mkDerivation, fetchurl, alsaLib, cmake, docbook_xsl, docbook_xml_dtd_45, doxygen
+, fluidsynth, pkgconfig, qtbase, qtsvg
 }:
 
-stdenv.mkDerivation rec {
-  name = "drumstick-${version}";
+mkDerivation rec {
+  pname = "drumstick";
   version = "1.1.2";
 
   src = fetchurl {
-    url = "mirror://sourceforge/drumstick/${version}/${name}.tar.bz2";
+    url = "mirror://sourceforge/${pname}/${version}/${pname}-${version}.tar.bz2";
     sha256 = "0kljqyqj7s1i2z52i24x7ail1bywn6dcxxfbad5c59drm8wv94bp";
   };
 
@@ -15,17 +15,12 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = true;
 
-  #Temporarily remove drumstick-piano; Gives segment fault. Submitted ticket
-  postInstall = ''
-    rm $out/bin/drumstick-vpiano
-    '';
-
   nativeBuildInputs = [ cmake pkgconfig docbook_xsl docbook_xml_dtd_45 docbook_xml_dtd_45 ];
   buildInputs = [
-    alsaLib doxygen fluidsynth qt5.qtbase qt5.qtsvg
+    alsaLib doxygen fluidsynth qtbase qtsvg
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     maintainers = with maintainers; [ solson ];
     description = "MIDI libraries for Qt5/C++";
     homepage = http://drumstick.sourceforge.net/;
