179df53a7a134e12d7c17dfaf4043e389aec6e47
Will Dietz <w@wdtz.org>
2019-09-21 00:06:07 -0500

    arcanist: fix, touchup

--- pkgs/development/tools/misc/arcanist/default.nix
+++ pkgs/development/tools/misc/arcanist/default.nix
@@ -1,13 +1,15 @@
 { stdenv, fetchFromGitHub, php, flex, makeWrapper }:
 
 let
-  libphutil = fetchFromGitHub {
+  libphutil = fetchFromGitHub rec {
+    name = "${repo}-${builtins.substring 0 7 rev}";
     owner = "phacility";
     repo = "libphutil";
     rev = "f51f1b3f72b50246949d0917239ca84f39ec7a54";
     sha256 = "1pffvq9rl5b1gldpzzyv6cvwmcr2f47kfavr9fkrdz63k3i7yan3";
   };
-  arcanist = fetchFromGitHub {
+  arcanist = fetchFromGitHub rec {
+    name = "${repo}-${builtins.substring 0 7 rev}";
     owner = "phacility";
     repo = "arcanist";
     rev = "3cdfe1fff806d2b54a2df631cf90193e518f42b7";
@@ -20,7 +22,8 @@ stdenv.mkDerivation rec {
 
   srcs = [ arcanist libphutil ];
   sourceRoot = ".";
-  buildInputs = [ php makeWrapper flex ];
+  nativeBuildInputs = [ makeWrapper ];
+  buildInputs = [ php flex ];
 
   #unpackPhase = ''
   #  cp -aR ${libphutil} libphutil
@@ -29,23 +32,25 @@ stdenv.mkDerivation rec {
   #'';
 
   postPatch = stdenv.lib.optionalString stdenv.isAarch64 ''
-    substituteInPlace libphutil/support/xhpast/Makefile \
+    substituteInPlace libphutil*/support/xhpast/Makefile \
       --replace "-minline-all-stringops" ""
   '';
 
-  buildPhase = ''
-    (
-      cd libphutil/support/xhpast
-      make clean all install
-    )
-  '';
+  makeFlags = [
+    "-C" "${libphutil.name}/support/xhpast"
+  ];
+  buildFlags = [
+    "clean"
+    "all"
+    "install"
+  ];
   installPhase = ''
-    mkdir -p $out/bin $out/libexec
-    cp -R libphutil $out/libexec/libphutil
-    cp -R arcanist  $out/libexec/arcanist
+    mkdir -p $out/{bin,libexec}
+    mv ${libphutil.name} $out/libexec/libphutil
+    # TODO: symlink to store ?
+    mv ${arcanist.name} $out/libexec/arcanist
 
-    ln -s $out/libexec/arcanist/bin/arc $out/bin
-    wrapProgram $out/bin/arc \
+    makeWrapper $out/libexec/arcanist/bin/arc $out/bin/arc \
       --prefix PATH : "${php}/bin"
   '';
 
