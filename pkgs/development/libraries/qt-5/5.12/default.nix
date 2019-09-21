60f05468a343b266963289aed73bcb4bd110e56a
Will Dietz <w@wdtz.org>
2019-09-21 13:14:39 -0500

    full: include qtgamepad

--- pkgs/development/libraries/qt-5/5.12/default.nix
+++ pkgs/development/libraries/qt-5/5.12/default.nix
@@ -139,7 +139,7 @@ let
 
       env = callPackage ../qt-env.nix {};
       full = env "qt-full-${qtbase.version}" ([
-        qtcharts qtconnectivity qtdeclarative qtdoc qtgraphicaleffects
+        qtcharts qtconnectivity qtdeclarative qtdoc qtgamepad qtgraphicaleffects
         qtimageformats qtlocation qtmultimedia qtquickcontrols qtquickcontrols2
         qtscript qtsensors qtserialport qtsvg qttools qttranslations
         qtvirtualkeyboard qtwebchannel qtwebengine qtwebkit qtwebsockets
