6c95578d3ed30533a79aaaaa55ad9d5c6dd61f57
Will Dietz <w@wdtz.org>
2019-09-23 08:28:14 -0500

    cog: 0.3.91 -> 0.4.0

--- pkgs/applications/misc/cog/default.nix
+++ pkgs/applications/misc/cog/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "cog";
-  version = "0.3.91";
+  version = "0.4.0";
 
   src = fetchurl {
     url = "https://wpewebkit.org/releases/${pname}-${version}.tar.xz";
-    sha256 = "1y7fvzik7jzz2khz5cwksgnlch6fszjsysw4my9a9fa1pisv1hcr";
+    sha256 = "00zw0k4idpn7ksyn13hcv0n5h364b5pix6s118sbfd144d8kmhg9";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
