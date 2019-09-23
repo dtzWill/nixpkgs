053536622d3dc6b67e353a97bc55fb5c131ab944
Mario Rodas <marsam@users.noreply.github.com>
2019-09-22 20:20:20 -0500

    cargo-vendor: drop
    Cargo 1.37 imported `cargo-vendor` as built-in command [1]
    
    [1] https://github.com/rust-lang/cargo/pull/6869
    
    (cherry picked from commit aed74e828479fcdc16593ca983ade1adff4a675e)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8486,7 +8486,6 @@ in
   buildRustCrateHelpers = callPackage ../build-support/rust/build-rust-crate/helpers.nix { };
   buildRustCrateTests = recurseIntoAttrs (callPackage ../build-support/rust/build-rust-crate/test { }).tests;
   cratesIO = callPackage ../build-support/rust/crates-io.nix { };
-  cargo-vendor = callPackage ../build-support/rust/cargo-vendor { };
 
   cargo-web = callPackage ../development/tools/cargo-web {
     inherit (darwin.apple_sdk.frameworks) CoreServices Security;
