316a3de1dd6254009e599709f15f0619d8b400e6
Will Dietz <w@wdtz.org>
2019-09-23 03:36:55 -0500

    trousers: 0.3.13 -> 0.3.14

--- pkgs/tools/security/trousers/default.nix
+++ pkgs/tools/security/trousers/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, openssl, pkgconfig }:
 
 stdenv.mkDerivation rec {
-  name = "trousers-${version}";
-  version = "0.3.13";
+  pname = "trousers";
+  version = "0.3.14";
 
   src = fetchurl {
-    url = "mirror://sourceforge/trousers/trousers/${version}/${name}.tar.gz";
-    sha256 = "1lvnla1c1ig2w3xvvrqg2w9qm7a1ygzy1j2gg8j7p8c87i58x45v";
+    url = "mirror://sourceforge/trousers/trousers/${version}/${pname}-${version}.tar.gz";
+    sha256 = "0iwgsbrbb7nfqgl61x8aailwxm8akxh9gkcwxhsvf50x4qx72l6f";
   };
 
   nativeBuildInputs = [ pkgconfig ];
