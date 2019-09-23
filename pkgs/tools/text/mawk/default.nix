3d2b82a3d0202e36467dca594f472d13b93fbaab
Will Dietz <w@wdtz.org>
2019-09-23 07:36:36 -0500

    mawk: pname

--- pkgs/tools/text/mawk/default.nix
+++ pkgs/tools/text/mawk/default.nix
@@ -1,12 +1,13 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "mawk-1.3.4-20171017";
+  pname = "mawk";
+  version = "1.3.4-20171017";
 
   src = fetchurl {
     urls = [
-      "ftp://ftp.invisible-island.net/mawk/${name}.tgz"
-      "https://invisible-mirror.net/archives/mawk/${name}.tgz"
+      "ftp://ftp.invisible-island.net/mawk/${pname}-${version}.tgz"
+      "https://invisible-mirror.net/archives/mawk/${pname}-${version}.tgz"
     ];
     sha256 = "0nwyxhipn4jx7j695lih1xggxm6cp4fjk4wbgihd33ni3rfi25yv";
   };
