c5c3dd05074169131627cc56a9ea806913a8dd34
Joachim Fasting <joachifm@fastmail.fm>
2019-09-24 11:24:13 +0200

    tree-wide: replace uses of splitString "." with lib.versions
    Quoting from the splitString docstring:
    
       NOTE: this function is not performant and should never be used.
    
    This replaces trivial uses of splitString for splitting version
    strings with the (potentially builtin) splitVersion.
    
    (cherry picked from commit bad07dfac57c36dff1c7b3fd4020fea806d7be93)

--- pkgs/development/libraries/science/math/cudnn/generic.nix
+++ pkgs/development/libraries/science/math/cudnn/generic.nix
@@ -46,7 +46,7 @@ stdenv.mkDerivation rec {
 
   passthru = {
     inherit cudatoolkit;
-    majorVersion = lib.head (lib.splitString "." version);
+    majorVersion = lib.versions.major version;
   };
 
   meta = with stdenv.lib; {
