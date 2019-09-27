79c4726da65158e8f1188c06c001592a653be6f4
xrelkd <46590321+xrelkd@users.noreply.github.com>
2019-09-28 07:48:53 +0800

    go-ethereum: 1.9.3 -> 1.9.5
    (cherry picked from commit 042597a5a4e414dbb1f942c535cb2296fb393d5e)

--- pkgs/applications/blockchains/go-ethereum.nix
+++ pkgs/applications/blockchains/go-ethereum.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   pname = "go-ethereum";
-  version = "1.9.2";
+  version = "1.9.5";
 
   goPackagePath = "github.com/ethereum/go-ethereum";
 
@@ -17,7 +17,7 @@ buildGoPackage rec {
     owner = "ethereum";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0lymwylh4j63fzj9jy7mcw676a2ksgpsj9mazif1r3d2q73h9m88";
+    sha256 = "1h1c02dgazlcgp9lrm0zsig80nfj0c9553jy9nsvjyzf95ym1542";
   };
 
   meta = with stdenv.lib; {
