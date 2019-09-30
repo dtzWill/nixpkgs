3ab0072d00007fef8e61a452c608529458fdb585
Will Dietz <w@wdtz.org>
2019-09-30 18:32:57 -0500

    Revert "networkmanager-openconnect: patch instead" (strings)
    This reverts commit 50810e5dc301ba543858a9e296049bc86023bf4d.

--- pkgs/tools/networking/network-manager/openconnect/default.nix
+++ pkgs/tools/networking/network-manager/openconnect/default.nix
@@ -1,30 +1,29 @@
-{ stdenv, fetchurl, substituteAll, openconnect, intltool, pkgconfig, autoreconfHook, networkmanager, gcr, libsecret, file
+{ stdenv, fetchurl, fetchFromGitLab, substituteAll, openconnect, intltool, pkgconfig, autoreconfHook, networkmanager, gcr, libsecret, file
 , gtk3, withGnome ? true, gnome3, kmod }:
 
 let
   pname   = "NetworkManager-openconnect";
-  version = "1.2.6";
+  version = "1.2.7-dev-9"; # 2019-09-25
 in stdenv.mkDerivation {
   name    = "${pname}${if withGnome then "-gnome" else ""}-${version}";
 
-  src = fetchurl {
-    url    = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0nlp290nkawc4wqm978n4vhzg3xdqi8kpjjx19l855vab41rh44m";
-  };
-  #src = fetchFromGitLab {
-  #  domain = "gitlab.gnome.org";
-  #  owner = "GNOME";
-  #  repo = pname;
-  #  rev = "de44e42509d85c0ce94492441b5aa70a90d186fb";
-  #  sha256 = "1n044w9793m95nml3va1dnwg23y82lwga4mz2g9i8b4jr64ijqpw";
+  #src = fetchurl {
+  #  url    = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
+  #  sha256 = "0nlp290nkawc4wqm978n4vhzg3xdqi8kpjjx19l855vab41rh44m";
   #};
+  src = fetchFromGitLab {
+    domain = "gitlab.gnome.org";
+    owner = "GNOME";
+    repo = pname;
+    rev = "de44e42509d85c0ce94492441b5aa70a90d186fb";
+    sha256 = "1n044w9793m95nml3va1dnwg23y82lwga4mz2g9i8b4jr64ijqpw";
+  };
 
   patches = [
     (substituteAll {
       src = ./fix-paths.patch;
       inherit kmod openconnect;
     })
-    ./handle-ipv6-nameservers.patch
     #./can-persist.patch
     #./0001-Bug-770880-Revamp-certificate-warning-accept-dialog.patch
     #./0001-Bug-770880-Disallow-manual-cert-acceptance.patch
@@ -33,7 +32,7 @@ in stdenv.mkDerivation {
   buildInputs = [ openconnect networkmanager ]
     ++ stdenv.lib.optionals withGnome [ gtk3 gcr libsecret ];
 
-  nativeBuildInputs = [ intltool pkgconfig file ];
+  nativeBuildInputs = [ intltool pkgconfig file autoreconfHook ];
 
   configureFlags = [
     "--with-gnome=${if withGnome then "yes" else "no"}"
