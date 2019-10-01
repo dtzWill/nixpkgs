62548231665d5793a435ccd93bd5bc5d75bc0aec
Will Dietz <w@wdtz.org>
2019-10-01 11:08:24 -0500

    spleen: 1.4.0 -> 1.5.0

--- pkgs/data/fonts/spleen/default.nix
+++ pkgs/data/fonts/spleen/default.nix
@@ -2,7 +2,7 @@
 
 let
   pname = "spleen";
-  version = "1.4.0";
+  version = "1.5.0";
 in fetchurl rec {
   name = "${pname}-${version}";
   url = "https://github.com/fcambus/spleen/releases/download/${version}/spleen-${version}.tar.gz";
@@ -15,7 +15,7 @@ in fetchurl rec {
     install -Dm644 *.{pcf.gz,psfu,bdf} -t $d
     install -m644 fonts.alias-spleen $d/fonts.alias
   '';
-  sha256 = "06j7bn5q16rzgxhgc2ad7x62szk45pbl1n28bc69bfl4h703np14";
+  sha256 = "0iyw2n5db2bm1swb4ac3xrdm74rr8g4hzw1zfbzb01l7bzmr0xqq";
 
   meta = with lib; {
     description = "Monospaced bitmap fonts";
