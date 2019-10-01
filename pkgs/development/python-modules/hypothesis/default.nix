d953e0982b43e10da054995ad5899e2791a84c29
Will Dietz <w@wdtz.org>
2019-10-01 11:08:57 -0500

    Revert "python3Packages.hypothesis: 4.24.6 -> 4.38.0"
    This reverts commit 3dc70d5e6f37147a4c9177c5e41e06151596ff09.

--- pkgs/development/python-modules/hypothesis/default.nix
+++ pkgs/development/python-modules/hypothesis/default.nix
@@ -9,7 +9,7 @@ buildPythonPackage rec {
   # pytz fake_factory django numpy pytest
   # If you need these, you can just add them to your environment.
 
-  version = "4.38.0";
+  version = "4.24.6";
   pname = "hypothesis";
 
   # Use github tarballs that includes tests
@@ -17,7 +17,7 @@ buildPythonPackage rec {
     owner = "HypothesisWorks";
     repo = "hypothesis-python";
     rev = "hypothesis-python-${version}";
-    sha256 = "1pbagiskbnh90gs0rkqsnhkh00qcsmpmwxchg19kgw50dl7k26zi";
+    sha256 = "04kns6bf0phws5q83gbi8dqlvvp2di8w5i4r5ln7ij4gis3z262h";
   };
 
   postUnpack = "sourceRoot=$sourceRoot/hypothesis-python";
