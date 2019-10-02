ccebfb6b2fbe1bc20fc2d41946c79966517d748c
Will Dietz <w@wdtz.org>
2019-10-02 08:05:56 -0500

    Revert "scfbuild: wip py3 patch from upstream PR"
    This reverts commit 85f2b2ae3bac0dcfabfd7795cb780706829067f0.

--- pkgs/tools/misc/scfbuild/default.nix
+++ pkgs/tools/misc/scfbuild/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, fetchpatch, buildPythonApplication, python, pyyaml, fonttools, fontforge }:
+{ lib, buildPythonApplication, fetchFromGitHub, python, pyyaml, fonttools, fontforge }:
 
 buildPythonApplication rec {
   name = "scfbuild-${version}";
@@ -10,14 +10,6 @@ buildPythonApplication rec {
     rev = "9acc7fc5fedbf48683d8932dd5bd7583bf922bae";
     sha256 = "1zlqsxkpg7zvmhdjgbqwwc9qgac2b8amzq8c5kwyh5cv95zcp6qn";
   };
-
-  patches = [
-    # WIP py3 PR!
-    (fetchpatch {
-      url = "https://github.com/13rac1/scfbuild/pull/19.patch";
-      sha256 = "1d512i87glp8l24zk6ngp04ac1mxjvqr7hslnflhhyi022z9rgyc";
-    })
-  ];
 
   phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
 
