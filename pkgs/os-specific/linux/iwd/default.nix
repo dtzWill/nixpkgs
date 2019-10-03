b9bea1400fa1a1df0f0713c0c09b2d58a93dddd2
Will Dietz <w@wdtz.org>
2019-10-03 08:29:40 -0500

    Revert "Revert "iwd: let's try git bump again""
    This reverts commit 32a30250b63dd936ea45a10320eb5c7fbb82056c.

--- pkgs/os-specific/linux/iwd/default.nix
+++ pkgs/os-specific/linux/iwd/default.nix
@@ -6,13 +6,13 @@ stdenv.mkDerivation rec {
   pname = "iwd";
 
   #version = "0.21";
-  version = "2019-09-24";
+  version = "2019-10-02";
 
   src = fetchgit {
     url = https://git.kernel.org/pub/scm/network/wireless/iwd.git;
     #rev = version;
-    rev = "64ff5dda7777957480462249bfe685e7823f9cc7";
-    sha256 = "02ija5yk0fsgy7fcvnjv15xd38r4l7s9371m3yfa6p89dg3000ck";
+    rev = "c4ecf969421f8c44930310819591cf4eab1f7dbe";
+    sha256 = "1mvw5ddi97rv3pjgm0a9mzhz88bqhr07ddamq6p03dcx7w2h90gv";
   };
 
   nativeBuildInputs = [
