e4f4a6d4c98f83a0559fda03dcaa0759926d70b0
Will Dietz <w@wdtz.org>
2019-10-02 22:50:23 -0500

    vit2: bump through 2.0.0 release, use upstream's fix! \o/

--- pkgs/applications/misc/vit/python.nix
+++ pkgs/applications/misc/vit/python.nix
@@ -4,14 +4,14 @@ with python3Packages;
 
 buildPythonApplication rec {
   pname = "vit";
-  version = "2.0.0b2-git";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "scottkosty";
     repo = pname;
   #  rev = "v${version}";
-    rev = "9960f135bb96142c517a865fd366aac855ecc414";
-    sha256 = "1q0yvqq0q1afh1f2z218van9m1qzpnad235janb1lasif22sq3is";
+    rev = "bf4cb069228e12b26e436b120fb9938a2b6a1531";
+    sha256 = "0s5q05pq0pc7y5azkmmxbizh88mvsq8nc3cimj1mwi5v8il16q8l";
   };
 
   propagatedBuildInputs = [
@@ -23,8 +23,6 @@ buildPythonApplication rec {
   preCheck = ''
     export TERM=linux
   '';
-
-  patches = [ ./no-columns.patch ];
 
   meta = with lib; {
     license = licenses.mit;
