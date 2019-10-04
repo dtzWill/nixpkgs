a99294de081b06b0ca38e02e5bd63f797111b2df
David Wood <david@davidtw.co>
2019-10-04 12:02:29 +0100

    starship: 0.19.0 -> 0.20.0
    (cherry picked from commit 1e06bcc82ee83ab1f18efc936d9f7bdb37ee5567)

--- pkgs/tools/misc/starship/default.nix
+++ pkgs/tools/misc/starship/default.nix
@@ -2,18 +2,18 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "starship";
-  version = "0.19.0";
+  version = "0.20.0";
 
   src = fetchFromGitHub {
     owner = "starship";
     repo = "starship";
     rev = "v${version}";
-    sha256 = "0cj3r8c583xzraggwl1xb36790czha4dja6c3yvn5f19whmmf2g9";
+    sha256 = "000acnwb63fiida51ym06rnv11dc6nsqlanz1v8sq5zb6nzv9q59";
   };
 
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ libiconv darwin.apple_sdk.frameworks.Security ];
 
-  cargoSha256 = "1ldhkys44nkwjm6cmz1x2k5j4n79rqkh6km86hssjps5bzw6niw4";
+  cargoSha256 = "0c82k6aw245vsqkcrg6bhqhylfbxdszcr040mrz7cz9ydxcb58b9";
   checkPhase = "cargo test -- --skip directory::home_directory --skip directory::directory_in_root";
 
   meta = with stdenv.lib; {
