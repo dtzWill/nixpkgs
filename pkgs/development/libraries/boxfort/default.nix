1f86282cf3764a560fba8f1f4cd5aacea82690c8
Will Dietz <w@wdtz.org>
2019-10-04 14:44:53 -0500

    boxfort: git bump, meson, tests
    Not sure re:cmake flags?

--- pkgs/development/libraries/boxfort/default.nix
+++ pkgs/development/libraries/boxfort/default.nix
@@ -1,21 +1,29 @@
-{ stdenv, fetchFromGitHub, cmake, pkgconfig, gettext, libcsptr, dyncall, nanomsg}:
+{ stdenv, fetchFromGitHub, meson, ninja, pkgconfig, gettext, libcsptr, dyncall, nanomsg, python3}:
 
 stdenv.mkDerivation rec {
-  name = "boxfort";
+  pname = "boxfort";
+  version = "unstable-2019-09-19";
 
   src = fetchFromGitHub {
     owner = "Snaipe";
-    repo = "BoxFort";
-    rev = "926bd4ce968592dbbba97ec1bb9aeca3edf29b0d";
-    sha256 = "0mzy4f8qij6ckn5578y3l4rni2470pdkjy5xww7ak99l1kh3p3v6";
+    repo = pname;
+    rev = "38fe63046fbabcae34ebc2ee9867d990ac28c4c5";
+    sha256 = "03zy982d1frpk9fr4hbp1ql1ah06s0v6dy7a56d1zl3jjjljhrhn";
   };
 
   enableParallelBuilding = true;
 
-  nativeBuildInputs = [ cmake pkgconfig gettext libcsptr dyncall nanomsg ];
+  nativeBuildInputs = [ meson ninja pkgconfig gettext libcsptr dyncall nanomsg ];
+  buildInputs = [ (python3.withPackages (ps: with ps; [ cram ])) ];
 
-  cmakeFlags = [ "-DBXF_TESTS=OFF" "-DBXF_SAMPLES=OFF" "-DBXF_FORK_RESILIENCE=OFF" ];
-  doCheck = false;
+  postPatch = ''
+    patchShebangs ci/isdir.py
+  '';
+
+  doCheck = true;
+
+  # cmakeFlags = [ "-DBXF_TESTS=OFF" "-DBXF_SAMPLES=OFF" "-DBXF_FORK_RESILIENCE=OFF" ];
+  # doCheck = false;
 
   meta = with stdenv.lib; {
     description = "Convenient & cross-platform sandboxing C library";
