8c3d75b466438c8eca77933947402ec9ece17551
wucke13 <wucke13@gmail.com>
2019-10-04 16:22:20 +0200

    stm32cubemx: init at 5.3.0
    (cherry picked from commit f37c03470d2ce61487086da045c1b6bb6f5b59bd)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10303,6 +10303,8 @@ in
 
   sselp = callPackage ../tools/X11/sselp{ };
 
+  stm32cubemx = callPackage ../development/tools/misc/stm32cubemx { };
+
   stm32flash = callPackage ../development/tools/misc/stm32flash { };
 
   strace = callPackage ../development/tools/misc/strace { };
