dcf722191efaec35b253749c95205cde525ee8a2
Quentin Vaucher <quentin.vaucher@protonmail.com>
2019-10-06 11:40:23 +0200

    formatter: init at 0.3.0
    (cherry picked from commit b99357eabd8db12c24d58bb15ffa281d1eebbcb7)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19021,6 +19021,8 @@ in
 
   fomp = callPackage ../applications/audio/fomp { };
 
+  formatter = callPackage ../applications/misc/formatter { };
+
   fractal = callPackage ../applications/networking/instant-messengers/fractal { };
 
   freecad = qt5.callPackage ../applications/graphics/freecad { mpi = openmpi; };
