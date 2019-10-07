cd1485be71690d898ae8fb5a7a66a3c7e896a87d
Will Dietz <w@wdtz.org>
2019-10-07 14:20:20 -0500

    libsForQt5.kpmcore: 4.0.0 -> 4.0.1

--- pkgs/development/libraries/kpmcore/default.nix
+++ pkgs/development/libraries/kpmcore/default.nix
@@ -5,24 +5,13 @@
 
 let
   pname = "kpmcore";
-  # kpmcore needs 2.32.2 and presently our version is 2.32.1,
-  # so override it locally until we've updated it system-wide.
-  utillinuxNewer = utillinux.overrideAttrs(o: rec {
-    name = "util-linux-${version}";
-    version = "2.33.2";
-    src = fetchurl {
-      url = "mirror://kernel/linux/utils/util-linux/v2.33/util-linux-2.33.2.tar.xz";
-      sha256 = "15yf2dh4jd1kg6066hydlgdhhs2j3na13qld8yx30qngqvmfh6v3";
-    };
-  });
-
 in stdenv.mkDerivation rec {
   name = "${pname}-${version}";
-  version = "4.0.0";
+  version = "4.0.1";
 
   src = fetchurl {
     url = "mirror://kde/stable/${pname}/${version}/src/${name}.tar.xz";
-    sha256 = "0vfz9pr9n6p9hs3d9cm8yirp9mkw76nhnin55v3bwsb34p549w6p";
+    sha256 = "1sslkwcj2cyrn7bpjdjdwikp1q8wrsxpsg2sxxd8hsairgy7ygh3";
   };
 
   buildInputs = [
@@ -33,7 +22,7 @@ in stdenv.mkDerivation rec {
     kdeFrameworks.kauth
     kdeFrameworks.kio
 
-    utillinuxNewer # needs blkid (note that this is not provided by utillinux-compat)
+    utillinux
   ];
   nativeBuildInputs = [ extra-cmake-modules ];
   enableParallelBuilding = true;
