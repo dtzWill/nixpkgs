476c34c6b7d212d6d5932aad40ddb62a5229c42a
Will Dietz <w@wdtz.org>
2019-10-07 20:49:30 -0500

    xdg-desktop-potral-kde: epoxy

--- pkgs/desktops/plasma-5/xdg-desktop-portal-kde.nix
+++ pkgs/desktops/plasma-5/xdg-desktop-portal-kde.nix
@@ -2,14 +2,14 @@
   mkDerivation,
   extra-cmake-modules, gettext, kdoctools, python,
   kcoreaddons, knotifications, kwayland, kwidgetsaddons, kwindowsystem,
-  cups, pcre, pipewire, kio
+  cups, pcre, pipewire, kio, epoxy
 }:
 
 mkDerivation {
   name = "xdg-desktop-portal-kde";
   nativeBuildInputs = [ extra-cmake-modules gettext kdoctools python ];
   buildInputs = [
-    cups pcre pipewire kio
+    cups pcre pipewire kio epoxy
     kcoreaddons knotifications kwayland kwidgetsaddons kwindowsystem
   ];
 }
