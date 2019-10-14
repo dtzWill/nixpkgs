354a9ac27f373b1a61eacc3c8a04f50c84c8b184
Will Dietz <w@wdtz.org>
2019-10-14 15:21:39 -0500

    Revert "nix-du: 0.3.0 -> 0.3.1"
    This reverts commit 25d487bbc6fc27bf89c52ce50d8b242497f2ed67.

--- pkgs/tools/package-management/nix-du/default.nix
+++ pkgs/tools/package-management/nix-du/default.nix
@@ -1,16 +1,15 @@
 { stdenv, fetchFromGitHub, rustPlatform, nix, boost, graphviz, darwin }:
 rustPlatform.buildRustPackage rec {
   pname = "nix-du";
-  version = "0.3.1";
+  version = "unstable-2019-09-30";
 
   src = fetchFromGitHub {
     owner = "symphorien";
     repo = "nix-du";
-    rev = "v${version}";
-    sha256 = "149d60mid29s5alv5m3d7jrhyzc6cj7b6hpiq399gsdwzgxr00wq";
+    rev = "2601d1fcdc9752c025a87687e3e5b01ba78e3a72";
+    sha256 = "1m4cn18f10gwjywxnc3i9rq5w08ghbdi958wqd2w6242kyyj8qk3";
   };
-  cargoSha256 = "18kb4car5nzch3vpl6z1499silhs3fyn8c6xj3rzk94mm2m9srg4";
-  verifyCargoDeps = true;
+  cargoSha256 = "071gbhxbvnwi7n3zpy7bmlprzir0sl0f0pb191xg2ynw678prd7v";
 
   doCheck = true;
   checkInputs = [ graphviz ];
