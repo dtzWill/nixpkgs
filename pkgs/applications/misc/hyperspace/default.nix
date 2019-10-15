1fa91885b3943c8676f2c8800f6dd922b0be403d
Will Dietz <w@wdtz.org>
2019-10-15 10:08:33 -0500

    hyperspace: try packaging via .deb instead of appimage? wip, crashes :(

--- pkgs/applications/misc/hyperspace/default.nix
+++ pkgs/applications/misc/hyperspace/default.nix
@@ -1,24 +1,48 @@
-{ appimageTools, fetchurl, lib, gsettings-desktop-schemas, gtk3 }:
+{ stdenv, fetchurl, lib
+, autoPatchelfHook, wrapGAppsHook, dpkg
+, atomEnv /* XXX: lazy */
+}:
 
-let
+stdenv.mkDerivation rec {
   pname = "hyperspace";
   version = "1.0.0";
-in appimageTools.wrapType2 rec {
-  name = "${pname}-${version}";
   src = fetchurl {
-    url = "https://github.com/hyperspacedev/hyperspace/releases/download/v${version}/hyperspace-${version}.AppImage";
-    sha256 = "1j25bqralmnc2bjzhs6v5il97njnksd3aid07bsk6bx9lpbyksmf";
+    url = "https://github.com/hyperspacedev/${pname}/releases/download/v${version}/${pname}_${version}_amd64.deb";
+    sha256 = "09j54jjajsdwrc38xm00ydf5k4i3a7ib1pdzswb0lx68h89cdcaj";
   };
 
-  profile = ''
-    export LC_ALL=C.UTF-8
-    export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
+  nativeBuildInputs = [
+    autoPatchelfHook
+    wrapGAppsHook
+    dpkg
+  ];
+
+  unpackPhase = ''
+    dpkg-deb -vx $src .
   '';
 
-  multiPkgs = null; # no 32bit needed
-  extraPkgs = appimageTools.defaultFhsEnvArgs.multiPkgs;
-  extraInstallCommands = "mv $out/bin/{${name},${pname}}";
-  # TODO: meta
-  #meta = with lib; {
-  #};
+  installPhase = ''
+    mkdir -p $out
+
+    # /usr/share
+    mv ./usr/* $out/
+
+    # /opt/Hyperspace
+    mv ./opt/Hyperspace $out/share/hyperspace
+
+    mkdir -p $out/bin
+    ln -s $out/share/hyperspace/hyperspace $out/bin/hyperspace
+  '';
+
+  buildInputs = /* FIXME: don't be lazy */ atomEnv.packages;
+
+  dontBuild = true;
+  dontStrip = true;
+  dontPatchELF = true;
+
+  meta = with lib; {
+    homepage = "https://hyperspace.marquiskurt.net";
+    # license = # XXX: "non-violent" ?!
+    maintainers = with maintainers; [ dtzWill ];
+  };
 }
