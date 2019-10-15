56f623d96e0f5676b45d3c0f6a74681c8ba47753
Will Dietz <w@wdtz.org>
2019-10-15 13:55:21 -0500

    mesa: 19.2.0 -> 19.2.1
    https://www.mesa3d.org/relnotes/19.2.1.html

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -27,7 +27,7 @@
 with stdenv.lib;
 
 let
-  version = "19.2.0";
+  version = "19.2.1";
   branch  = head (splitString "." version);
 in
 
@@ -42,7 +42,7 @@ stdenv.mkDerivation rec {
       "ftp://ftp.freedesktop.org/pub/mesa/older-versions/${branch}.x/${version}/mesa-${version}.tar.xz"
       "https://mesa.freedesktop.org/archive/mesa-${version}.tar.xz"
     ];
-    sha256 = "0al5235xrsv5walyavn192mg4n4g1rykgxqg2qqn918gl2iclq5h";
+    sha256 = "1s81kwcjkkahnf5y5mshmd3q9j057hhsai7awpq6yb6im2hkriac";
   };
 
   prePatch = "patchShebangs .";
