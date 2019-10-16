15c168f9b1cea929c658f620415524ac7f3ad1f6
worldofpeace <worldofpeace@protonmail.ch>
2019-10-16 01:57:23 +0000

    Merge pull request #67670 from kampka/zsh-history
    zsh-history: init at unstable-2019-08-29
    (cherry picked from commit fce8b4837ae1eb74aef36968b93e9776f8715154)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7357,6 +7357,8 @@ in
 
   zsh-git-prompt = callPackage ../shells/zsh/zsh-git-prompt { };
 
+  zsh-history = callPackage ../shells/zsh/zsh-history { };
+  
   zsh-history-substring-search = callPackage ../shells/zsh/zsh-history-substring-search { };
 
   zsh-navigation-tools = callPackage ../tools/misc/zsh-navigation-tools { };
