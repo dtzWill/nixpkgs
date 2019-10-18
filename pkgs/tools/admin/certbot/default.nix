3df8caf63b0be4699ec012c3db390b7bac362c52
Félix Baylac-Jacqué <felix@alternativebit.fr>
2019-10-18 19:11:49 +0200

    certbot: 0.31.0 -> 0.39.0
    Updating:
    
    - nixos module to use the new `account_reg.json` file.
    - use nixpkgs pebble for integration tests.
    
    Co-authored-by: Florian Klink <flokli@flokli.de>
    
    Replace certbot-embedded pebble
    
    (cherry picked from commit 38e84151e04f4b31dd729abb28db4159045bfd41)

--- pkgs/tools/admin/certbot/default.nix
+++ pkgs/tools/admin/certbot/default.nix
@@ -1,6 +1,7 @@
-{ stdenv, python3Packages, fetchFromGitHub, dialog }:
+{ stdenv, python37Packages, fetchFromGitHub, fetchurl, dialog, autoPatchelfHook, nginx, pebble }:
 
-python3Packages.buildPythonApplication rec {
+
+python37Packages.buildPythonApplication rec {
   pname = "certbot";
   version = "0.39.0";
 
@@ -11,7 +12,11 @@ python3Packages.buildPythonApplication rec {
     sha256 = "1s32xg2ljz7ci78wc8rqkjvgrz7vprb7fkznrlf9a4blm55pp54c";
   };
 
-  propagatedBuildInputs = with python3Packages; [
+  patches = [
+    ./0001-pebble_artifacts-hardcode-pebble-location.patch
+  ];
+
+  propagatedBuildInputs = with python37Packages; [
     ConfigArgParse
     acme
     configobj
@@ -27,11 +32,19 @@ python3Packages.buildPythonApplication rec {
     zope_component
     zope_interface
   ];
-  buildInputs = [ dialog ] ++ (with python3Packages; [ mock gnureadline ]);
 
-  patchPhase = ''
+  buildInputs = [ dialog ] ++ (with python37Packages; [ mock gnureadline ]);
+
+  checkInputs = with python37Packages; [
+    pytest_xdist
+    pytest
+    dateutil
+  ];
+
+  postPatch = ''
     substituteInPlace certbot/notify.py --replace "/usr/sbin/sendmail" "/run/wrappers/bin/sendmail"
     substituteInPlace certbot/util.py --replace "sw_vers" "/usr/bin/sw_vers"
+    substituteInPlace certbot-ci/certbot_integration_tests/utils/pebble_artifacts.py --replace "@pebble@" "${pebble}/bin/pebble"
   '';
 
   postInstall = ''
@@ -41,8 +54,15 @@ python3Packages.buildPythonApplication rec {
     done
   '';
 
-  checkInputs = with python3Packages; [ pytest pyyaml ];
-  doCheck = false /* integration tests want 'docker', not sure how to disable */;
+  # tests currently time out, because they're trying to do network access
+  # Upstream issue: https://github.com/certbot/certbot/issues/7450
+  doCheck = false;
+
+  checkPhase = ''
+    PATH="$out/bin:${nginx}/bin:$PATH" pytest certbot-ci/certbot_integration_tests
+  '';
+
+  dontUseSetuptoolsCheck = true;
 
   meta = with stdenv.lib; {
     homepage = src.meta.homepage;
