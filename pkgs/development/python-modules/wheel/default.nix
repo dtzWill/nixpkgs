e2f7ddaba2b924c619fa1fa8220d9c3a4467b9a6
Frederik Rietdijk <fridh@fridh.nl>
2019-10-20 15:33:35 +0200

    Python: setuptools/wheel/pip now bootstrap from source
    Since wheel support was introduced in 2015 we always relied on pre-built
    wheels for bootstrapping. Now, we can bootstrap directly from the
    sources of these packages in git.
    
    The `bootstrapped-pip` packages is used to build `pip`, `setuptools` and `wheel`,
    after which those packages are used to build everything else.
    
    Note that when building `bootstrapped-pip` some errors are shown.
    These are not important, the build actually does succeed and work as intended.
    
    (cherry picked from commit 56727dc1ff430bc2d39b08601e03094de4ab189e)

--- pkgs/development/python-modules/wheel/default.nix
+++ pkgs/development/python-modules/wheel/default.nix
@@ -2,7 +2,7 @@
 , setuptools
 , pip
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , pytest
 , pytestcov
 , coverage
@@ -12,12 +12,15 @@
 
 buildPythonPackage rec {
   pname = "wheel";
-  version = "0.33.4";
+  version = "0.33.6";
   format = "other";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "62fcfa03d45b5b722539ccbc07b190e4bfff4bb9e3a4d470dd9f6a0981002565";
+  src = fetchFromGitHub {
+    owner = "pypa";
+    repo = pname;
+    rev = version;
+    sha256 = "1bg4bxazsjxp621ymaykd8l75k7rvcvwawlipmjk7nsrl72l4p0s";
+    name = "${pname}-${version}-source";
   };
 
   checkInputs = [ pytest pytestcov coverage ];
