806d128908dff1287670bf77327a40681055261c
Malte Brandy <malte.brandy@maralorn.de>
2019-10-21 22:36:27 +0200

    nixos/redis: Disable hugepages for redis via sysctl and not via a systemd-oneshot
    (cherry picked from commit fe143bac5b8dbdbdc9332abc3f2acdf839652c9e)

--- nixos/modules/services/databases/redis.nix
+++ nixos/modules/services/databases/redis.nix
@@ -193,9 +193,9 @@ in
 
   config = mkIf config.services.redis.enable {
 
-    boot.kernel.sysctl = mkIf cfg.vmOverCommit {
-      "vm.overcommit_memory" = "1";
-    };
+    boot.kernel.sysctl = {
+      "vm.nr_hugepages" = "0";
+    } // mkIf cfg.vmOverCommit { "vm.overcommit_memory" = "1"; };
 
     networking.firewall = mkIf cfg.openFirewall {
       allowedTCPPorts = [ cfg.port ];
@@ -207,15 +207,6 @@ in
     };
 
     environment.systemPackages = [ cfg.package ];
-
-    systemd.services.disable-transparent-huge-pages = {
-      enable = config.services.redis.enable;
-      description = "Disable Transparent Huge Pages (required by Redis)";
-      before = [ "redis.service" ];
-      wantedBy = [ "redis.service" ];
-      script = "echo never >/sys/kernel/mm/transparent_hugepage/enabled";
-      serviceConfig.Type = "oneshot";
-    };
 
     systemd.services.redis =
       { description = "Redis Server";
