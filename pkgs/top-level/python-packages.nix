fadf657fc38f8681a6064ee0e9adce48fe72a5c4
David Pätzel <david.paetzel@informatik.uni-augsburg.de>
2019-10-21 08:09:02 +0200

    pythonPackages.pyside: inherit mesa from pkgs
    (cherry picked from commit 3db3c787d50f37536ab1fe6603759bd8db580aa1)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1006,7 +1006,9 @@ in {
 
   pyschedule = callPackage ../development/python-modules/pyschedule { };
 
-  pyside = callPackage ../development/python-modules/pyside { };
+  pyside = callPackage ../development/python-modules/pyside {
+    inherit (pkgs) mesa;
+  };
 
   pysideShiboken = callPackage ../development/python-modules/pyside/shiboken.nix {
     inherit (pkgs) libxml2 libxslt; # Do not need the Python bindings.
