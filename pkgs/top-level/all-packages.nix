ea882487d24b1743d685bea85fc41bd1893745f1
Matt Huszagh <huszaghmatt@gmail.com>
2019-10-20 17:53:07 -0700

    fparser: init at 4.5.1
    (cherry picked from commit fc66192b37c3e209879a99981140352566d521b7)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24566,6 +24566,8 @@ in
 
   caneda = libsForQt5.callPackage ../applications/science/electronics/caneda { };
 
+  fparser = callPackage ../applications/science/electronics/fparser { };
+
   geda = callPackage ../applications/science/electronics/geda {
     guile = guile_2_0;
   };
