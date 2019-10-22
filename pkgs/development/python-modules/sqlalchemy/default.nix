2c301c473428c48090cf89e472bc4ba90e6e3749
Will Dietz <w@wdtz.org>
2019-10-22 13:10:12 -0500

    python3Packages.sqlalchemy: grab upstream PR to fix tests w/sqlite 3.30+
    * PR is not yet accepted
    * PR may change in the future, intentionally fetching in a way that
      is prone to break should that happen, as that's worth knowing.
    
    For both of the above reasons, this isn't ready for nixpkgs proper yet.

--- pkgs/development/python-modules/sqlalchemy/default.nix
+++ pkgs/development/python-modules/sqlalchemy/default.nix
@@ -2,6 +2,7 @@
 , mock
 , pysqlite
 , pytest
+, fetchpatch
 }:
 
 buildPythonPackage rec {
@@ -12,6 +13,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "0v74sl9xr5llv1q14hsn0wa9bwjcs5rjqmz1yphifiarvfsnh1qg";
   };
+
+  patches = [
+    # fix two tests started failing w/sqlite 3.30, using upstream PR (not merged!)
+    (fetchpatch {
+      url = "https://github.com/sqlalchemy/sqlalchemy/pull/4921.patch";
+      sha256 = "0y37kjsfz7j82ic9fvc5cg5fj0yayr4v5mz3q3pd15j0vbljpci9";
+    })
+  ];
 
   checkInputs = [
     pytest
