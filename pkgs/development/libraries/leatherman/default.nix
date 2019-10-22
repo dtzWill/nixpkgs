cf0cf768e84a076bc435571aff029f4588487fc9
R. RyanTM <ryantm-bot@ryantm.com>
2019-10-22 00:42:19 -0700

    leatherman: 1.7.2 -> 1.8.0
    Semi-automatic update generated by
    https://github.com/ryantm/nixpkgs-update tools. This update was made
    based on information from
    https://repology.org/metapackage/leatherman/versions
    
    (cherry picked from commit 8310e6430adf03c49cacf9164c43cbfd8fab6707)

--- pkgs/development/libraries/leatherman/default.nix
+++ pkgs/development/libraries/leatherman/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchFromGitHub, boost, cmake, curl, ruby }:
 
 stdenv.mkDerivation rec {
-  name = "leatherman-${version}";
-  version = "1.7.1";
+  pname = "leatherman";
+  version = "1.8.0";
 
   src = fetchFromGitHub {
-    sha256 = "0m2dm1gzwj0kwyl031bif89h3n4znml3m5n97hywlbra58ni8km1";
+    sha256 = "0iizy20pdkbnsfj6bzrkkj7faizc85lcpkpandvnxfybiq7j60iw";
     rev = version;
     repo = "leatherman";
     owner = "puppetlabs";
