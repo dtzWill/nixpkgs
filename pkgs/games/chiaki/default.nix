efc0cfbd9e76ce93d41853edee9b8ecd13ff52e1
Will Dietz <w@wdtz.org>
2019-10-22 12:50:55 -0500

    chiaki: use SDL2 for input again (default), now our SDL2 is fixed
    The cmake spaces/list issue broke using SDL2 here.
    
    Not sure what reasons there are for using qtgamepad but leave it
    as an option for now in case this doesn't work or for testing.

--- pkgs/games/chiaki/default.nix
+++ pkgs/games/chiaki/default.nix
@@ -1,6 +1,8 @@
 { lib, mkDerivation, fetchFromGitHub
-, cmake, ffmpeg, libopus, qtbase, qtmultimedia, qtsvg, qtgamepad, pkgconfig, protobuf
-, openssl, python3Packages, SDL2 }:
+, cmake, ffmpeg, libopus, qtbase, qtmultimedia, qtsvg , pkgconfig, protobuf
+, openssl, python3Packages
+, useQtGamePad ? false, qtgamepad ? null
+, SDL2 /* needed either way, I think? */ }:
 
 mkDerivation rec {
   pname = "chiaki";
@@ -17,11 +19,12 @@ mkDerivation rec {
   nativeBuildInputs = [
     cmake pkgconfig protobuf python3Packages.python python3Packages.protobuf
   ];
-  buildInputs = [ ffmpeg openssl libopus qtbase qtmultimedia qtsvg protobuf SDL2 qtgamepad ];
+  buildInputs = [ ffmpeg openssl libopus qtbase qtmultimedia qtsvg protobuf SDL2 ]
+    ++ lib.optional useQtGamePad qtgamepad;
 
   cmakeFlags = [
-    "-DCHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER=OFF"
-    "-DCHIAKI_GUI_ENABLE_QT_GAMEPAD=ON"
+    "-DCHIAKI_GUI_ENABLE_SDL_GAMECONTROLLER=${if useQtGamePad then "OFF" else "ON"}"
+    "-DCHIAKI_GUI_ENABLE_QT_GAMEPAD=${if useQtGamePad then "ON" else "OFF"}"
   ];
 
   doCheck = true;
