ff92c01ca56a8559de9f68590dd400ae29617487
Will Dietz <w@wdtz.org>
2019-10-23 10:13:16 -0500

    ell: git bump

--- pkgs/os-specific/linux/ell/default.nix
+++ pkgs/os-specific/linux/ell/default.nix
@@ -8,15 +8,15 @@
 stdenv.mkDerivation rec {
   pname = "ell";
   #version = "0.24";
-  version = "2019-10-21";
+  version = "2019-10-23";
 
   outputs = [ "out" "dev" ];
 
   src = fetchgit {
      url = "https://git.kernel.org/pub/scm/libs/${pname}/${pname}.git";
      #rev = version;
-     rev = "2af3691521288dd158002da1b7f773dde64ddb1c";
-     sha256 = "11a1a07fwn6pd95sfczqcv8al13gizx79hpqf6lq0fnwsvk8bflv";
+     rev = "0914606bb89cf55e02b60b150867b53876bea968";
+     sha256 = "12pin0iwf48kb6w93b5yd669dc4901fb0akk9x8kz9iarld8y59x";
   };
 
   patches = [
