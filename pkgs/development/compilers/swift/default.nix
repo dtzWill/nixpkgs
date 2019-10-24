42f7c95398bc342a2847d4bb195dbfa5e469be24
Will Dietz <w@wdtz.org>
2019-10-24 00:03:48 -0500

    swift: okay no icu

--- pkgs/development/compilers/swift/default.nix
+++ pkgs/development/compilers/swift/default.nix
@@ -242,6 +242,7 @@ stdenv.mkDerivation rec {
     patch -p1 -d swift -i ${./patches/nothing-too-fancy-yet.patch}
     patch -p1 -d swift -i ${./patches/0001-build-presets-linux-don-t-require-using-Ninja.patch}
     patch -p1 -d swift -i ${./patches/0002-build-presets-linux-allow-custom-install-prefix.patch}
+    patch -p1 -d swift -i ${./patches/0003-build-presets-linux-don-t-build-extra-libs.patch}
     patch -p1 -d swift -i ${./patches/0004-build-presets-linux-plumb-extra-cmake-options.patch}
 
     sed -i swift/utils/build-presets.ini \
