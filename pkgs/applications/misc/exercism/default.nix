7e9cca7b26bd15a0ab0d6728d811fcb05e51f9b8
Norbert Melzer <nmelzer@nobbz.dev>
2019-10-24 21:09:20 +0200

    exercism: 3.0.12 -> 3.0.13
    (cherry picked from commit f03715ca8673e63ec9d15bd23314e4fa4b4f2284)

--- pkgs/applications/misc/exercism/default.nix
+++ pkgs/applications/misc/exercism/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   pname = "exercism";
-  version = "3.0.12";
+  version = "3.0.13";
 
   goPackagePath = "github.com/exercism/cli";
 
@@ -10,7 +10,7 @@ buildGoPackage rec {
     owner  = "exercism";
     repo   = "cli";
     rev    = "v${version}";
-    sha256 = "1xvxcl7j5izx5lgmjd97zd28lg2sydwgbgn2cnisz5r0d27pj3ra";
+    sha256 = "17gvz9a0sn4p36hf4l77bxhhfipf4x998iay31layqwbnzmb4xy7";
   };
 
   goDeps = ./deps.nix;
