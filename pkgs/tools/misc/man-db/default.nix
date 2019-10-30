96213b0560d8d8b8284ec2500c1b48af9aa51cf1
Will Dietz <w@wdtz.org>
2019-10-30 11:24:48 -0500

    man-db: 2.8.6.1 -> 2.9.0, pname

--- pkgs/tools/misc/man-db/default.nix
+++ pkgs/tools/misc/man-db/default.nix
@@ -1,11 +1,12 @@
 { stdenv, fetchurl, pkgconfig, libpipeline, db, groff, libiconv, makeWrapper, buildPackages }:
 
 stdenv.mkDerivation rec {
-  name = "man-db-2.8.6.1";
+  pname = "man-db";
+  version = "2.9.0";
 
   src = fetchurl {
-    url = "mirror://savannah/man-db/${name}.tar.xz";
-    sha256 = "0a1sh5gxa16k6irzf3q2lli8m204w9ik1xm62wjgf1mzknxs4xrc";
+    url = "mirror://savannah/man-db/${pname}-${version}.tar.xz";
+    sha256 = "0qg2sdn8mayya0ril484iz1r7hi46l68d2d80cr6lvc7x3csqjjx";
   };
 
   outputs = [ "out" "doc" ];
