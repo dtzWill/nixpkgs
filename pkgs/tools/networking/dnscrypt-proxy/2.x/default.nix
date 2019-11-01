0cd4cb61cf8a686a577a76ce9b82915ff5d0cc0e
Will Dietz <w@wdtz.org>
2019-10-31 19:37:05 -0500

    dnscrypt-proxy2: 2.0.29 -> 2.0.31

--- pkgs/tools/networking/dnscrypt-proxy/2.x/default.nix
+++ pkgs/tools/networking/dnscrypt-proxy/2.x/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   pname = "dnscrypt-proxy2";
-  version = "2.0.29";
+  version = "2.0.31";
 
   goPackagePath = "github.com/jedisct1/dnscrypt-proxy";
 
@@ -10,7 +10,7 @@ buildGoPackage rec {
     owner = "jedisct1";
     repo = "dnscrypt-proxy";
     rev = version;
-    sha256 = "196yc34xw6h9qpns4n1fw8bjh8lv4xcsbx89i391qw92h2a3qkmc";
+    sha256 = "1izhjdb7vns30gplfp3pi1rkdv04s5nbf6yrjr3nv95pf6m1skka";
   };
 
   meta = with stdenv.lib; {
