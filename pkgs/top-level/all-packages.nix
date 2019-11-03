506523e47bd2380d88dbab805ed150c653690a12
Will Dietz <w@wdtz.org>
2019-11-03 13:12:23 -0600

    Revert "inkscape: use latest poppler, use gtkspell2, drop `-fpermissive`"
    poppler in this tree is too new, maybe? :(
    
    This reverts commit 5eded2a35a413b8d2c0b4d0d537e7458bdb2b823.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19429,6 +19429,7 @@ in
 
   inkscape = callPackage ../applications/graphics/inkscape {
     lcms = lcms2;
+    poppler = poppler_0_61;
   };
 
   inspectrum = libsForQt5.callPackage ../applications/radio/inspectrum { };
