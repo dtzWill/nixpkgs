bda8a2bcab6b06a1ea23de2219d49b67668503e7
Jan Hrnko <jan.hrnko@satoshilabs.com>
2019-11-03 16:43:57 +0100

    dokuwiki: init at stable_2018-04-22b
    (cherry picked from commit 4792479a4133faa6998acdd417931d0cac4d40cc)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1582,6 +1582,8 @@ in
 
   doitlive = callPackage ../tools/misc/doitlive { };
 
+  dokuwiki = callPackage ../servers/web-apps/dokuwiki { };
+
   dosage = callPackage ../applications/graphics/dosage {
     pythonPackages = python3Packages;
   };
