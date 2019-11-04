381d69434b67048cc10f3a465e19f11ff3ad6ede
Will Dietz <w@wdtz.org>
2019-11-04 16:50:03 -0600

    iwd: fix test failure caused by reversed assert condition

--- pkgs/os-specific/linux/iwd/default.nix
+++ pkgs/os-specific/linux/iwd/default.nix
@@ -64,6 +64,9 @@ stdenv.mkDerivation rec {
     # Fix write past end of buffer in certain circumstances
     # ... circumstances I encounter often, thanks neighbors ;)
     ./completion-crash-fix-wip.patch
+
+    # Fix assert broken in recent refactoring
+    ./fix-inverted-assert.patch
   ];
 
   doCheck = true;
