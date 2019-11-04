755bcdb1eac58db018c1a23e84d6c5d95afdda34
Will Dietz <w@wdtz.org>
2019-11-04 07:48:28 -0600

    Revert "Revert "llvm_{7,8,9}: py -> py3" (rebuilds, later)"
    This reverts commit 4de6a52cebd1cd1d1e21a60dee491d51638e6179.

--- pkgs/development/compilers/llvm/7/llvm.nix
+++ pkgs/development/compilers/llvm/7/llvm.nix
@@ -2,7 +2,7 @@
 , fetch
 , fetchpatch
 , cmake
-, python
+, python3
 , libffi
 , libbfd
 , libpfm
@@ -47,8 +47,8 @@ in stdenv.mkDerivation (rec {
   outputs = [ "out" "python" ]
     ++ optional enableSharedLibraries "lib";
 
-  nativeBuildInputs = [ cmake python ]
-    ++ optional enableManpages python.pkgs.sphinx;
+  nativeBuildInputs = [ cmake python3 ]
+    ++ optional enableManpages python3.pkgs.sphinx;
 
   buildInputs = [ libxml2 libffi ]
     ++ optional enablePFM libpfm; # exegesis
