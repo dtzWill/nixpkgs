b34d4d1af9b03b3668b658a6c9e6e6a8530a8776
Robin Gloster <mail@glob.in>
2019-11-04 22:14:38 +0100

    Merge pull request #71266 from philandstuff/add-gnupg-pkcs11-scd
    gnupg-pkcs11-scd: init at 0.9.2
    (cherry picked from commit fa7d7eb95bf1acec2b89140a6d1743e08510892a)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3551,6 +3551,8 @@ in
   };
   gnupg = gnupg22;
 
+  gnupg-pkcs11-scd = callPackage ../tools/security/gnupg-pkcs11-scd { };
+
   gnuplot = libsForQt5.callPackage ../tools/graphics/gnuplot { };
 
   gnuplot_qt = gnuplot.override { withQt = true; };
