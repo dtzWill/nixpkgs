210a53c2537e0ed07b44f116caf9f228ea15ce6b
Will Dietz <w@wdtz.org>
2019-11-04 20:59:17 -0600

    granite: drop patch no longer needed and no longer applies

--- pkgs/desktops/pantheon/granite/default.nix
+++ pkgs/desktops/pantheon/granite/default.nix
@@ -1,6 +1,5 @@
 { stdenv
 , fetchFromGitHub
-, fetchpatch
 , python3
 , meson
 , ninja
@@ -25,15 +24,6 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "0z40vhcp2w8s8rnc56pzvjc4s77bln8k84rwwypivjmk3lhpw1vi";
   };
-
-  patches = [
-    # Resolve the circular dependency between granite and the datetime wingpanel indicator
-    # See: https://github.com/elementary/granite/pull/242
-    (fetchpatch {
-      url = "https://src.fedoraproject.org/rpms/granite/raw/0550b44ed6400c9b1ff7e70871913747df2ff323/f/00-datetime-clock-format-gsettings.patch";
-      sha256 = "0i9yvdmn77x5fjdwd1raw6ym8js8yxa7w6ydc7syx7hcyls00dmq";
-    })
-  ];
 
   passthru = {
     updateScript = pantheon.updateScript {
