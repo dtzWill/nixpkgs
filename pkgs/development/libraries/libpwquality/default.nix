61dba945dcafef54186584ab466c22bf0434b8a2
Will Dietz <w@wdtz.org>
2019-11-05 22:58:05 -0600

    Revert "Revert "libpwquality: 1.4.1 -> 1.4.2""
    This reverts commit 4aa8187fc1b017807b8985154dc3228272015eea.

--- pkgs/development/libraries/libpwquality/default.nix
+++ pkgs/development/libraries/libpwquality/default.nix
@@ -1,14 +1,14 @@
 { stdenv, lib, fetchFromGitHub, autoreconfHook, perl, cracklib, python }:
 
 stdenv.mkDerivation rec {
-  name = "libpwquality-${version}";
-  version = "1.4.1";
+  pname = "libpwquality";
+  version = "1.4.2";
 
   src = fetchFromGitHub {
     owner = "libpwquality";
     repo = "libpwquality";
-    rev = name;
-    sha256 = "150gk1d0gq9cig3ylyns7fgihgm3qb1basncahgyh1kzxplrdqm7";
+    rev = "${pname}-${version}";
+    sha256 = "0n4pjhm7wfivk0wizggaxq4y4mcxic876wcarjabkp5z9k14y36h";
   };
 
   nativeBuildInputs = [ autoreconfHook perl ];
