40c37bbd67f53c0d70cd72bf7167724d20dc5404
Will Dietz <w@wdtz.org>
2019-11-05 23:19:06 -0600

    nixpkgs-fmt: 0.6.0 -> 0.6.1

--- pkgs/tools/nix/nixpkgs-fmt/default.nix
+++ pkgs/tools/nix/nixpkgs-fmt/default.nix
@@ -1,16 +1,16 @@
 { lib, rustPlatform, fetchFromGitHub }:
 rustPlatform.buildRustPackage rec {
   pname = "nixpkgs-fmt";
-  version = "0.6.0";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "nix-community";
     repo = pname;
     rev = "v${version}";
-    sha256 = "18kvsgl3kpla33dp1nbpd1kdgndfqcmlwwpjls55fp4mlczf8lcx";
+    sha256 = "1iylldgyvrcarfigpbhicg6j6qyipfiqn7gybza7qajfzyprjqfa";
   };
 
-  cargoSha256 = "18kr9ad3y1f557vq95af8nvxgb0zhaq10yvr8phlxv9kb7ldbphl";
+  cargoSha256 = "04my7dlp76dxs1ydy2sbbca8sp3n62wzdxyc4afcmrg8anb0ghaf";
 
   meta = with lib; {
     description = "Nix code formatter for nixpkgs";
