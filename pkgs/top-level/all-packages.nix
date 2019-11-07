7334c52d2e7c1c263f10789ea3ed2c3694ef35df
Matthew Bauer <mjbauer95@gmail.com>
2019-11-07 17:34:43 -0500

    Merge pull request #68949 from adsr/feature/mle
    mle: init at 1.4.1
    (cherry picked from commit 1cee79c4dc5953c13d4d19fa2d5ffa7e0920b49e)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20367,6 +20367,8 @@ in
 
   micro = callPackage ../applications/editors/micro { };
 
+  mle = callPackage ../applications/editors/mle { };
+
   nano = callPackage ../applications/editors/nano { };
 
   nanoblogger = callPackage ../applications/misc/nanoblogger { };
