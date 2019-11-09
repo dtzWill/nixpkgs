69e7c4b9dda6b408bd7b9522bfa6be963f2903a6
Mario Rodas <marsam@users.noreply.github.com>
2019-11-09 04:20:00 -0500

    nushell: init at 0.5.0
    (cherry picked from commit 10246893188f6aff33a251f7febdf04a3c4e5b8b)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16724,6 +16724,10 @@ in
 
   musl = callPackage ../os-specific/linux/musl { };
 
+  nushell = callPackage ../shells/nushell {
+    inherit (darwin.apple_sdk.frameworks) Security;
+  };
+
   nettools = if stdenv.isLinux then callPackage ../os-specific/linux/net-tools { }
              else unixtools.nettools;
 
