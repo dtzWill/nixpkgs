ead3eacab7dd8318eaa6eced86772103346d5d63
Will Dietz <w@wdtz.org>
2019-11-15 17:30:02 -0600

    spleen: consolefonts

--- pkgs/data/fonts/spleen/default.nix
+++ pkgs/data/fonts/spleen/default.nix
@@ -12,13 +12,16 @@ in fetchurl rec {
   postFetch = ''
     tar xvf $downloadedFile --strip=1
     d="$out/share/fonts/X11/misc/spleen"
-    install -Dm644 *.{pcf.gz,psfu,bdf} -t $d
+    install -Dm644 *.{pcf.gz,bdf} -t $d
     install -m644 fonts.alias-spleen $d/fonts.alias
 
     # create fonts.dir so NixOS xorg module adds to fp
     ${mkfontdir}/bin/mkfontdir $d
+
+    # psfu -> consolefonts
+    install -Dm644 *.psfu -t $out/share/consolefonts
   '';
-  sha256 = "13ndyw1m6c778k7jr7qwbfg28gqmpcgksprn2mjgsv75wvy51z19";
+  sha256 = "0rjlnpklx58sma3b9pvw90s31ya5dk7cynk4ihgs7fh458ijjrcx";
 
   meta = with lib; {
     description = "Monospaced bitmap fonts";
