e3a58a0fe4de3d5d56f5b936b71ebba05a11833b
Will Dietz <w@wdtz.org>
2019-11-16 00:08:47 -0600

    font-manager: unstable-2019-10-02 -> unstable-2019-11-12

--- pkgs/applications/misc/font-manager/default.nix
+++ pkgs/applications/misc/font-manager/default.nix
@@ -6,14 +6,14 @@
 stdenv.mkDerivation rec {
   pname = "font-manager";
 #  version = "0.7.5";
-  version = "unstable-2019-10-02";
+  version = "unstable-2019-11-12";
 
   src = fetchFromGitHub {
     owner = "FontManager";
     repo = pname;
     #rev = version;
-    rev = "14519aacba5590183db5e342b3f2b8f2dbfae836";
-    sha256 = "12pk279hyr0xzd09lfcdf8w2sbnzd76rgap5qwlwx846yzgds6s1";
+    rev = "47dad5497236d7a5ef80f33e58ac2f3894295590";
+    sha256 = "0v7gmrah5rg8n63xikzyrr4vas9jkqg9042cga3isnrj20hn5mbx";
   };
 
   nativeBuildInputs = [
