40d6a571970caa3bb3e96d9d0c491d9f765cb5df
Will Dietz <w@wdtz.org>
2019-11-16 07:20:01 -0600

    public-sans: 1.006 -> 1.007
    https://github.com/uswds/public-sans/releases/tag/v1.007

--- pkgs/data/fonts/public-sans/default.nix
+++ pkgs/data/fonts/public-sans/default.nix
@@ -1,18 +1,19 @@
 { lib, fetchzip }:
 
 let
-  version = "1.006";
+  version = "1.007";
 in fetchzip rec {
   name = "public-sans-${version}";
 
   url = "https://github.com/uswds/public-sans/releases/download/v${version}/public-sans-v${version}.zip";
 
   postFetch = ''
-    mkdir -p $out/share
-    unzip $downloadedFile fonts/{otf,variable}/\*.\[ot\]tf -d $out/share/
+    mkdir -p $out/share/fonts/{opentype,truetype}
+    unzip -j $downloadedFile binaries/otf/\*.otf -d $out/share/fonts/opentype
+    unzip -j $downloadedFile binaries/variable/\*.ttf -d $out/share/fonts/truetype
   '';
 
-  sha256 = "1x04mpynfhcgiwx68w5sawgn69xld7k65mbq7n5vcgbfzh2sjwhq";
+  sha256 = "0j9kwq80km0ai3gipzq5h66y6crkqczbfdh9s9mgx8h2n0vaivfa";
 
   meta = with lib; {
     description = "A strong, neutral, principles-driven, open source typeface for text or display";
