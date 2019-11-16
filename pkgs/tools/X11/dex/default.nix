bf199ccf451f794fc457c797608bf45bb1b95f3d
Will Dietz <w@wdtz.org>
2019-11-16 09:21:35 -0600

    dex: pick upstream commits for --term and --wait

--- pkgs/tools/X11/dex/default.nix
+++ pkgs/tools/X11/dex/default.nix
@@ -18,8 +18,19 @@ stdenv.mkDerivation rec {
 
   patches = [
     (fetchpatch {
+      name = "fix-version-number.patch";
       url = https://github.com/jceb/dex/commit/107358ddf5e1ca4fa56ef1a7ab161dc3b6adc45a.patch;
       sha256 = "06dfkfzxp8199by0jc5wim8g8qw38j09dq9p6n9w4zaasla60pjq";
+    })
+    (fetchpatch {
+      name = "add-term-option.patch";
+      url = https://github.com/jceb/dex/commit/e183bc04f7afe719ba3fc137604f2cdf08e03c22.patch;
+      sha256 = "1krzlxpy6qwh0agmxn2k2jslzs9pl2dqwg6g0ap10ryw9zxdynz5";
+    })
+    (fetchpatch {
+      name = "add-wait-option.patch";
+      url = https://github.com/jceb/dex/commit/cfbb76cd2ff32b9c821b91b5372a1770d6ac300a.patch;
+      sha256 = "1ghmgzd9r1jg1hva5dxcxav9ynzsmyka3pnqndknpngkqb0vnf0s";
     })
   ];
 
