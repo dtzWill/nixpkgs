a998dfd1c8ff63749a2f8d579778c368f28c1baf
Will Dietz <w@wdtz.org>
2019-11-17 14:36:13 -0600

    alsa*: revert for now, rebuilds

--- pkgs/os-specific/linux/alsa-firmware/default.nix
+++ pkgs/os-specific/linux/alsa-firmware/default.nix
@@ -1,16 +1,15 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation rec {
-  pname = "alsa-firmware";
-  version = "1.2.1";
+  name = "alsa-firmware-1.0.29";
 
   src = fetchurl {
-    url = "mirror://alsa/firmware/${pname}-${version}.tar.bz2";
-    sha256 = "1aq8z8ajpjvcx7bwhwp36bh5idzximyn77ygk3ifs0my3mbpr8mf";
+    url = "mirror://alsa/firmware/${name}.tar.bz2";
+    sha256 = "0gfcyj5anckjn030wcxx5v2xk2s219nyf99s9m833275b5wz2piw";
   };
 
   configureFlags = [
-    "--with-hotplug-dir=${placeholder "out"}/lib/firmware"
+    "--with-hotplug-dir=$(out)/lib/firmware"
   ];
 
   dontStrip = true;
@@ -18,15 +17,17 @@ stdenv.mkDerivation rec {
   postInstall = ''
     # These are lifted from the Arch PKGBUILD
     # remove files which conflicts with linux-firmware
-    rm -rvf $out/lib/firmware/{ct{efx,speq}.bin,ess,korg,sb16,yamaha}
+    rm -rf $out/lib/firmware/{ct{efx,speq}.bin,ess,korg,sb16,yamaha}
     # remove broken symlinks (broken upstream)
-    rm -rvf $out/lib/firmware/turtlebeach
+    rm -rf $out/lib/firmware/turtlebeach
+    # remove empty dir
+    rm -rf $out/bin
   '';
 
   meta = {
     homepage = http://www.alsa-project.org/;
     description = "Soundcard firmwares from the alsa project";
-    license = stdenv.lib.licenses.gpl2;
+    license = stdenv.lib.licenses.gpl2Plus;
     platforms = stdenv.lib.platforms.linux;
   };
 }
