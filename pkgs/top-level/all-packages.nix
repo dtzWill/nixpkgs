fdfad5b924069385bd399fbd93d7559f230c0734
Janne Heß <janne@hess.ooo>
2019-11-17 19:56:39 +0100

    geoipupdate: Init at 4.1.5
    (cherry picked from commit f06ca051e2e845b7a415117f628d38fa84f4736d)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18781,6 +18781,8 @@ in
   geany = callPackage ../applications/editors/geany { };
   geany-with-vte = callPackage ../applications/editors/geany/with-vte.nix { };
 
+  geoipupdate = callPackage ../applications/misc/geoipupdate/default.nix { };
+
   ghostwriter = libsForQt5.callPackage ../applications/editors/ghostwriter { };
 
   gitweb = callPackage ../applications/version-management/git-and-tools/gitweb { };
