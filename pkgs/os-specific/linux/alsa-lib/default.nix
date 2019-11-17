2834330b502ea5986147b7cfcc582bf7b8958107
Will Dietz <w@wdtz.org>
2019-11-17 14:33:38 -0600

    alsaLib: remove fixups, maybe not needed (REVISIT!)

--- pkgs/os-specific/linux/alsa-lib/default.nix
+++ pkgs/os-specific/linux/alsa-lib/default.nix
@@ -13,16 +13,6 @@ stdenv.mkDerivation rec {
     ./alsa-plugin-conf-multilib.patch
   ];
 
-  ## # Fix pcm.h file in order to prevent some compilation bugs
-  ## # 2: see http://stackoverflow.com/questions/3103400/how-to-overcome-u-int8-t-vs-uint8-t-issue-efficiently
-  ## postPatch = ''
-  ##   sed -i -e 's|//int snd_pcm_mixer_element(snd_pcm_t \*pcm, snd_mixer_t \*mixer, snd_mixer_elem_t \*\*elem);|/\*int snd_pcm_mixer_element(snd_pcm_t \*pcm, snd_mixer_t \*mixer, snd_mixer_elem_t \*\*elem);\*/|' include/pcm.h
-
-
-  ##   sed -i -e '1i#include <stdint.h>' include/pcm.h
-  ##   sed -i -e 's/u_int\([0-9]*\)_t/uint\1_t/g' include/pcm.h
-  ## '';
-
   outputs = [ "out" "dev" ];
 
   meta = with stdenv.lib; {
