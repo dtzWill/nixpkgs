34822fdd21479f8983cae791e9471b0cd45300ee
Will Dietz <w@wdtz.org>
2019-11-17 09:55:52 -0600

    gusb: 0.3.1

--- pkgs/development/libraries/gusb/default.nix
+++ pkgs/development/libraries/gusb/default.nix
@@ -1,23 +1,17 @@
-{ stdenv, fetchurl, fetchFromGitHub, meson, ninja, pkgconfig, gettext, gobject-introspection
+{ stdenv, fetchurl, meson, ninja, pkgconfig, gettext, gobject-introspection
 , gtk-doc, docbook_xsl, docbook_xml_dtd_412, docbook_xml_dtd_44
 , glib, systemd, libusb1, vala, hwdata
 }:
 stdenv.mkDerivation rec {
   pname = "gusb";
-  version = "0.3.1-git"; # git for recent fwupd usage
+  version = "0.3.1";
 
   outputs = [ "bin" "out" "dev" "devdoc" ];
 
-  src = fetchFromGitHub {
-    owner = "hughsie";
-    repo = "lib${pname}";
-    rev = "13233c38eed954baf0ca3a95a23bd28ae02c8ba7";
-    sha256 = "0jix7dnk8j19kkf932zi34b6fjgiadnsm1ygln3343vabl9jvw12";
+  src = fetchurl {
+    url = "https://people.freedesktop.org/~hughsient/releases/libgusb-${version}.tar.xz";
+    sha256 = "1v8zhlf70nbiggkn0nbc6zj53l52dl3sqgdvn5q8jx4p5qvplrsb";
   };
-  #src = fetchurl {
-  #  url = "https://people.freedesktop.org/~hughsient/releases/libgusb-${version}.tar.xz";
-  #  sha256 = "1p4f6jdjw6zl986f93gzdjg2hdcn5dlz6rcckcz4rbmnk47rbryq";
-  #};
 
   nativeBuildInputs = [
     meson ninja pkgconfig gettext
