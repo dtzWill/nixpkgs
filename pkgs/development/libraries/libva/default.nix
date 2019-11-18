d865c8fef2dde2bd4cbe3a7e046c9ceb3ba99821
Will Dietz <w@wdtz.org>
2019-11-18 08:45:00 -0600

    libva: git bump to fix iHD

--- pkgs/development/libraries/libva/default.nix
+++ pkgs/development/libraries/libva/default.nix
@@ -6,14 +6,15 @@
 
 stdenv.mkDerivation rec {
   name = "libva-${lib.optionalString minimal "minimal-"}${version}";
-  version = "2.6.0.pre1";
+  version = "2.6.0.pre1-git";
 
   # update libva-utils and vaapiIntel as well
   src = fetchFromGitHub {
     owner  = "01org";
     repo   = "libva";
-    rev    = version;
-    sha256 = "1vlvk0wyzrj2qnvkzafs78164z5dmblcw8in6wd5w9b20g62vhp5";
+    #rev    = version;
+    rev = "09c8c2e20b7763fc44299172dbb16061b57f9c71";
+    sha256 = "0spa6j7yp58bxqfrm344y6891kvcsxzg74a9rhyfwwbw8hbj2j8p";
   };
 
   outputs = [ "dev" "out" ];
