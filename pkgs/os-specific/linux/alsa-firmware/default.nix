8b749326d26df1b69f1339e290f5108853558704
Will Dietz <w@wdtz.org>
2019-11-18 07:57:29 -0600

    Revert "alsa*: revert for now, rebuilds"
    This reverts commit a998dfd1c8ff63749a2f8d579778c368f28c1baf.

--- pkgs/os-specific/linux/alsa-firmware/default.nix
+++ pkgs/os-specific/linux/alsa-firmware/default.nix
@@ -1,15 +1,16 @@
 {stdenv, fetchurl}:
 
 stdenv.mkDerivation rec {
-  name = "alsa-firmware-1.0.29";
+  pname = "alsa-firmware";
+  version = "1.2.1";
 
   src = fetchurl {
-    url = "mirror://alsa/firmware/${name}.tar.bz2";
-    sha256 = "0gfcyj5anckjn030wcxx5v2xk2s219nyf99s9m833275b5wz2piw";
+    url = "mirror://alsa/firmware/${pname}-${version}.tar.bz2";
+    sha256 = "1aq8z8ajpjvcx7bwhwp36bh5idzximyn77ygk3ifs0my3mbpr8mf";
   };
 
   configureFlags = [
-    "--with-hotplug-dir=$(out)/lib/firmware"
+    "--with-hotplug-dir=${placeholder "out"}/lib/firmware"
   ];
 
   dontStrip = true;
@@ -17,17 +18,15 @@ stdenv.mkDerivation rec {
   postInstall = ''
     # These are lifted from the Arch PKGBUILD
     # remove files which conflicts with linux-firmware
-    rm -rf $out/lib/firmware/{ct{efx,speq}.bin,ess,korg,sb16,yamaha}
+    rm -rvf $out/lib/firmware/{ct{efx,speq}.bin,ess,korg,sb16,yamaha}
     # remove broken symlinks (broken upstream)
-    rm -rf $out/lib/firmware/turtlebeach
-    # remove empty dir
-    rm -rf $out/bin
+    rm -rvf $out/lib/firmware/turtlebeach
   '';
 
   meta = {
     homepage = http://www.alsa-project.org/;
     description = "Soundcard firmwares from the alsa project";
-    license = stdenv.lib.licenses.gpl2Plus;
+    license = stdenv.lib.licenses.gpl2;
     platforms = stdenv.lib.platforms.linux;
   };
 }
--- pkgs/os-specific/linux/alsa-lib/default.nix
+++ pkgs/os-specific/linux/alsa-lib/default.nix
@@ -1,26 +1,17 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "alsa-lib-1.1.9";
+  pname = "alsa-lib";
+  version = "1.2.1";
 
   src = fetchurl {
-    url = "mirror://alsa/lib/${name}.tar.bz2";
-    sha256 = "0jwr9g4yxg9gj6xx0sb2r6wrdl8amrjd19hilkrq4rirynp770s8";
+    url = "mirror://alsa/lib/${pname}-${version}.tar.bz2";
+    sha256 = "1ky7mpxr1s6gj1lyhlfrnp23i09rvjy47c4c9h1nijwg3qdzy3cz";
   };
 
   patches = [
     ./alsa-plugin-conf-multilib.patch
   ];
-
-  # Fix pcm.h file in order to prevent some compilation bugs
-  # 2: see http://stackoverflow.com/questions/3103400/how-to-overcome-u-int8-t-vs-uint8-t-issue-efficiently
-  postPatch = ''
-    sed -i -e 's|//int snd_pcm_mixer_element(snd_pcm_t \*pcm, snd_mixer_t \*mixer, snd_mixer_elem_t \*\*elem);|/\*int snd_pcm_mixer_element(snd_pcm_t \*pcm, snd_mixer_t \*mixer, snd_mixer_elem_t \*\*elem);\*/|' include/pcm.h
-
-
-    sed -i -e '1i#include <stdint.h>' include/pcm.h
-    sed -i -e 's/u_int\([0-9]*\)_t/uint\1_t/g' include/pcm.h
-  '';
 
   outputs = [ "out" "dev" ];
 
