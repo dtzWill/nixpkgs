9ba7358b8ddb91ca71498d6d74c2f4d29d6be78b
Will Dietz <w@wdtz.org>
2019-11-19 19:15:02 -0600

    xfce*: touchups/fixes

--- nixos/modules/services/x11/desktop-managers/xfce.nix
+++ nixos/modules/services/x11/desktop-managers/xfce.nix
@@ -41,15 +41,14 @@ in
         description = "Enable the Xfce desktop environment.";
       };
 
-      # thunarPlugins... not supported w/xfce4-14?
-      #thunarPlugins = mkOption {
-      #  default = [];
-      #  type = types.listOf types.package;
-      #  example = literalExample "[ pkgs.xfce.thunar-archive-plugin ]";
-      #  description = ''
-      #    A list of plugin that should be installed with Thunar.
-      #  '';
-      #};
+      thunarPlugins = mkOption {
+        default = [];
+        type = types.listOf types.package;
+        example = literalExample "[ pkgs.xfce.thunar-archive-plugin ]";
+        description = ''
+          A list of plugin that should be installed with Thunar.
+        '';
+      };
 
       noDesktop = mkOption {
         type = types.bool;
