973c035ecc12f53c5c4b1512c6e64e6c0904cf03
edef <edef@edef.eu>
2019-11-27 19:54:48 +0000

    pounce: init at 1.0p1
    (cherry picked from commit 2a06e1521fe2d0aab2b0e244a1566fcc20b27f1b)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25082,6 +25082,8 @@ in
 
   pjsip = callPackage ../applications/networking/pjsip { };
 
+  pounce = callPackage ../servers/pounce { };
+
   ppsspp = libsForQt5.callPackage ../misc/emulators/ppsspp { };
 
   pt = callPackage ../applications/misc/pt { };
