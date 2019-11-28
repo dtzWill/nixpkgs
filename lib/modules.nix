f1a4e8353042b7071a1b0dae4e4e6c59b1682547
Will Dietz <w@wdtz.org>
2019-11-28 15:00:32 -0600

    lib/modules: revert warnings -> assertions until pull in module fixups

--- lib/modules.nix
+++ lib/modules.nix
@@ -595,14 +595,12 @@ rec {
         visible = false;
         apply = x: throw "The option `${showOption optionName}' can no longer be used since it's been removed. ${replacementInstructions}";
       });
-      config.assertions =
-        let opt = getAttrFromPath optionName options; in [{
-          assertion = !opt.isDefined;
-          message = ''
+      # TODO: master has this assertions, not warnings?
+      config.warnings =
+        let opt = getAttrFromPath optionName options; in
+        optional opt.isDefined ''
             The option definition `${showOption optionName}' in ${showFiles opt.files} no longer has any effect; please remove it.
-            ${replacementInstructions}
-          '';
-        }];
+            ${replacementInstructions}'';
     };
 
   /* Return a module that causes a warning to be shown if the
