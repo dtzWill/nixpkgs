7c42dd384d18ab56402e3fa3c4f71daf9d44b1d4
Matthieu Coudron <mattator@gmail.com>
2019-12-01 00:03:16 +0100

    libtermkey: compile with unibilium
    As the alternative isn't well tested and will be ruled out very soon.
    
    (cherry picked from commit 9c6ceb58f5eaf0ddff0ad746f818400d2bfd8231)

--- pkgs/development/libraries/libtermkey/default.nix
+++ pkgs/development/libraries/libtermkey/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchzip, libtool, pkgconfig, ncurses }:
+{ stdenv, lib, fetchzip, libtool, pkgconfig, ncurses, unibilium }:
 
 stdenv.mkDerivation rec {
   name = "libtermkey-${version}";
@@ -13,11 +13,12 @@ stdenv.mkDerivation rec {
   makeFlags = [ "PREFIX=${placeholder "out"}" ]
     ++ stdenv.lib.optional stdenv.isDarwin "LIBTOOL=${libtool}/bin/libtool";
 
-  nativeBuildInputs = [ pkgconfig libtool ];
-  buildInputs = [ ncurses ];
+  nativeBuildInputs = [ libtool pkgconfig ];
+  buildInputs = [ ncurses unibilium ];
 
   meta = with lib; {
     description = "Terminal keypress reading library";
+    homepage = http://www.leonerd.org.uk/code/libtermkey;
     license = licenses.mit;
     platforms = platforms.unix;
   };
