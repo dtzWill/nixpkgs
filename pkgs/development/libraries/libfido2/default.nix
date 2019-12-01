35a1504d66964291dfe9c98df5a64049abe1f4e9
Will Dietz <w@wdtz.org>
2019-12-01 08:51:31 -0600

    libfido2: re-propagate libcrypto (libressl) to fix pam_u2f
    Propagate so pam_u2f uses same libcrypto.

--- pkgs/development/libraries/libfido2/default.nix
+++ pkgs/development/libraries/libfido2/default.nix
@@ -9,7 +9,8 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
-  buildInputs = [ libcbor libressl udev ];
+  buildInputs = [ libcbor udev ];
+  propagatedBuildInputs = [ libressl /* libcrypto, pam_u2f breaks if not propagated */ ];
 
   cmakeFlags = [ "-DUDEV_RULES_DIR=${placeholder "out"}/etc/udev/rules.d" ];
 
