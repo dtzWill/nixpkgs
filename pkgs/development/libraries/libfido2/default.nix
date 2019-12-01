87c526222ee1f37cdfa42f218ee3cc098d187c38
Will Dietz <w@wdtz.org>
2019-12-01 08:50:33 -0600

    libfido2: -> 1.3.0 (sync w/ PR74571)

--- pkgs/development/libraries/libfido2/default.nix
+++ pkgs/development/libraries/libfido2/default.nix
@@ -1,23 +1,15 @@
-{ stdenv, fetchurl, fetchFromGitHub, cmake, pkgconfig, libcbor, libressl, udev }:
+{ stdenv, fetchurl, cmake, pkgconfig, libcbor, libressl, udev }:
 
 stdenv.mkDerivation rec {
   pname = "libfido2";
-  version = "unstable-2019-11-19";
-  #version = "1.2.0";
-  #src = fetchurl {
-  #  url = "https://developers.yubico.com/libfido2/Releases/libfido2-${version}.tar.gz";
-  #  sha256 = "1pbllhzcrzkgxad00bai7lna8dpkwiv8khx8p20miy661abv956v";
-  #};
-  src = fetchFromGitHub {
-    owner = "Yubico";
-    repo = pname;
-    rev = "9343b25536ad4ad6f33b5ae28d310634cec3812a";
-    sha256 = "04q03ysjvwpa1mi654akxfc1yy14kv1bysizhgzn82jmw9l6gw44";
+  version = "1.3.0";
+  src = fetchurl {
+    url = "https://developers.yubico.com/${pname}/Releases/${pname}-${version}.tar.gz";
+    sha256 = "1izyl3as9rn7zcxpsvgngjwr55gli5gy822ac3ajzm65qiqkcbhb";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
-  buildInputs = [ libcbor udev ];
-  propagatedBuildInputs = [ libressl /* libcrypto */ ];
+  buildInputs = [ libcbor libressl udev ];
 
   cmakeFlags = [ "-DUDEV_RULES_DIR=${placeholder "out"}/etc/udev/rules.d" ];
 
