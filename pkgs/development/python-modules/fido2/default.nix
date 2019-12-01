40ecc83d16f6c89b82870f655fe496792429fb32
Will Dietz <w@wdtz.org>
2019-12-01 09:20:03 -0600

    python3Packages.fido2: 0.7.1 -> 0.8.1

--- pkgs/development/python-modules/fido2/default.nix
+++ pkgs/development/python-modules/fido2/default.nix
@@ -5,11 +5,11 @@
 
 buildPythonPackage rec {
   pname = "fido2";
-  version = "0.7.1";
+  version = "0.8.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "10i61g8srx1dk0wfjj11s7ka5pv0661ivwg2r0y3y2nsnf5b90s4";
+    sha256 = "1hzprnd407g2xh9kyv8j8pq949hwr1snmg3fp65pqfbghzv6i424";
   };
 
   propagatedBuildInputs = [ six cryptography ];
