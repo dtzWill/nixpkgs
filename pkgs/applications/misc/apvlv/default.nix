bcdb82972cc75b159781f7d9c707d2ded9a16997
Will Dietz <w@wdtz.org>
2019-12-02 11:33:24 -0600

    Revert "apvlv: 0.1.5 -> 0.2.0? wip"
    This reverts commit d88fe15ea91fdefa01e97a0d6435f2fdd23c438d.

--- pkgs/applications/misc/apvlv/default.nix
+++ pkgs/applications/misc/apvlv/default.nix
@@ -3,14 +3,14 @@
 }:
 
 stdenv.mkDerivation rec {
-  version = "0.2.0";
+  version = "0.1.5";
   name = "apvlv-${version}";
 
   src = fetchFromGitHub {
     owner = "naihe2010";
     repo = "apvlv";
     rev = "v${version}";
-    sha256 = "0lnp7kp2b44d6frj7hfdkhj6njy2mp6kn98gccv2v441gmj5lx9y";
+    sha256 = "1n4xiic8lqnv3mqi7wpdv866gyyakax71gffv3n9427rmcld465i";
   };
 
   NIX_CFLAGS_COMPILE = "-I${poppler.dev}/include/poppler";
@@ -25,6 +25,23 @@ stdenv.mkDerivation rec {
     poppler pcre libxkbcommon epoxy
     freetype gtk3
     libpthreadstubs libXdmcp libxshmfence # otherwise warnings in compilation
+  ];
+
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/naihe2010/apvlv/commit/d432635b9c5ea6c052a2ae1fb71aedec5c4ad57a.patch";
+      sha256 = "1am8dgv2kkpqmm2vaysa61czx8ppdx94zb3c59sx88np50jpy70w";
+    })
+    (fetchpatch {
+      url = "https://github.com/naihe2010/apvlv/commit/4c7a583e8431964def482e5471f02e6de8e62a7b.patch";
+      sha256 = "1dszm120lwm90hcg5zmd4vr6pjyaxc84qmb7k0fr59mmb3qif62j";
+    })
+    # fix build with gcc7
+    (fetchpatch {
+      url = "https://github.com/naihe2010/apvlv/commit/a3a895772a27d76dab0c37643f0f4c73f9970e62.patch";
+      sha256 = "1fpc7wr1ajilvwi5gjsy5g9jcx4bl03gp5dmajg90ljqbhwz2bfi";
+    })
+    ./fix-build-with-poppler-0.73.0.patch
   ];
 
   installPhase = ''
