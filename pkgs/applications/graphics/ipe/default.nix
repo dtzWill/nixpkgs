11f76d1420b447bd04728cba07720d7b4428de28
Will Dietz <w@wdtz.org>
2019-12-02 10:00:06 -0600

    ipe: mkDerivation, pname

--- pkgs/applications/graphics/ipe/default.nix
+++ pkgs/applications/graphics/ipe/default.nix
@@ -1,19 +1,20 @@
-{ stdenv, fetchurl, makeWrapper, pkgconfig, zlib, freetype, cairo, lua5, texlive, ghostscript
+{ lib, mkDerivation, fetchurl, makeWrapper, pkgconfig, zlib, freetype, cairo, lua5, texlive, ghostscript
 , libjpeg, libpng, qtbase
 }:
 
-stdenv.mkDerivation rec {
-  name = "ipe-7.2.13";
+mkDerivation rec {
+  pname = "ipe";
+  version = "7.2.13";
 
   src = fetchurl {
-    url = "https://dl.bintray.com/otfried/generic/ipe/7.2/${name}-src.tar.gz";
+    url = "https://dl.bintray.com/otfried/generic/ipe/7.2/${pname}-${version}-src.tar.gz";
     sha256 = "1a6a88r7j5z01z6k1z72a8g3n6lxdjjxxkdrzrfdd6df2gbs6g5g";
   };
 
-  sourceRoot = "${name}/src";
+  sourceRoot = "${pname}-${version}/src";
 
-  IPEPREFIX="${placeholder "out"}";
-  URWFONTDIR="${texlive}/texmf-dist/fonts/type1/urw/";
+  IPEPREFIX = placeholder "out";
+  URWFONTDIR = "${texlive}/texmf-dist/fonts/type1/urw/";
   LUA_PACKAGE = "lua";
 
   buildInputs = [
@@ -32,16 +33,16 @@ stdenv.mkDerivation rec {
 
   #TODO: make .desktop entry
 
-  meta = {
+  meta = with lib; {
     description = "An editor for drawing figures";
     homepage = http://ipe.otfried.org;
-    license = stdenv.lib.licenses.gpl3Plus;
+    license = licenses.gpl3Plus;
     longDescription = ''
       Ipe is an extensible drawing editor for creating figures in PDF and Postscript format.
       It supports making small figures for inclusion into LaTeX-documents
       as well as presentations in PDF.
     '';
-    maintainers = [ stdenv.lib.maintainers.ttuegel ];
-    platforms = stdenv.lib.platforms.linux;
+    maintainers = [ lib.maintainers.ttuegel ];
+    platforms = lib.platforms.linux;
   };
 }
