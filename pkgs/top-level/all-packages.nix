8710c89928590024cefb7c215ec7378452dd3334
Kirill Boltaev <aske@fmap.me>
2019-12-04 12:18:28 +0300

    qtspim: init at 722
    (cherry picked from commit c86c8dae7afd18936ed95f6f1efc8aed13ecf59e)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1981,6 +1981,8 @@ in
 
   socklog = callPackage ../tools/system/socklog { };
 
+  qtspim = qt5.callPackage ../applications/virtualization/spim { };
+
   staccato = callPackage ../tools/text/staccato { };
 
   stagit = callPackage ../development/tools/stagit { };
