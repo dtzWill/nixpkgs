924fd5039995355ec2a1bd3762952ff293019f28
Izorkin <izorkin@elven.pw>
2019-12-07 12:32:02 +0300

    phpPackages.ast: 1.0.3 -> 1.0.5
    (cherry picked from commit b5eb1c9f24940f2c72f3ae8bdd091edd672d3e57)

--- pkgs/top-level/php-packages.nix
+++ pkgs/top-level/php-packages.nix
@@ -39,11 +39,11 @@ let
     buildInputs = [ apcu (if isPhp73 then pkgs.pcre2 else pkgs.pcre) ];
   };
 
-  ast = buildPecl rec {
-    version = "1.0.3";
+  ast = buildPecl {
+    version = "1.0.5";
     pname = "ast";
 
-    sha256 = "1sk9bkyw3ck9jgvlazxx8zl2nv6lc0gq66v1rfcby9v0zyydb7xr";
+    sha256 = "16c5isldm4csjbcvz1qk2mmrhgvh24sxsp6w6f5a37xpa3vciawp";
   };
 
   box = mkDerivation rec {
