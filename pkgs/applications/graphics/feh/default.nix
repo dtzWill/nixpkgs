380387fdceb42633ab25500393c738e529ed0dc6
R. RyanTM <ryantm-bot@ryantm.com>
2019-12-07 21:49:54 -0800

    feh: 3.2.1 -> 3.3
    (cherry picked from commit 457a3c760d105f4907585fe8c4de3a63c6ab13a4)

--- pkgs/applications/graphics/feh/default.nix
+++ pkgs/applications/graphics/feh/default.nix
@@ -5,12 +5,12 @@
 with stdenv.lib;
 
 stdenv.mkDerivation rec {
-  name = "feh-${version}";
-  version = "3.2.1";
+  pname = "feh";
+  version = "3.3";
 
   src = fetchurl {
-    url = "https://feh.finalrewind.org/${name}.tar.bz2";
-    sha256 = "070axq8jpibcabmjfv4fmjmpk3k349vzvh4qhsi4n62bkcwl35wg";
+    url = "https://feh.finalrewind.org/${pname}-${version}.tar.bz2";
+    sha256 = "04c8cgwzkax481sz7lbzy23mk79bqmjy3qpvr7vxa4c14mc9k5gk";
   };
 
   outputs = [ "out" "man" "doc" ];
