2a34e18ec97ba18bc02f25ad10c18bb53ef8ae48
André-Patrick Bubel <code@andre-bubel.de>
2019-12-08 20:53:49 +0100

    bluez: 5.51 -> 5.52, aliased bluezFull to bluez
    (cherry picked from commit 83655d69937f6d1b62e8cc791cea27026bd21249)

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -47,6 +47,7 @@ mapAliases ({
   at_spi2_core = at-spi2-core; # added 2018-02-25
   bar-xft = lemonbar-xft;  # added 2015-01-16
   bashCompletion = bash-completion; # Added 2016-09-28
+  bluezFull = bluez; # Added 2019-12-03
   bridge_utils = bridge-utils;  # added 2015-02-20
   bro = zeek; # added 2019-09-29
   btrfsProgs = btrfs-progs; # added 2016-01-03
