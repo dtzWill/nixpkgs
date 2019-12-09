8985952ce14dbe32b40f13982800623faa1402e8
Timo Kaufmann <timokau@zoho.com>
2019-12-09 08:11:20 +0100

    fplll: update formatting
    According to my perception of the current community standards.
    
    (cherry picked from commit ed8d53655fe36e69262ae517f388ec016b3a5ad0)

--- pkgs/development/libraries/fplll/default.nix
+++ pkgs/development/libraries/fplll/default.nix
@@ -1,23 +1,43 @@
-{stdenv, fetchFromGitHub, autoconf, automake, libtool, gettext, autoreconfHook
-, gmp, mpfr
+{ stdenv
+, fetchFromGitHub
+, autoconf
+, automake
+, libtool
+, gettext
+, autoreconfHook
+, gmp
+, mpfr
 }:
+
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "fplll";
   version = "5.2.1";
+
   src = fetchFromGitHub {
-    owner = "${pname}";
-    repo = "${pname}";
-    rev = "${version}";
+    owner = "fplll";
+    repo = "fplll";
+    rev = version;
     sha256 = "015qmrd7nfaysbv1hbwiprz9g6hnww1y1z1xw8f43ysb7k1b5nbg";
   };
-  nativeBuildInputs = [autoconf automake libtool gettext autoreconfHook];
-  buildInputs = [gmp mpfr];
-  meta = {
-    inherit version;
+
+  nativeBuildInputs = [
+    autoconf
+    automake
+    libtool
+    gettext
+    autoreconfHook
+  ];
+
+  buildInputs = [
+    gmp
+    mpfr
+  ];
+
+  meta = with stdenv.lib; {
     description = ''Lattice algorithms using floating-point arithmetic'';
-    license = stdenv.lib.licenses.lgpl21Plus;
-    maintainers = [stdenv.lib.maintainers.raskin];
-    platforms = stdenv.lib.platforms.linux;
+    license = licenses.lgpl21Plus;
+    maintainers = with maintainers; [raskin];
+    platforms = platforms.unix;
   };
 }
