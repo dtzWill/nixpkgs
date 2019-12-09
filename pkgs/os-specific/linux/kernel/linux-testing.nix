93417f8de5b9d73c2e9f5e13ddbf47b5fa75fa96
Austin Seipp <aseipp@pobox.com>
2019-12-09 10:05:01 -0600

    linux_testing: 5.4-rc7 -> 5.5-rc1
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    (cherry picked from commit 452efe57a9223f2054aad8fc862bb27ccec2cfa1)

--- pkgs/os-specific/linux/kernel/linux-testing.nix
+++ pkgs/os-specific/linux/kernel/linux-testing.nix
@@ -3,15 +3,15 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.4-rc8";
-  extraMeta.branch = "5.4";
+  version = "5.5-rc1";
+  extraMeta.branch = "5.5";
 
   # modDirVersion needs to be x.y.z, will always add .0
   modDirVersion = if (modDirVersionArg == null) then builtins.replaceStrings ["-"] [".0-"] version else modDirVersionArg;
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "0k8d98c7g0bxp8xdpfnxqqjpih6gy85irggdgxhplabgy08b245k";
+    sha256 = "0d2s2z08bj8jp4hnx116wpvivx7h8risv2xdxj7155lz155gffdp";
   };
 
   # Should the testing kernels ever be built on Hydra?
