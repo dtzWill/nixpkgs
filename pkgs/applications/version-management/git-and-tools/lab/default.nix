61b3f6ffe795bae2c1dd5f4618410636c0d77090
Will Dietz <w@wdtz.org>
2019-12-12 13:40:50 -0600

    lab: revert, needs go 1.13

--- pkgs/applications/version-management/git-and-tools/lab/default.nix
+++ pkgs/applications/version-management/git-and-tools/lab/default.nix
@@ -2,18 +2,19 @@
 
 buildGoModule rec {
   pname = "lab";
-  version = "0.17.0";
+  version = "0.16.0-2019-08-15";
 
   src = fetchFromGitHub {
     owner = "zaquestion";
     repo = "lab";
-    rev = "v${version}";
-    sha256 = "1p8q21k8p1zw1g4fn6f7b80r3wziywbm1av1vg7hc8w1rfqj51wp";
+#    rev = "v${version}";
+    rev = "118fa13b3e0b66b0da346140246e59c1227886c9";
+    sha256 = "0v51i9zcr4sw0icjh05saavlx2nybwfqg08pks7j3z2pra1ppx96";
   };
 
   subPackages = [ "." ];
 
-  modSha256 = "16y87cdxrmg7wy5kifvzvsapjq4iwrpii971cgqmzzm0d1d90gj3";
+  modSha256 = "0r36janm2386p8wa68b54h0mc5k162d84283zq3dp8v15iy29zgs";
 
   postInstall = ''
     mkdir -p "$out/share/bash-completion/completions" "$out/share/zsh/site-functions"
