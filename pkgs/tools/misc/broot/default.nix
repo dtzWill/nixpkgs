c55828911108b3c0ddfda4734408f02115f50875
Will Dietz <w@wdtz.org>
2019-12-16 13:44:13 -0600

    broot: 0.10.3 -> 0.10.4

--- pkgs/tools/misc/broot/default.nix
+++ pkgs/tools/misc/broot/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "broot";
-  version = "0.10.3";
+  version = "0.10.4";
 
   src = fetchFromGitHub {
     owner = "Canop";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0dr4an5ch3jb965bi52hz08ksiqniv25hwi1a1f5hqm1fwggxqvj";
+    sha256 = "0rj1ab8q7102fqcw89cjwzkqk6lxvqkq8sbwfgw9xgn6spnbdkdr";
   };
 
-  cargoSha256 = "127z0wphkj823aiq9cgxhgbcxdggzywblzhf03jgqj14mjwq6w20";
+  cargoSha256 = "17nazm4cgw4dm0vjlxgc93v28p960gls1203r61xbx6l7aibxrn3";
 
   meta = with stdenv.lib; {
     description = "An interactive tree view, a fuzzy search, a balanced BFS descent and customizable commands";
