5fba83b7e7279f9f5e45d8c6d8fc38adfc17cd3d
R. RyanTM <ryantm-bot@ryantm.com>
2019-12-23 20:02:01 -0800

    osm2pgsql: 1.2.0 -> 1.2.1
    (cherry picked from commit 1875c65877afb79e6b0febffa8c9c15ae97d635b)

--- pkgs/tools/misc/osm2pgsql/default.nix
+++ pkgs/tools/misc/osm2pgsql/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchFromGitHub, cmake, expat, proj, bzip2, zlib, boost, postgresql, lua}:
 
 stdenv.mkDerivation rec {
-  name = "osm2pgsql-${version}";
-  version = "0.96.0";
+  pname = "osm2pgsql";
+  version = "1.2.1";
 
   src = fetchFromGitHub {
     owner = "openstreetmap";
     repo = "osm2pgsql";
     rev = version;
-    sha256 = "032cydh8ynaqfhdzmkvgbmqyjql668y6qln1l59l2s3ni9963bbl";
+    sha256 = "1ysan01lpqzjxlq3y2kdminfjs5d9zksicpf9vvzpdk3fzq51fc9";
   };
 
   nativeBuildInputs = [ cmake ];
