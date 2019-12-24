11c2cafe486ffc302029c9cee51de3320bf938ed
R. RyanTM <ryantm-bot@ryantm.com>
2019-12-24 10:27:47 -0800

    tautulli: 2.1.38 -> 2.1.39
    (cherry picked from commit a0d1a7fbee2a496102a07434f1250d36936b5393)

--- pkgs/servers/tautulli/default.nix
+++ pkgs/servers/tautulli/default.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchFromGitHub, python }:
 
 stdenv.mkDerivation rec {
-  version = "2.1.33";
+  version = "2.1.39";
   pname = "Tautulli";
   name = "${pname}-${version}";
 
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
     owner = "Tautulli";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1yj4akp10fxqndsbb59rjdw9by6rhmz24syyq6d4x5bg72an9n80";
+    sha256 = "1lr0pf01fxay4wppzgwp9kz2vlnnb9f8lvgfic0zgwwil2ky6b1w";
   };
 
   buildPhase = ":";
