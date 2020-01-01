65a95669f21eb1bf203bf4fd790ef0d5411ed152
Will Dietz <w@wdtz.org>
2020-01-01 13:45:47 -0600

    whois: 5.5.3 -> 5.5.4, pname

--- pkgs/tools/networking/whois/default.nix
+++ pkgs/tools/networking/whois/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchFromGitHub, perl, gettext, pkgconfig, libidn2, libiconv }:
 
 stdenv.mkDerivation rec {
-  version = "5.5.3";
-  name = "whois-${version}";
+  version = "5.5.4";
+  pname = "whois";
 
   src = fetchFromGitHub {
     owner = "rfc1036";
-    repo = "whois";
+    repo = pname;
     rev = "v${version}";
-    sha256 = "099yvqng085f69k815961jnwk50nzmiknvhl1wwmji2hl36r160g";
+    sha256 = "0ky4c7xyvshrgnyk8vs3gc9kwhlbxkh3qf5hcdy7ibvnz4zznsbs";
   };
 
   nativeBuildInputs = [ perl gettext pkgconfig ];
