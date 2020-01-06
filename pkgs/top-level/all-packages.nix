ab0b1542e13307696772bbd967035d3b1c2b45b4
Francesco Gazzetta <fgaz@fgaz.me>
2020-01-06 15:01:23 +0100

    facette: remove
    It never actually built within the sandbox, and it's now more difficult
    to package than ever
    
    (cherry picked from commit d56e84b9c4e55665c74c4658de3ffac1a00d720e)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15064,8 +15064,6 @@ in
 
   exim = callPackage ../servers/mail/exim { };
 
-  facette = callPackage ../servers/monitoring/facette { };
-
   fcgiwrap = callPackage ../servers/fcgiwrap { };
 
   felix = callPackage ../servers/felix { };
