018a025cba4d50308c35318dc03d3881d2a2a214
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-06 17:18:15 -0800

    microsoft_gsl: 2.0.0 -> 2.1.0
    (cherry picked from commit 69b380251721785772a5dc2dfed53dbd51fd1d90)

--- pkgs/development/libraries/microsoft_gsl/default.nix
+++ pkgs/development/libraries/microsoft_gsl/default.nix
@@ -5,14 +5,14 @@ let
   nativeBuild = stdenv.hostPlatform == stdenv.buildPlatform;
 in
 stdenv.mkDerivation rec {
-  name = "microsoft_gsl-${version}";
-  version = "2.0.0";
+  pname = "microsoft_gsl";
+  version = "2.1.0";
 
   src = fetchFromGitHub {
     owner = "Microsoft";
     repo = "GSL";
     rev = "v${version}";
-    sha256 = "1kxfca9ik934nkzyn34ingkyvwpc09li81cg1yc6vqcrdw51l4ri";
+    sha256 = "09f08lxqm00152bx9yrizlgabzpzxlpbv06h00z4w78yxywgxlgx";
   };
 
   # build phase just runs the unit tests, so skip it if
