9957e29eeda8e0f718afcae6d0f796a22cf5ea8b
zimbatm <zimbatm@zimbatm.com>
2020-01-10 18:04:22 +0000

    matterbridge: 1.11.0 -> 1.16.3 (#77454)
    (cherry picked from commit 4371ecb8a61f672b3bbf82fca32efbb418a3730f)

--- pkgs/servers/matterbridge/default.nix
+++ pkgs/servers/matterbridge/default.nix
@@ -1,19 +1,16 @@
-{ stdenv, buildGoModule, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchurl }:
 
 buildGoModule rec {
   pname = "matterbridge";
-  version = "1.16.2";
+  version = "1.16.3";
 
   goPackagePath = "github.com/42wim/matterbridge";
+  modSha256 = "sha256-Q6R6AhAELirFijw5ntyjly46HCzFMpLGSJYfv864gt0=";
 
-  src = fetchFromGitHub {
-    owner = "42wim";
-    repo = pname;
-    rev = "refs/tags/v${version}";
-    sha256 = "0cddm9vnismiifcgc13p4r6bdwvk7l9q1cxksp7v14w0x24jyb91";
+  src = fetchurl {
+    url = "https://github.com/42wim/matterbridge/archive/v${version}.tar.gz";
+    sha256 = "sha256-VAbZSXilmmd2z2bK4/UZzOrjohDVcJHah9t3DE1mtOE=";
   };
-
-  modSha256 = "1fvwx1wks4brw314z6kl3wn117qg239phzxhzxgqzmvk4x26c4bp";
 
   meta = with stdenv.lib; {
     description = "Simple bridge between Mattermost, IRC, XMPP, Gitter, Slack, Discord, Telegram, Rocket.Chat, Hipchat(via xmpp), Matrix and Steam";
