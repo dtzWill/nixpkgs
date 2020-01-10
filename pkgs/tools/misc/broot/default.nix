430f5c924a0cf9db3294973883a149b654348b23
Will Dietz <w@wdtz.org>
2020-01-10 15:42:14 -0600

    broot: 0.11.2 -> 0.11.5

--- pkgs/tools/misc/broot/default.nix
+++ pkgs/tools/misc/broot/default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "broot";
-  version = "0.11.2";
+  version = "0.11.5";
 
   src = fetchFromGitHub {
     owner = "Canop";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0ms9wdh3ryy8q1rpm8r2hqikanbwx7h9vqjkc70b11j6p8iicmmr";
+    sha256 = "1b64cl5080rzfbxx54y60klcdv0hvrmwklskl95pkcspgfffk2nm";
   };
 
   cargoSha256 = "0w62sxdl2w1wc0fjxszpifw9jxl35nz5nsmfr6pd0lvbgm3c7vba";
