3e7e0e02c10e4386928b51b30f6a93a000f4ec13
Ben Darwin <bcdarwin@gmail.com>
2020-01-09 22:34:07 -0500

    pythonPackages.hydra: init at 1.4.1
    (cherry picked from commit ee7d45c638be124d8ab410d9ba2bbd0b5bc5520d)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3490,6 +3490,8 @@ in {
 
   hvac = callPackage ../development/python-modules/hvac { };
 
+  hydra = callPackage ../development/python-modules/hydra { };
+
   hypothesis = callPackage ../development/python-modules/hypothesis { };
 
   colored = callPackage ../development/python-modules/colored { };
