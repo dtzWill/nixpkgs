c2ad8dac550a98a32b8ee37733d9d387e4c81e4e
Will Dietz <w@wdtz.org>
2020-01-12 15:19:12 -0600

    kdbg: qt's mkDerivation instead of manually wrapping for QT_PLUGIN_PATH

--- pkgs/development/tools/misc/kdbg/default.nix
+++ pkgs/development/tools/misc/kdbg/default.nix
@@ -1,9 +1,9 @@
-{ stdenv, fetchurl, cmake, extra-cmake-modules, qt5,
+{ mkDerivation, lib, fetchurl, cmake, extra-cmake-modules, qt5,
   ki18n, kconfig, kiconthemes, kxmlgui, kwindowsystem,
   qtbase, makeWrapper,
 }:
 
-stdenv.mkDerivation rec {
+mkDerivation rec {
   pname = "kdbg";
   version = "3.0.1";
   src = fetchurl {
@@ -11,17 +11,13 @@ stdenv.mkDerivation rec {
     sha256 = "1gax6xll8svmngw0z1rzhd77xysv01zp0i68x4n5pq0xgh7gi7a4";
   };
 
-  nativeBuildInputs = [ cmake extra-cmake-modules makeWrapper ];
+  nativeBuildInputs = [ cmake extra-cmake-modules ];
   buildInputs = [ qt5.qtbase ki18n kconfig kiconthemes kxmlgui kwindowsystem ];
 
   enableParallelBuilding = true;
 
 
-  postInstall = ''
-    wrapProgram $out/bin/kdbg --prefix QT_PLUGIN_PATH : ${qtbase}/${qtbase.qtPluginPrefix}
-  '';
-
-  meta = with stdenv.lib; {
+  meta = with lib; {
     homepage = http://www.kdbg.org/;
     description = ''
       A graphical user interface to gdb, the GNU debugger. It provides an
