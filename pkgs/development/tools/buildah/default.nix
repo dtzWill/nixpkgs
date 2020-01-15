f81f0657e50c4c6d3027dce000ac898f8151354a
Mario Rodas <marsam@users.noreply.github.com>
2020-01-14 22:22:22 -0500

    buildah: 1.12.0 -> 1.13.1
    Changelog:
    - https://github.com/containers/buildah/releases/tag/v1.13.0
    - https://github.com/containers/buildah/releases/tag/v1.13.1
    
    (cherry picked from commit 9e3456a58e49974d8f0e13a104540c5ca21c976b)

--- pkgs/development/tools/buildah/default.nix
+++ pkgs/development/tools/buildah/default.nix
@@ -4,13 +4,13 @@
 
 buildGoModule rec {
   pname = "buildah";
-  version = "1.12.0";
+  version = "1.13.1";
 
   src = fetchFromGitHub {
     owner  = "containers";
     repo   = "buildah";
     rev    = "v${version}";
-    sha256 = "0lsjsfp6ls38vlgibbnsyd1m7jvmjwdmpyrd0qigp4aa2abwi4dg";
+    sha256 = "0swhpdr970ik6fhvmj45r84lsp1n6rxm0bgv9i1lvrxy1mdv7r9x";
   };
 
   modSha256 = "0xmwp3id5h2b749gyy3y2ihq9b5gxz8zj7l2ybdvjl3w4x4imxkl";
@@ -19,10 +19,6 @@ buildGoModule rec {
 
   goPackagePath = "github.com/containers/buildah";
   excludedPackages = [ "tests" ];
-
-  # Disable module-mode, because Go 1.13 automatically enables it if there is
-  # go.mod file. Remove after https://github.com/NixOS/nixpkgs/pull/73380
-  GO111MODULE = "off";
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ gpgme libgpgerror lvm2 btrfs-progs ostree libselinux libseccomp ];
@@ -33,6 +29,7 @@ buildGoModule rec {
     pushd go/src/${goPackagePath}
     make GIT_COMMIT="unknown"
     install -Dm755 buildah $bin/bin/buildah
+    install -Dm444 contrib/completions/bash/buildah $bin/share/bash-completion/completions/buildah
   '';
 
   postBuild = ''
@@ -42,6 +39,7 @@ buildGoModule rec {
   meta = with stdenv.lib; {
     description = "A tool which facilitates building OCI images";
     homepage = "https://github.com/containers/buildah";
+    changelog = "https://github.com/containers/buildah/releases/tag/v${version}";
     license = licenses.asl20;
     maintainers = with maintainers; [ Profpatsch vdemeester saschagrunert ];
   };
