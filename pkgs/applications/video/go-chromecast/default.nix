9de686366a51fa35cef01ec4f4841e425eef535e
Will Dietz <w@wdtz.org>
2020-01-14 19:15:41 -0600

    go-chromecast: git -> 0.0.14, update mkv patch

--- pkgs/applications/video/go-chromecast/default.nix
+++ pkgs/applications/video/go-chromecast/default.nix
@@ -2,15 +2,13 @@
 
 buildGoModule rec {
   pname = "go-chromecast";
-  #version = "0.0.13";
-  version = "2019-10-23";
+  version = "0.0.14";
 
   src = fetchFromGitHub {
     owner = "vishen";
     repo = pname;
-    #rev = "refs/tags/v${version}";
-    rev = "c44efa2cc92aaf579bdfddb8aa5a46ac524a5136";
-    sha256 = "0gqifw7d47bxj922fs9b7gy8rqqbw0dljdjg30n4q427jh9g6ska";
+    rev = "refs/tags/v${version}";
+    sha256 = "08732gkl0g570idyysz883knfp71sj5xkva9xv4zq5y9z20wsl9m";
   };
   modSha256 = "03b1f5vfrzwd69sqcr231i6bgl34jjjjryqmfflh7b57fpym8215";
 
