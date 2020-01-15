07f0a3ed0eb0226d8655ea067bc7b9352b0e738e
Will Dietz <w@wdtz.org>
2020-01-15 08:50:43 -0600

    slack-term: unstable-2019-10-12 -> unstable-2020-01-08

--- pkgs/applications/networking/instant-messengers/slack-term/default.nix
+++ pkgs/applications/networking/instant-messengers/slack-term/default.nix
@@ -3,7 +3,7 @@
 buildGoModule rec {
   # https://github.com/erroneousboat/slack-term
   name = "slack-term-${version}";
-  version = "unstable-2019-10-12";
+  version = "unstable-2020-01-08";
 
   goPackagePath = "github.com/erroneousboat/slack-term";
 
@@ -11,8 +11,8 @@ buildGoModule rec {
     owner = "erroneousboat";
     repo = "slack-term";
     #rev = "v${version}";
-    rev = "aa5d501a0db4cbddb1072dc8030813f91a1bd2eb";
-    sha256 = "1g3fdhxidcjcd3wxwmly4dzdjfa46cmrsv5c39v5f3nlzvz9ssk5";
+    rev = "78d1eb5e13984b3d6b859d3f69553e2ad20643ab";
+    sha256 = "19p0ijyqmmrilzmr3b4sm3ssdq2762cn34v62zjsvshhssw186lf";
   };
 
   modSha256 = "1jsyxqkmw4ap4c1fh1vj4j31wnbfyrhh2qgp3rnlryp004v6x9qs";
