1f8f41abd10cf3e2f4d9383e0f27af45f469f249
Will Dietz <w@wdtz.org>
2020-01-15 05:12:08 -0600

    luarocks-nix: nix-community

--- pkgs/development/tools/misc/luarocks/luarocks-nix.nix
+++ pkgs/development/tools/misc/luarocks/luarocks-nix.nix
@@ -1,11 +1,11 @@
 { luarocks, fetchFromGitHub }:
 luarocks.overrideAttrs(old: {
   pname = "luarocks-nix";
-  version = "2020-01-15";
+  version = "2019-09-07";
   src = fetchFromGitHub {
-    owner = "dtzWill"; # "teto";
+    owner = "nix-community";
     repo = "luarocks";
-    rev = "d0cad2d1deb90bf13e0fd30552b16e245a6aa3b5";
+    rev = "fa7c367bcdad36768db5f19fd4fcdd9681a14429";
     sha256 = "0kziwfw5gqq5xsckl7qf9wasaiy8rp42h5qrcnjx07qp47a9ldx7";
   };
   patches = [
