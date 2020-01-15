e775893843ebdb02fb5a88007bb4441439575141
Will Dietz <w@wdtz.org>
2020-01-15 06:35:32 -0600

    luarocks-nix: move to fork, fix handling of missing license info

--- pkgs/development/tools/misc/luarocks/luarocks-nix.nix
+++ pkgs/development/tools/misc/luarocks/luarocks-nix.nix
@@ -1,12 +1,12 @@
 { luarocks, fetchFromGitHub }:
 luarocks.overrideAttrs(old: {
   pname = "luarocks-nix";
-  version = "2019-09-07";
+  version = "2020-01-15";
   src = fetchFromGitHub {
-    owner = "nix-community";
+    owner = "dtzWill";
     repo = "luarocks";
-    rev = "fa7c367bcdad36768db5f19fd4fcdd9681a14429";
-    sha256 = "0kziwfw5gqq5xsckl7qf9wasaiy8rp42h5qrcnjx07qp47a9ldx7";
+    rev = "79f73945d8b9846848da417d970b5dcd4964644a";
+    sha256 = "08fw71qz5cpry9i9akr3pjhzpmz0c06yqf1pal6drdcgylirz8y7";
   };
   patches = [
     ./darwin-3.1.3.patch
