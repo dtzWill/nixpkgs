4778470d594c972f0007bbc39bfbdf54e51bc23d
Will Dietz <w@wdtz.org>
2020-01-17 03:23:31 -0600

    virt-what: man page, provide some optional deps

--- pkgs/applications/virtualization/virt-what/default.nix
+++ pkgs/applications/virtualization/virt-what/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, lib, fetchurl }:
+{ stdenv, lib, fetchurl, perl, busybox, dmidecode }:
+# busybox: cat grep id sed uname which
 
 stdenv.mkDerivation rec {
   pname = "virt-what";
@@ -8,6 +9,15 @@ stdenv.mkDerivation rec {
     url = "https://people.redhat.com/~rjones/${pname}/files/${pname}-${version}.tar.gz";
     sha256 = "1s0hg5w47gmnllbs935bx21k3zqrgvqx1wn0zzij2lfxkb9dq4zr";
   };
+
+  nativeBuildInputs = [ perl /* pod2man, for man pages */ ];
+
+  # The tool is more effective when it has some optinal tools
+  postPatch = ''
+    substituteInPlace virt-what.in \
+      --replace ':''${PATH}"' \
+                ':${stdenv.lib.makeBinPath [ busybox dmidecode ]}:''${PATH}"'
+  '';
 
   meta = with lib; {
     description = "Detect if running in a virtual machine and prints its type";
