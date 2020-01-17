16830fcd78b575afcf73d0d36df81473dd8f2469
Will Dietz <w@wdtz.org>
2020-01-16 23:59:58 -0600

    elemtnary-planner: 2.0.4 "Planner 2 is here"

--- pkgs/applications/office/elementary-planner/default.nix
+++ pkgs/applications/office/elementary-planner/default.nix
@@ -19,12 +19,14 @@
 
 stdenv.mkDerivation rec {
   pname = "planner";
-  version = "unstable-2019-11-14";
+  #version = "unstable-2019-11-14";
+  version = "2.0.4";
   src = fetchFromGitHub {
     owner = "alainm23";
     repo = pname;
-    rev = "a11808a528cacb453fc945bcc1c382d8f4ecfcce";
-    sha256 = "1r123qgy4dfzl2f3w7qiycfk5gviazvn0f8h8np58x4c62wg75qa";
+    #rev = "a11808a528cacb453fc945bcc1c382d8f4ecfcce";
+    rev = version;
+    sha256 = "15k2zh0fnpi27xim62l1k8c5cl6nspp4cpx8x8zljzzdarjxws5x";
   };
 
   nativeBuildInputs = [
