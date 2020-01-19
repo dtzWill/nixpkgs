35e30260ccfd773e9ce0a39bee265f40a2a34b27
Will Dietz <w@wdtz.org>
2020-01-18 19:24:56 -0600

    prometheus/alertmanager.nix: fix typo in mkRemovedOptionModule move

--- nixos/modules/services/monitoring/prometheus/alertmanager.nix
+++ nixos/modules/services/monitoring/prometheus/alertmanager.nix
@@ -30,7 +30,7 @@ in {
   imports = [
     (mkRemovedOptionModule [ "services" "prometheus" "alertmanager" "user" ] "The alertmanager service is now using systemd's DynamicUser mechanism which obviates a user setting.")
     (mkRemovedOptionModule [ "services" "prometheus" "alertmanager" "group" ] "The alertmanager service is now using systemd's DynamicUser mechanism which obviates a group setting.")
-    (mkRemovedOptionModule [ "services" "prometheus" "alertmanagerURL" ] ''
+    (mkRemovedOptionModule [ "services" "prometheus2" "alertmanagerURL" ] ''
       Due to incompatibility, the alertmanagerURL option has been removed,
       please use 'services.prometheus2.alertmanagers' instead.
     '')
