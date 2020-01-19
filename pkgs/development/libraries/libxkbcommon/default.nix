77a24da7874a39e1b895291d19c8ea9de724c17a
Will Dietz <w@wdtz.org>
2020-01-19 03:56:12 -0600

    Revert "libxkbcommon: 0.9.1 -> 0.10.0" (rebuilds)
    This reverts commit 37007a999675c895dd1b948a4077e251e0786e4b.

--- pkgs/development/libraries/libxkbcommon/default.nix
+++ pkgs/development/libraries/libxkbcommon/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "libxkbcommon";
-  version = "0.10.0";
+  version = "0.9.1";
 
   src = fetchurl {
     url = "https://xkbcommon.org/download/${pname}-${version}.tar.xz";
-    sha256 = "1wmnl0hngn6vrqrya4r8hvimlkr4jag39yjprls4gyrqvh667hsp";
+    sha256 = "1q4v378sr9ad8fy9znl7k8xpf0wch655r9m6z0bcc7sw1azsminl";
   };
 
   outputs = [ "out" "dev" "doc" ];
