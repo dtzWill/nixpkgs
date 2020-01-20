b535831409f41b9096aedaa2593f6bfcb57a5d84
Will Dietz <w@wdtz.org>
2020-01-19 20:27:15 -0600

    wasabiwallet: whitespace? sync w/nixpkgs master

--- pkgs/applications/blockchains/wasabiwallet/default.nix
+++ pkgs/applications/blockchains/wasabiwallet/default.nix
@@ -9,30 +9,30 @@ stdenv.mkDerivation rec {
     sha256 = "0qcgrw106rqcls6p5iq02sq3w6xrzhc5z7w8v5almbw7ikv6f0s2";
   };
 
-	dontBuild = true;
-	dontPatchELF = true;
+  dontBuild = true;
+  dontPatchELF = true;
 
-	desktopItem = makeDesktopItem {
-		name = "wasabi";
-		exec = "wasabiwallet";
-		desktopName = "Wasabi";
-		genericName = "Bitcoin wallet";
-		comment = meta.description;
-		categories = "Application;Network;Utility;";
-	};
+  desktopItem = makeDesktopItem {
+    name = "wasabi";
+    exec = "wasabiwallet";
+    desktopName = "Wasabi";
+    genericName = "Bitcoin wallet";
+    comment = meta.description;
+    categories = "Application;Network;Utility;";
+  };
 
   installPhase = ''
-		mkdir -p $out/opt/${pname} $out/bin $out/share/applications
-		cp -Rv . $out/opt/${pname}
-		cd $out/opt/${pname}
-		for i in $(find . -type f -name '*.so') wassabee
-			do
-				patchelf --set-rpath ${stdenv.lib.makeLibraryPath [ openssl stdenv.cc.cc.lib xorg.libX11 curl fontconfig.lib krb5 zlib dotnet-sdk ]} $i
-			done
-		patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" wassabee
-		ln -s $out/opt/${pname}/wassabee $out/bin/${pname}
-		cp -v $desktopItem/share/applications/* $out/share/applications
-	'';
+    mkdir -p $out/opt/${pname} $out/bin $out/share/applications
+    cp -Rv . $out/opt/${pname}
+    cd $out/opt/${pname}
+    for i in $(find . -type f -name '*.so') wassabee
+      do
+        patchelf --set-rpath ${stdenv.lib.makeLibraryPath [ openssl stdenv.cc.cc.lib xorg.libX11 curl fontconfig.lib krb5 zlib dotnet-sdk ]} $i
+      done
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" wassabee
+    ln -s $out/opt/${pname}/wassabee $out/bin/${pname}
+    cp -v $desktopItem/share/applications/* $out/share/applications
+  '';
 
   meta = with stdenv.lib; {
     description = "Privacy focused Bitcoin wallet";
