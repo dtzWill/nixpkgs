68d1c8c529334ec7245f53b389f57167a09d9718
Drew Risinger <drewrisinger@users.noreply.github.com>
2020-01-21 13:20:26 -0500

    pythonPackages.docloud: init at 1.0.375
    The IBM Decision Optimization on Cloud Python client.
    
    Requirement of docplex, which is a dependency of qiskit-aqua.
    
    (cherry picked from commit f0d53bd6f9f0cbd806d0f486e4bf7d4d69062f1b)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2285,6 +2285,8 @@ in {
 
   docker_pycreds = callPackage ../development/python-modules/docker-pycreds {};
 
+  docloud = callPackage ../development/python-modules/docloud { };
+
   docopt = callPackage ../development/python-modules/docopt { };
 
   doctest-ignore-unicode = callPackage ../development/python-modules/doctest-ignore-unicode { };
