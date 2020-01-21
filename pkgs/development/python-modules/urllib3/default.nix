fecd4ffcc798e83d038e4bf0689845c5ebf63757
Will Dietz <w@wdtz.org>
2020-01-21 02:40:57 -0600

    Revert "python: urllib3: 1.25.6 -> 1.25.7" (rebuilds)
    This reverts commit 1697dd78502c2d6c89376bd78f80b953d93d8a4d.

--- pkgs/development/python-modules/urllib3/default.nix
+++ pkgs/development/python-modules/urllib3/default.nix
@@ -4,11 +4,11 @@
 
 buildPythonPackage rec {
   pname = "urllib3";
-  version = "1.25.7";
+  version = "1.25.5";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "f3c5fd51747d450d4dcf6f923c81f78f811aab8205fda64b0aba34a4e48b0745";
+    sha256 = "1jy23d4a2i0kkhvsdmcjniaa0jdp2229ddbmks4wfayrlpzasgig";
   };
 
   outputs = [ "out" "dev" ];
