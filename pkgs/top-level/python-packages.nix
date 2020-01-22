49cd0f6d2c387122e2089e92f5de0879028029ca
Vincent Breitmoser <look@my.amazin.horse>
2020-01-22 13:18:01 +0100

    pythonPackages.etesync: init at 0.9.3
    (cherry picked from commit 5b244b986d79b99a692afaa745fe16f6924b6cef)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -3130,6 +3130,8 @@ in {
 
   et_xmlfile = callPackage ../development/python-modules/et_xmlfile { };
 
+  etesync = callPackage ../development/python-modules/etesync { };
+
   eventlet = callPackage ../development/python-modules/eventlet { };
 
   exifread = callPackage ../development/python-modules/exifread { };
