1f6ebc38b449e24b761a360822081d37443dc6ca
R. RyanTM <ryantm-bot@ryantm.com>
2020-01-22 07:48:38 +0000

    palemoon: 28.8.0 -> 28.8.1
    (cherry picked from commit 3aa2742e2ba21838f374db6330c4498b9468638e)

--- pkgs/applications/networking/browsers/palemoon/default.nix
+++ pkgs/applications/networking/browsers/palemoon/default.nix
@@ -13,14 +13,14 @@ let
 
 in stdenv.mkDerivation rec {
   pname = "palemoon";
-  version = "28.7.2";
+  version = "28.8.1";
 
   src = fetchFromGitHub {
     name   = "${pname}-${version}";
     owner  = "MoonchildProductions";
     repo   = "UXP";
     rev    = "PM${version}_Release";
-    sha256 = "08w90269mwcqsdhx4vvp18c5iccvzqhaaw6aw7w0nppf2f2k8lri";
+    sha256 = "055bmfgasxf7azjqry06bbgwx6ryrdc1zrcq8b217b6zb1in037x";
   };
 
   desktopItem = makeDesktopItem {
