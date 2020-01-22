ca084966e3f5c742c0835cba7eec543af2d59a08
Will Dietz <w@wdtz.org>
2020-01-22 08:20:07 -0600

    blobmoji: py3

--- pkgs/data/fonts/blobmoji/default.nix
+++ pkgs/data/fonts/blobmoji/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchFromGitHub, cairo, pngquant, zopfli
-, which, pythonPackages, pkgconfig, imagemagick }: stdenv.mkDerivation rec {
+, which, python3Packages, pkgconfig, imagemagick }: stdenv.mkDerivation rec {
   pname = "blobmoji";
   version = "2019-06-14-Emoji-12";
 
@@ -12,7 +12,7 @@
 
   buildInputs = [ cairo ];
   nativeBuildInputs = [ cairo pngquant zopfli which pkgconfig imagemagick ]
-                      ++ (with pythonPackages; [ python fonttools nototools ]);
+                      ++ (with python3Packages; [ python fonttools nototools ]);
   enableParallelBuilding = true;
 
   postPatch = ''
