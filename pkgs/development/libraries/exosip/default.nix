d3defc2e89211da34d7461d53aabcd304758c99a
Will Dietz <w@wdtz.org>
2020-01-22 07:43:36 -0600

    exosip: 4.1.0 -> 5.1.1, adjust tarball capitalization

--- pkgs/development/libraries/exosip/default.nix
+++ pkgs/development/libraries/exosip/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
  pname = "libexosip2";
- version = "4.1.0";
+ version = "5.1.1";
 
  src = fetchurl {
-    url = "mirror://savannah/exosip/libeXosip2-${version}.tar.gz";
-    sha256 = "17cna8kpc8nk1si419vgr6r42k2lda0rdk50vlxrw8rzg0xp2xrw";
+    url = "mirror://savannah/exosip/${pname}-${version}.tar.gz";
+    sha256 = "1qkj28kmx15sklw0q0visgqyddqjxjz6c5qn6vzra24fpw00qhi1";
   };
 
   nativeBuildInputs = [ pkgconfig ];
