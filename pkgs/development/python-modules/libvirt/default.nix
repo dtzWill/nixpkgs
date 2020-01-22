3b77e3eadf3eb74f6acb8c7d70aa51a68a939c56
Will Dietz <w@wdtz.org>
2020-01-22 08:13:18 -0600

    libvirt: 5.4.0 -> 6.0.0 (and perl/python bindings)

--- pkgs/development/python-modules/libvirt/default.nix
+++ pkgs/development/python-modules/libvirt/default.nix
@@ -2,12 +2,12 @@
 
 buildPythonPackage rec {
   pname = "libvirt";
-  version = "5.4.0";
+  version = "6.0.0";
 
   src = assert version == libvirt.version; fetchgit {
     url = git://libvirt.org/libvirt-python.git;
     rev = "v${version}";
-    sha256 = "0ja35z90i3m7vsjfpzfm7awkmja3h0150376i5pzmf2q8vp61fi5";
+    sha256 = "0p6a8mlqf1kb0pg1avygg6ymlfvqsk7qisyhmyaxkj4pn8q87cxy";
   };
 
   nativeBuildInputs = [ pkgconfig ];
