3bc0af223d434c71698a0b3a277beed19de0fa3a
Will Dietz <w@wdtz.org>
2020-01-22 13:21:11 -0600

    palemoon: drop hardeningDisable, maintainer (sync w/nixpkgs)

--- pkgs/applications/networking/browsers/palemoon/default.nix
+++ pkgs/applications/networking/browsers/palemoon/default.nix
@@ -92,8 +92,6 @@ in stdenv.mkDerivation rec {
     "
   '';
 
-  hardeningDisable = [ "format" ];
-
   buildPhase = ''
     $src/mach build
   '';
@@ -130,7 +128,7 @@ in stdenv.mkDerivation rec {
     '';
     homepage    = "https://www.palemoon.org/";
     license     = licenses.mpl20;
-    maintainers = with maintainers; [ rnhmjoj AndersonTorres OPNA2608 ];
+    maintainers = with maintainers; [ AndersonTorres OPNA2608 ];
     platforms   = [ "i686-linux" "x86_64-linux" ];
   };
 }
