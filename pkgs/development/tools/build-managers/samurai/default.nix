e4deee9bc55f12db29e221dcafce53a602314eda
Will Dietz <w@wdtz.org>
2020-01-22 18:15:05 -0600

    samurai: patch for compdb support (from upstream)

--- pkgs/development/tools/build-managers/samurai/default.nix
+++ pkgs/development/tools/build-managers/samurai/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, fetchpatch }:
 
 stdenv.mkDerivation rec {
   pname = "samurai";
@@ -10,6 +10,14 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "1jsxfpwa6q893x18qlvpsiym29rrw5cj0k805wgmk2n57j9rw4f2";
   };
+
+  # Add support for compdb tool (compilation database)!
+  patches = [
+    (fetchpatch {
+      url = "https://github.com/michaelforney/${pname}/compare/${src.rev}..9c1f015c21211c5df5822f70d3d5be81bd54fee2.patch";
+      sha256 = "1iczhijrrhdhn7qyadm6v1q3pcz4nd862h5zns9yxr5mvrl5djbb";
+    })
+  ];
 
   makeFlags = [ "DESTDIR=" "PREFIX=${placeholder "out"}" ];
 
