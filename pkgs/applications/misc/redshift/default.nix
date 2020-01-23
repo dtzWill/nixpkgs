5f1321f4a26fd5037c41172d0e59f249cfe41e1c
Will Dietz <w@wdtz.org>
2020-01-23 12:00:39 -0600

    redshift{,-wlr}: wlr fork already has patches, move to redshift-only

--- pkgs/applications/misc/redshift/default.nix
+++ pkgs/applications/misc/redshift/default.nix
@@ -13,34 +13,13 @@
 
 let
   mkRedshift =
-    { pname, version, src, meta }:
+    { pname, version, src,  meta, extraPatches ? [] }:
     stdenv.mkDerivation rec {
       inherit pname version src meta;
 
       patches = [
         # https://github.com/jonls/redshift/pull/575
         ./575.patch
-
-        (fetchpatch {
-          name = "docs-fix-config-file-path.patch";
-          url = "https://github.com/jonls/redshift/commit/001d4aeffadec94368cde5738b957740d2e36980.patch";
-          sha256 = "0dr09y1521jamh2p1r5vw23l9hv7yvnb396dghvdbp9glbappqls";
-        })
-        (fetchpatch {
-          name = "prefer-symbolic-icon-if-avail.patch";
-          url = "https://github.com/jonls/redshift/commit/dca8334646d8fefeb56332eb1535c595a6599c7d.patch";
-          sha256 = "1m0mv8imbikxybl79vm0d3yagr384j8sjv53ixj9wp37wf2a5sii";
-        })
-        (fetchpatch {
-          name = "allow-hash-comment-char.patch";
-          url = "https://github.com/jonls/redshift/commit/f7fbbb04c335cd844b97c80128145349854128f4.patch";
-          sha256 = "0h1zgdahg3h91gcv6w7migf1ja6k2p7r6l6bh870xqi8h71cpspq";
-        })
-        (fetchpatch {
-          name = "additional-suspend-durations.patch";
-          url = "https://github.com/jonls/redshift/commit/2e91da281e1161af594e07eed62b672f9b006a39.patch";
-          sha256 = "0dhf75l1x67g8nbwm70gsm5923diwvjlwhh5zyffn5ylbml81cg9";
-        })
       ];
 
       nativeBuildInputs = [
@@ -102,6 +81,31 @@ rec {
       rev = "v${version}";
       sha256 = "12cb4gaqkybp4bkkns8pam378izr2mwhr2iy04wkprs2v92j7bz6";
     };
+    
+    # Various nice improvements, already included in wayland fork
+    extraPatches = [
+      (fetchpatch {
+        name = "docs-fix-config-file-path.patch";
+        url = "https://github.com/jonls/redshift/commit/001d4aeffadec94368cde5738b957740d2e36980.patch";
+        sha256 = "0dr09y1521jamh2p1r5vw23l9hv7yvnb396dghvdbp9glbappqls";
+      })
+      (fetchpatch {
+        name = "prefer-symbolic-icon-if-avail.patch";
+        url = "https://github.com/jonls/redshift/commit/dca8334646d8fefeb56332eb1535c595a6599c7d.patch";
+        sha256 = "1m0mv8imbikxybl79vm0d3yagr384j8sjv53ixj9wp37wf2a5sii";
+      })
+      (fetchpatch {
+        name = "allow-hash-comment-char.patch";
+        url = "https://github.com/jonls/redshift/commit/f7fbbb04c335cd844b97c80128145349854128f4.patch";
+        sha256 = "0h1zgdahg3h91gcv6w7migf1ja6k2p7r6l6bh870xqi8h71cpspq";
+      })
+      (fetchpatch {
+        name = "additional-suspend-durations.patch";
+        url = "https://github.com/jonls/redshift/commit/2e91da281e1161af594e07eed62b672f9b006a39.patch";
+        sha256 = "0dhf75l1x67g8nbwm70gsm5923diwvjlwhh5zyffn5ylbml81cg9";
+      })
+    ];
+
 
     meta = with stdenv.lib; {
       description = "Screen color temperature manager";
