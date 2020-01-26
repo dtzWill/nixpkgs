b48a98cc3488ce35b00326bfade8a422b0e25114
Will Dietz <w@wdtz.org>
2020-01-26 15:17:18 -0600

    cvc4: back to 1.6, nvm

--- pkgs/applications/science/logic/cvc4/default.nix
+++ pkgs/applications/science/logic/cvc4/default.nix
@@ -1,39 +1,26 @@
-{ stdenv, fetchFromGitHub, cln, gmp, swig, pkgconfig, cmake
-, readline, libantlr3c, boost, jdk
+{ stdenv, fetchurl, cln, gmp, swig, pkgconfig
+, readline, libantlr3c, boost, jdk, autoreconfHook
 , python3, antlr3_4
 }:
 
 stdenv.mkDerivation rec {
-  pname = "cvc4";
-  version = "1.7";
+  name = "cvc4-${version}";
+  version = "1.6";
 
-  src = fetchFromGitHub {
-    owner = pname;
-    repo = pname;
-    rev = version;
-    sha256 = "0mi3ym9j3y00h66115q3jsj7a1wcxjc94fcsw2lxq899mviywk2z";
+  src = fetchurl {
+    url = "https://cvc4.cs.stanford.edu/downloads/builds/src/cvc4-${version}.tar.gz";
+    sha256 = "1iw793zsi48q91lxpf8xl8lnvv0jsj4whdad79rakywkm1gbs62w";
   };
 
-  nativeBuildInputs = [ pkgconfig cmake ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig ];
   buildInputs = [ gmp cln readline swig libantlr3c antlr3_4 boost jdk python3 ];
-  cmakeBuildType = "Production"; # "Release" not supported
-  cmakeFlags = [
-    "-DENABLE_GPL=ON"
-    #"-DENABLE_BEST" # needs more deps
-    "-DUSE_CLN=ON"
-    "-DUSE_READLINE=ON"
-
-    "-DBUILD_BINDINGS_JAVA=ON"
-    #"-DBUILD_BINDINGS_PYTHON=ON"
-    "-DUSE_PYTHON3=ON"
+  configureFlags = [
+    "--enable-language-bindings=c,c++,java"
+    "--enable-gpl"
+    "--with-cln"
+    "--with-readline"
+    "--with-boost=${boost.dev}"
   ];
-  #configureFlags = [
-  #  "--enable-language-bindings=c,c++,java"
-  #  "--enable-gpl"
-  #  "--with-cln"
-  #  "--with-readline"
-  #  "--with-boost=${boost.dev}"
-  #];
 
   prePatch = ''
     patch -p1 -i ${./minisat-fenv.patch} -d src/prop/minisat
