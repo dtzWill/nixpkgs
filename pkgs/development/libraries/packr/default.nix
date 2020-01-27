9e63994e502d2d733d226afcb7e421866d0535eb
Will Dietz <w@wdtz.org>
2020-01-27 07:48:28 -0600

    packr: touchup

--- pkgs/development/libraries/packr/default.nix
+++ pkgs/development/libraries/packr/default.nix
@@ -14,7 +14,9 @@ buildGoModule rec {
     sha256 = "0m5kl2fq8gf1v4vllgag2xl8fd382sdgqrcdb8f5alsnrdn08kb9";
   };
 
-  modSha256 = "086gydrl3i35hawb5m7rsb4a0llcpdpgid1xfw2z9n6jkwkclw4n";
+  subPackages = [ "packr" "v2/packr2" ];
+
+  modSha256 = "0afhkvivma16bi8rz3kwcsz9mhmcn4zm6rrymxkvazx6b844hcdv";
 
   meta = with lib; {
     description = "The simple and easy way to embed static files into Go binaries";
