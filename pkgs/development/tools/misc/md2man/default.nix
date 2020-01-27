b6f6e4f4c9cedd17ee6d21135502b6d0b8b4dcf8
Will Dietz <w@wdtz.org>
2020-01-27 07:38:55 -0600

    go-md2man: buildGoPackage

--- pkgs/development/tools/misc/md2man/default.nix
+++ pkgs/development/tools/misc/md2man/default.nix
@@ -1,8 +1,8 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ lib, buildGoPackage, fetchFromGitHub }:
 
 with lib;
 
-buildGoModule rec {
+buildGoPackage rec {
   pname = "go-md2man";
   version = "1.0.10";
 
@@ -14,8 +14,6 @@ buildGoModule rec {
     repo = "go-md2man";
     sha256 = "1bqkf2bvy1dns9zd24k81mh2p1zxsx2nhq5cj8dz2vgkv1xkh60i";
   };
-
-  modSha256 = "1cdgw84brpwpn2zhs6g0hb1i8fwx5l3fvjwy37ni1fshymqin0ag";
 
   meta = {
     description = "Go tool to convert markdown to man pages";
