7a674970b20a87cad0b39f1ebc1e537e83d82f0f
Will Dietz <w@wdtz.org>
2020-01-26 19:49:46 -0600

    Revert "libpipeline: 1.5.1 -> 1.5.2" (rebuilds, man-db)
    This reverts commit b129dad446704b715876cee718120a34bc4a5dd8.

--- pkgs/development/libraries/libpipeline/default.nix
+++ pkgs/development/libraries/libpipeline/default.nix
@@ -1,11 +1,11 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  name = "libpipeline-1.5.2";
+  name = "libpipeline-1.5.1";
 
   src = fetchurl {
     url = "mirror://savannah/libpipeline/${name}.tar.gz";
-    sha256 = "1ysrn22ixd4nmggy6f7qcsm7waadmlbg2i0n9mh6g7dfq54wcngx";
+    sha256 = "0bwh5xz5f2czwb7f564jz1mp4znm8pldnvf65fs0hpw4gmmp0cyn";
   };
 
   patches = stdenv.lib.optionals stdenv.isDarwin [ ./fix-on-osx.patch ];
