17ab135cd3ed28abd4d17da4d8fe27c2008e8626
Will Dietz <w@wdtz.org>
2020-01-27 20:00:04 -0600

    keybase-git: fix pos

--- pkgs/tools/security/keybase/git.nix
+++ pkgs/tools/security/keybase/git.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchFromGitHub, keybase }:
 
-keybase.overrideAttrs (o: rec {
+keybase.overrideAttrs (o: let attrs = rec {
   pname = "keybase";
   version = "5.1.1";
   name = "${pname}-${version}";
@@ -11,4 +11,6 @@ keybase.overrideAttrs (o: rec {
     rev = "v${version}";
     sha256 = "06rhnmk59was7dd5pd4czlrwqvmh02l9gka9561dkpl6w3j6814a";
   };
-})
+
+  pos = builtins.unsafeGetAttrPos "version" attrs;
+}; in attrs)
