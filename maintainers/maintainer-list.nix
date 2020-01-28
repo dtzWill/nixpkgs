c0e1e2cdcb848d4832946e5f715a65e311507ac0
foxit64 <foxit64@users.noreply.github.com>
2020-01-28 14:48:10 +0100

    maintainers: add foxit64
    (cherry picked from commit 39cc559403366e781305c26e72d0136059d9d055)

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -7569,4 +7569,10 @@
     githubId = 8686360;
     name = "Illia Shestakov";
   };
+  foxit64 = {
+    email = "o4nsxy05@gmail.com";
+    github = "foxit64";
+    githubId = 56247270;
+    name = "Foxit";
+  };
 }
