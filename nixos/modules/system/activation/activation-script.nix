2a6a578b8f816f4b5e6b03d48d73c155ced209ef
Will Dietz <w@wdtz.org>
2020-01-27 22:34:47 -0600

    activation-script: 'rm -d'? hmm not if two share same dir...

--- nixos/modules/system/activation/activation-script.nix
+++ nixos/modules/system/activation/activation-script.nix
@@ -218,8 +218,7 @@ in
               mv -f ${target}.tmp ${target} # atomically replace
             '' else ''
               rm -f ${target}
-              [ -d $(dirname ${target}) ] && \
-                rmdir --ignore-fail-on-non-empty $(dirname ${target})
+              rm -f -d $(dirname ${target})
             '')
           {
             "i686-linux"   ."/lib/ld-linux.so.2"          = "${pkgs.glibc.out}/lib/ld-linux.so.2";
