4a66ce98d39d712a7c58f8971c9e5964da34129e
Will Dietz <w@wdtz.org>
2020-01-29 19:08:32 -0600

    vanilla-dmz: mirror://debian

--- pkgs/data/icons/vanilla-dmz/default.nix
+++ pkgs/data/icons/vanilla-dmz/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "vanilla-dmz-${version}";
   version = "0.4.4";
   src = fetchzip {
-    url = "http://ftp.de.debian.org/debian/pool/main/d/dmz-cursor-theme/dmz-cursor-theme_${version}.tar.gz";
+    url = "mirror://debian/pool/main/d/dmz-cursor-theme/dmz-cursor-theme_${version}.tar.gz";
     sha256 = "1l0c0svk7dy0d7icg7j2181wdn3fvks5gmyqnvjk749ppy5ks8mj";
   };
   buildInputs = [ xorg.xcursorgen ];
