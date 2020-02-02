31f0b63bcc4e0e4d15f6889b44b768b34e4a5432
Will Dietz <w@wdtz.org>
2020-02-02 12:38:58 -0600

    cheat: 3.0.3 -> 3.4.1, new home

--- pkgs/applications/misc/cheat/default.nix
+++ pkgs/applications/misc/cheat/default.nix
@@ -2,23 +2,23 @@
 
 buildGoModule rec {
   pname = "cheat";
-  version = "3.0.3";
+  version = "3.4.1";
 
   src = fetchFromGitHub {
-    owner = "chrisallenlane";
+    owner = "cheat";
     repo = "cheat";
     rev = version;
-    sha256 = "19w1admdcgld9vlc4fsyc5d9bi6rmwhr2x2ji43za2vjlk34hnnx";
+    sha256 = "07pi1pfy8nk5bc4xhq0xq4nr456r5609i0mxg55q4kxy1530infd";
   };
 
   subPackages = [ "cmd/cheat" ];
 
-  modSha256 = "189cqnfl403f4lk7g9v68mwk93ciglqli639dk4x9091lvn5gq5q";
+  modSha256 = "0d4rk5zfhl4zf5n8f5k6kyk3yn5vdbb7hz3wgk8cfygwrbmblkc4";
 
   meta = with stdenv.lib; {
     description = "Create and view interactive cheatsheets on the command-line";
     maintainers = with maintainers; [ mic92 ];
     license = with licenses; [ gpl3 mit ];
-    homepage = "https://github.com/chrisallenlane/cheat";
+    homepage = "https://github.com/cheat/cheat";
   };
 }
