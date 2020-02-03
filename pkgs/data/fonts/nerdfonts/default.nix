20747b48371312c0b5e7c30d205a09b0ed9ccbc9
Chuck <chuck@intelligence.org>
2020-02-03 10:01:30 -0800

    nerdfonts: 2.0.0 -> 2.1.0 (DTZ: pname only)
    This release adds 216 fonts.
    
    Note: The Meslo LG {L,M,S}{ DZ,} Regular Nerd Font Complete{, Mono}.otf
    fonts are now .ttf files instead of .otf files in this release.
    
    (cherry picked from commit 384ab839e93e7dee690c24dbabb21340bd6e838d)

--- pkgs/data/fonts/nerdfonts/default.nix
+++ pkgs/data/fonts/nerdfonts/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   version = "2.1.0";
-  name = "nerdfonts-${version}";
+  pname = "nerdfonts";
   src = fetchFromGitHub {
     owner = "ryanoasis";
     repo = "nerd-fonts";
