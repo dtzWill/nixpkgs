6678df1f68cfde54bae626aae38bf927b0f97525
Will Dietz <w@wdtz.org>
2020-02-03 03:04:01 -0600

    elementary-planner: use system themes?

--- pkgs/applications/office/elementary-planner/default.nix
+++ pkgs/applications/office/elementary-planner/default.nix
@@ -48,6 +48,8 @@ stdenv.mkDerivation rec {
     pantheon.elementary-icon-theme
   ];
 
+  patches = [ ./dont-force-elementary-themes.patch ];
+
   postPatch = ''
     chmod +x build-aux/meson/post_install.py
     patchShebangs build-aux/meson/post_install.py
