747230baf68a33327c972a8d71d149ab1fc017f2
Will Dietz <w@wdtz.org>
2020-02-03 01:08:10 -0600

    xterm: 352 -> 353

--- pkgs/applications/misc/xterm/default.nix
+++ pkgs/applications/misc/xterm/default.nix
@@ -4,14 +4,14 @@
 
 stdenv.mkDerivation rec {
   pname = "xterm";
-  version = "352";
+  version = "353";
 
   src = fetchurl {
     urls = [
      "ftp://ftp.invisible-island.net/${pname}/${pname}-${version}.tgz"
      "https://invisible-mirror.net/archives/${pname}/${pname}-${version}.tgz"
    ];
-    sha256 = "0znma9ffj1dd22f2sfjgn3wx46qbv5bm5c12hy6fsdsk1vdghhkk";
+    sha256 = "0s5pkfn4r8iy09s1q1y78zhnr9f3sm6wgbqir7azaqggkppd68g5";
   };
 
   buildInputs =
