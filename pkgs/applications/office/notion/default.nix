c9753941f61c53f8a5f9591d2bb236af7a76b06b
Will Dietz <w@wdtz.org>
2020-02-03 22:55:17 -0600

    notion-app: default electron

--- pkgs/applications/office/notion/default.nix
+++ pkgs/applications/office/notion/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, dmg2img, p7zip, libicns, makeWrapper, electron_3 }:
+{ stdenv, fetchurl, dmg2img, p7zip, libicns, makeWrapper, electron }:
 
 stdenv.mkDerivation rec {
   pname = "notion-app";
@@ -39,7 +39,7 @@ stdenv.mkDerivation rec {
     # share/icons?
     cp -v Notion_512x512x32.png $out/share/notion-app/
 
-    makeWrapper ${electron_3}/bin/electron $out/bin/notion-app \
+    makeWrapper ${electron}/bin/electron $out/bin/notion-app \
       --run "cd $out/share/notion-app" \
       --add-flags "$out/share/notion-app/app.asar"
 
