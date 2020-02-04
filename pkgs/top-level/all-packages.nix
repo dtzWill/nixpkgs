da01b65ca16d6e2392acbd2fe109b6f8c565d430
Will Dietz <w@wdtz.org>
2020-02-03 23:07:09 -0600

    electron_3: drop, unsupported and recently unused

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9516,7 +9516,6 @@ in
   inherit (callPackages ../development/tools/electron { })
     electron_4 electron_5 electron_6 electron_7 electron_8;
 
-  electron_3 = callPackage ../development/tools/electron/3.x.nix { };
   electron = electron_8;
 
   autobuild = callPackage ../development/tools/misc/autobuild { };
