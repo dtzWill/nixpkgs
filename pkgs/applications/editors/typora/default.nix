632dc950d664dd454e826b61a5a0c4b34094bf66
Will Dietz <w@wdtz.org>
2020-02-03 22:40:31 -0600

    typora: electron default

--- pkgs/applications/editors/typora/default.nix
+++ pkgs/applications/editors/typora/default.nix
@@ -2,7 +2,7 @@
 , lib
 , fetchurl
 , makeWrapper
-, electron_5
+, electron
 , dpkg
 , gtk3
 , glib
@@ -51,7 +51,7 @@ stdenv.mkDerivation rec {
   '';
 
   postFixup = ''
-    makeWrapper ${electron_5}/bin/electron $out/bin/typora \
+    makeWrapper ${electron}/bin/electron $out/bin/typora \
       --add-flags $out/share/typora \
       "''${gappsWrapperArgs[@]}" \
       ${lib.optionalString withPandoc ''--prefix PATH : "${lib.makeBinPath [ pandoc ]}"''} \
