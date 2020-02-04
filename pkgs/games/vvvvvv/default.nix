3826aa15751a8572be7988be109972d6ea850322
Will Dietz <w@wdtz.org>
2020-02-04 01:14:25 -0600

    vvvvvv: sync w/PR 79068

--- pkgs/games/vvvvvv/default.nix
+++ pkgs/games/vvvvvv/default.nix
@@ -1,9 +1,10 @@
-{ stdenv, fetchurl, fetchFromGitHub, requireFile
+{ stdenv, fetchurl, fetchFromGitHub, requireFile, makeWrapper
 , SDL2, SDL2_mixer, cmake, ninja
 , fullGame ? false }:
 
 let
   dataZip = if fullGame then requireFile {
+    # the data file for the full game
     name = "data.zip";
     sha256 = "1q2pzscrglmwfgdl8yj300wymwskh51iq66l4xcd0qk0q3g3rbkg";
     message = ''
@@ -14,41 +15,47 @@ let
       nix-prefetch-url file://\$PWD/data.zip
     '';
   } else fetchurl {
+    # the data file for the free Make and Play edition
     url = https://thelettervsixtim.es/makeandplay/data.zip;
+    name = "mapdata.zip";
     sha256 = "1q2pzscrglmwfgdl8yj300wymwskh51iq66l4xcd0qk0q3g3rbkg";
   };
+
+  # if the user does not own the full game, build the Make and Play edition
+  flags = if fullGame then [] else [ "-DMAKEANDPLAY" ];
 in stdenv.mkDerivation rec {
   pname = "vvvvvv";
-  version = "unstable-2020-01-22";
+  version = "unstable-2020-02-02";
 
   src = fetchFromGitHub {
     owner = "TerryCavanagh";
     repo = "VVVVVV";
-    rev = "f9525020bbcb4c2f5c44218cadcd475bcc1d2037";
-    sha256 = "1z5lp456s4lvbihdr5mfqrhxj9b84rnacj5cd6zzsyf1pf6wifp6";
+    rev = "4bc76416f551253452012d28e2bc049087e2be73";
+    sha256 = "1sc64f7sxf063bdgnkg23vc170chq2ix25gs836hyswx98iyg5ly";
   };
 
-  nativeBuildInputs = [ cmake ninja ];
+  CFLAGS = flags;
+  CXXFLAGS = flags;
+
+  nativeBuildInputs = [ cmake ninja makeWrapper ];
   buildInputs = [ SDL2 SDL2_mixer ];
 
-  preConfigure = ''
-    cd desktop_version
-  '';
+  sourceRoot = "source/desktop_version";
 
   installPhase = ''
-    mkdir -p $out/bin
-    cp -t $out/bin VVVVVV
-    cp ${dataZip} $out/bin/data.zip
+    install -d $out/bin
+    install -t $out/bin VVVVVV
+    wrapProgram $out/bin/VVVVVV --add-flags "-assets ${dataZip}"
   '';
 
   meta = with stdenv.lib; {
-    description = "A platform game based on flipping gravity";
+    description = "A retro-styled platform game";
     longDescription = ''
       VVVVVV is a platform game all about exploring one simple mechanical
-      idea - what if you reversed gravity instead of jumping? 
+      idea - what if you reversed gravity instead of jumping?
     '';
-    homepage = https://thelettervsixtim.es;
-    license = licenses.unfree;
+    homepage = "https://thelettervsixtim.es";
+    license = if fullGame then licenses.unfree else licenses.unfreeRedistributable;
     maintainers = [ maintainers.dkudriavtsev ];
     platforms = platforms.all;
   };
