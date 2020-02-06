2e09a5a9c53c2b512cd142aab9f0bbd36ec12818
Will Dietz <w@wdtz.org>
2020-02-06 04:32:52 -0600

    google-fonts: 2020-02-05

--- pkgs/data/fonts/google-fonts/default.nix
+++ pkgs/data/fonts/google-fonts/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "google-fonts";
-  version = "unstable-2020-01-29";
+  version = "unstable-2020-02-05";
 
   src = fetchFromGitHub {
     owner = "google";
     repo = "fonts";
-    rev = "3959f16f7099c99142bb40123c042fa40949d6b4";
-    sha256 = "09vrb3w1jpwcxpww01x1m8sx4jk68ly5f6kf75fyja191pvgxf17";
+    rev = "5050dd268f0e06faf74360fb4147814a44eedc2a";
+    sha256 = "18icf9jhkix87n3mc1qbkwb9b4fs4r9bxbv1ra8yw89fs6kg06mk";
   };
 
   phases = [ "unpackPhase" "patchPhase" "installPhase" ];
