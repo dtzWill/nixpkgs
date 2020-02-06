a3794599403894d63904192dc9370d06c0564fb4
Will Dietz <w@wdtz.org>
2020-02-05 21:14:21 -0600

    smokeping: 2.6.11 -> 2.7.3, wip

--- pkgs/tools/networking/smokeping/default.nix
+++ pkgs/tools/networking/smokeping/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   name = "smokeping-${version}";
-  version = "2.6.11";
+  version = "2.7.3";
   src = fetchurl {
     url = "https://oss.oetiker.ch/smokeping/pub/smokeping-${version}.tar.gz";
-    sha256 = "1p9hpa2zs33p7hzrds80kwrm5255s0869v3s3qmsyx2sx63c7czj";
+    sha256 = "1d1nimi9iq1072h5j64dsfyrx5y05w2jqxvzi650d554620da3s3";
   };
   propagatedBuildInputs = [ rrdtool ] ++
     (with perlPackages; [ perl FCGI CGI CGIFast ConfigGrammar DigestHMAC NetTelnet
-      NetOpenSSH NetSNMP LWP IOTty fping NetDNS perlldap ]);
+      NetOpenSSH NetSNMP LWP IOTty fping NetDNS perlldap NetSSLeay IOSocketSSL MozillaCA ]);
 
   postInstall = ''
     mv $out/htdocs/smokeping.fcgi.dist $out/htdocs/smokeping.fcgi
