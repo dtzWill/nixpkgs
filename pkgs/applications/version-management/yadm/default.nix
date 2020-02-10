9ee7c2e806adc999de33ffd2a617f9dc9615a15e
R. RyanTM <ryantm-bot@ryantm.com>
2020-02-10 21:57:25 +0000

    yadm: 2.3.0 -> 2.4.0
    (cherry picked from commit 9a4eb10862e0dbed3cd5caea585d00f74c1dc32c)

--- pkgs/applications/version-management/yadm/default.nix
+++ pkgs/applications/version-management/yadm/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchFromGitHub }:
 
-let version = "1.12.0"; in
+let version = "2.4.0"; in
 stdenv.mkDerivation {
   name = "yadm-${version}";
 
   src = fetchFromGitHub {
     owner  = "TheLocehiliosan";
     repo   = "yadm";
-    rev    = "${version}";
-    sha256 = "0873jgks7dpfkj5km1jchxdrhf7lia70p0f8zsrh9p4crj5f4pc6";
+    rev    = version;
+    sha256 = "0kpahznrkxkyj92vrhwjvldg2affi1askgwvpgbs4mg40f92szlp";
   };
 
   buildCommand = ''
