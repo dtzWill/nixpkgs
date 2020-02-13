39465e85d8adc719de2937e8ec79040aee65206f
Will Dietz <w@wdtz.org>
2020-02-13 11:12:51 -0600

    featherpad: qmake -> cmake

--- pkgs/applications/editors/featherpad/default.nix
+++ pkgs/applications/editors/featherpad/default.nix
@@ -1,5 +1,5 @@
 { mkDerivation, lib, fetchFromGitHub, pkgconfig, hunspell
-, qmake, qttools, qtbase, qtsvg, qtx11extras }:
+, cmake, qttools, qtbase, qtsvg, qtx11extras }:
 
 mkDerivation rec {
   version = "0.12.1";
@@ -10,7 +10,7 @@ mkDerivation rec {
     rev = "V${version}";
     sha256 = "12d48zp41nszfxqldfaj1fxg7426r25bipjajwv25pvgw3myvy7s";
   };
-  nativeBuildInputs = [ qmake pkgconfig qttools ];
+  nativeBuildInputs = [ cmake pkgconfig qttools ];
   buildInputs = [ qtbase qtsvg qtx11extras hunspell ];
   meta = with lib; {
     description = "Lightweight Qt5 Plain-Text Editor for Linux";
