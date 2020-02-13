a904b26b4d84003c370a69501d98224d17cfe236
Will Dietz <w@wdtz.org>
2020-02-12 18:06:28 -0600

    metamath: sync

--- pkgs/development/interpreters/metamath/default.nix
+++ pkgs/development/interpreters/metamath/default.nix
@@ -6,16 +6,18 @@ stdenv.mkDerivation {
 
   buildInputs = [ autoreconfHook ];
 
-  # This points to my own repository because there is no official repository
-  # for metamath; there's a download location but it gets updated in place with
-  # no permanent link. See discussion at
-  # https://groups.google.com/forum/#!topic/metamath/N4WEWQQVUfY
   src = fetchFromGitHub {
     owner = "metamath";
     repo = "metamath-exe";
     rev = "469e1b253f29be838411e2cc9c93d7704297059c";
     sha256 = "0nazi7z8qrpn7nnmxk99ilwf8smkzh26jcvn17wyfnywxpdsb7wa";
   };
+
+  # the files necessary to build the DATA target are not in this distribution
+  # luckily, they're not really needed so we don't build it.
+  makeFlags = [ "DATA=" ];
+
+  installTargets = [ "install-exec" ];
 
   meta = with stdenv.lib; {
     description = "Interpreter for the metamath proof language";
