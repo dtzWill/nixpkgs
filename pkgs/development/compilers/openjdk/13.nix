97aa03f6b80fe763dda5dbaad1acfb033b6035d5
Will Dietz <w@wdtz.org>
2020-02-14 12:49:15 -0600

    openjdk13: drop swing gtk patch similarly
    Glance at the patched file from latest sources suggests this has been fixed

--- pkgs/development/compilers/openjdk/13.nix
+++ pkgs/development/compilers/openjdk/13.nix
@@ -43,8 +43,6 @@ let
         url = https://src.fedoraproject.org/rpms/java-openjdk/raw/06c001c7d87f2e9fe4fedeef2d993bcd5d7afa2a/f/rh1673833-remove_removal_of_wformat_during_test_compilation.patch;
         sha256 = "082lmc30x64x583vqq00c8y0wqih3y4r0mp1c4bqq36l22qv6b6r";
       })
-    ] ++ lib.optionals (!headless && enableGnome2) [
-      ./swing-use-gtk-jdk10.patch
     ];
 
     prePatch = ''
