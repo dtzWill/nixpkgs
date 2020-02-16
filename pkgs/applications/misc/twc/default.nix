489e09c045af3045dd6a300c9cfc06aa8fc32832
Will Dietz <w@wdtz.org>
2020-02-15 23:03:44 -0600

    twc: deps, try using our newer deps (hopeful-face)

--- pkgs/applications/misc/twc/default.nix
+++ pkgs/applications/misc/twc/default.nix
@@ -1,8 +1,21 @@
-{ lib, buildPythonApplication, fetchPypi,
-tasklib, prompt_toolkit, attrs, mgcomm
-}:
+{ lib, python3, fetchPypi }:
 
-buildPythonApplication rec {
+# TODO: refactor this into separate file, as it should be :)
+let
+  python = python3.override {
+    packageOverrides = self: super: {
+      mgcomm = super.buildPythonPackage rec {
+        pname = "mgcomm";
+        version = "0.2.0";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "007fc1s1filbf0xi4025p1bnd7shj85q9gki8s58vdrgy75081xk";
+        };
+        nativeBuildInputs = [ super.setuptools_scm ];
+      };
+    };
+  };
+in with python.pkgs; buildPythonApplication rec {
   pname = "twc";
   version = "0.8.0";
 
@@ -11,6 +24,14 @@ buildPythonApplication rec {
     sha256 = "1vs2484mplwjsi17c2yb6ypfdj0ikh5nvgksdvxzp2x9nabqa5da";
   };
 
+  postPatch = ''
+    substituteInPlace setup.py \
+      --replace 'tasklib==1.1.0' 'tasklib' \
+      --replace 'attrs==19.1.0' 'attrs' \
+      --replace 'prompt_toolkit==2.0.9' 'prompt_toolkit'
+  '';
+
+  nativeBuildInputs = [ setuptools_scm ];
   buildInputs = [
     tasklib prompt_toolkit attrs mgcomm
   ];
