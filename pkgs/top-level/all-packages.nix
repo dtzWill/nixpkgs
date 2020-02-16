84a6e34103205862491762ce590ceb448c9eb0c8
nyanloutre <paul@nyanlout.re>
2020-02-16 16:52:24 +0100

    jftui: init at 0.2.2
    (cherry picked from commit f7e8e15cb7d110975080d161e497fcd80daf8c13)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20148,6 +20148,8 @@ in
     inherit (darwin) libobjc;
   };
 
+  jftui = callPackage ../applications/video/jftui { };
+
   lilyterm = callPackage ../applications/misc/lilyterm {
     inherit (gnome2) vte;
     gtk = gtk2;
