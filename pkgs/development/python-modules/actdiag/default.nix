58e8856771801536f527d80428a5f02387e94cae
Will Dietz <w@wdtz.org>
2020-02-16 22:18:57 -0600

    actdiag: drop included patch

--- pkgs/development/python-modules/actdiag/default.nix
+++ pkgs/development/python-modules/actdiag/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, buildPythonPackage, fetchPypi, fetchpatch
+{ stdenv, buildPythonPackage, fetchPypi
 , pep8, nose, unittest2, docutils, blockdiag, reportlab }:
 
 buildPythonPackage rec {
@@ -8,12 +8,6 @@ buildPythonPackage rec {
   src = fetchPypi {
     inherit pname version;
     sha256 = "0g51v9dmdq18z33v332f1f0cmb3hqgaga5minj0mc2sglark1s7h";
-  };
-
-  patches = fetchpatch {
-    name = "drop_test_pep8.py.patch";
-    url = https://bitbucket.org/blockdiag/actdiag/commits/c1f2ed5947a1e93291f5860e4e30cee098bd635d/raw;
-    sha256 = "1zxzwb0fvwlc8xgs45fx65341sjhb3h6l2p6rdj6i127vg1hsxb4";
   };
 
   buildInputs = [ pep8 nose unittest2 docutils ];
