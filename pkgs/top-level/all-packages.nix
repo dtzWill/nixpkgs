ac48368f536eeaee5eff781ecd03f98027d0a879
Justin Humm <justin.humm@posteo.de>
2020-02-17 23:04:05 +0100

    fontpreview: init at 1.0.6
    (cherry picked from commit 2811cf8bd85f2908852ea267db11fd637f7ae054)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -19183,6 +19183,8 @@ in
 
   font-manager = callPackage ../applications/misc/font-manager { };
 
+  fontpreview = callPackage ../applications/misc/fontpreview { };
+
   foo-yc20 = callPackage ../applications/audio/foo-yc20 { };
 
   fossil = callPackage ../applications/version-management/fossil { };
