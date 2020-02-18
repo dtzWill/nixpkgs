ba856dd56fe5879bfea48fada3fca1f9bd93ee96
ajs124 <git@ajs124.de>
2020-02-18 03:57:05 +0100

    mimalloc: 1.1.0 -> 1.6.1
    (cherry picked from commit 194f3aa4b548330af1bac5bc526e151a457d3db5)

--- pkgs/development/libraries/mimalloc/default.nix
+++ pkgs/development/libraries/mimalloc/default.nix
@@ -7,40 +7,42 @@ let
 in
 stdenv.mkDerivation rec {
   pname   = "mimalloc";
-  version = "1.6.0";
-  ver = stdenv.lib.versions.majorMinor version;
+  version = "1.6.1";
 
   src = fetchFromGitHub {
     owner  = "microsoft";
     repo   = pname;
-    rev    = "refs/tags/v${version}";
-    sha256 = "1x2jf0myfbz3k23jh6xfg603xzx9g40glbj6pbqk1ak1habdrqj3";
+    rev    = "v${version}";
+    sha256 = "1zql498587wvb0gaavnzxj2zm535sgm22x0sjgl4ncfk7ragnv9c";
   };
 
   nativeBuildInputs = [ cmake ninja ];
   enableParallelBuilding = true;
   cmakeFlags = stdenv.lib.optional secureBuild [ "-DMI_SECURE=ON" ];
 
-  postInstall = ''
+  postInstall = let
+    rel = stdenv.lib.versions.majorMinor version;
+  in ''
     # first, install headers, that's easy
     mkdir -p $dev
     mv $out/lib/*/include $dev/include
 
     # move .a and .o files into place
-    mv $out/lib/mimalloc-${ver}/libmimalloc*.a           $out/lib/libmimalloc.a
-    mv $out/lib/mimalloc-${ver}/mimalloc*.o              $out/lib/mimalloc.o
+    find $out/lib
+    mv $out/lib/mimalloc-${rel}/libmimalloc*.a           $out/lib/libmimalloc.a
+    mv $out/lib/mimalloc-${rel}/mimalloc*.o              $out/lib/mimalloc.o
 
   '' + (if secureBuild then ''
-    mv $out/lib/mimalloc-${ver}/libmimalloc-secure${soext}.${ver} $out/lib/libmimalloc-secure${soext}.${ver}
-    ln -sfv $out/lib/libmimalloc-secure${soext}.${ver} $out/lib/libmimalloc-secure${soext}
-    ln -sfv $out/lib/libmimalloc-secure${soext}.${ver} $out/lib/libmimalloc${soext}
+    mv $out/lib/mimalloc-${rel}/libmimalloc-secure${soext}.${rel} $out/lib/libmimalloc-secure${soext}.${rel}
+    ln -sfv $out/lib/libmimalloc-secure${soext}.${rel} $out/lib/libmimalloc-secure${soext}
+    ln -sfv $out/lib/libmimalloc-secure${soext}.${rel} $out/lib/libmimalloc${soext}
   '' else ''
-    mv $out/lib/mimalloc-${ver}/libmimalloc${soext}.${ver} $out/lib/libmimalloc${soext}.${ver}
-    ln -sfv $out/lib/libmimalloc${soext}.${ver} $out/lib/libmimalloc${soext}
+    mv $out/lib/mimalloc-${rel}/libmimalloc${soext}.${rel} $out/lib/libmimalloc${soext}.${rel}
+    ln -sfv $out/lib/libmimalloc${soext}.${rel} $out/lib/libmimalloc${soext}
   '') + ''
     # remote duplicate dir. FIXME: try to fix the .cmake file distribution
     # so we can re-use it for dependencies...
-    rm -rf $out/lib/mimalloc-${ver}
+    rm -rf $out/lib/mimalloc-${rel}
   '';
 
   outputs = [ "out" "dev" ];
@@ -51,5 +53,6 @@ stdenv.mkDerivation rec {
     license     = licenses.bsd2;
     platforms   = platforms.unix;
     maintainers = with maintainers; [ thoughtpolice ];
+    badPlatforms = platforms.darwin;
   };
 }
