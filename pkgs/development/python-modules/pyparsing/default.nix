991f40212883e8201093751cf1b8cd9d75b453c6
Will Dietz <w@wdtz.org>
2020-02-18 17:47:44 -0600

    Revert "pyparsing: sync w/nixpkgs master" (revisit, rebuilds)
    This reverts commit dff27e8c56b1d2d790b4586ea556f0eda0f97d45.

--- pkgs/development/python-modules/pyparsing/default.nix
+++ pkgs/development/python-modules/pyparsing/default.nix
@@ -1,35 +1,19 @@
-{ buildPythonPackage
-, fetchFromGitHub
-, lib
-
-# pythonPackages
-, coverage
-}:
-
+{ stdenv, buildPythonPackage, fetchPypi }:
 buildPythonPackage rec {
-  pname = "pyparsing";
-  version = "2.4.6";
+    pname = "pyparsing";
+    version = "2.4.0";
 
-  src = fetchFromGitHub {
-    owner = "pyparsing";
-    repo = pname;
-    rev = "pyparsing_${version}";
-    sha256 = "1fh7s3cfr274pd6hh6zygl99842rqws98an2nkrrqj2spb9ldxcm";
-  };
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "0anb80c0sn7mjnrj6gs3by8yn34rzw0z5fj6jx78y4gw44rw0wqq";
+    };
 
-  # https://github.com/pyparsing/pyparsing/blob/847af590154743bae61a32c3dc1a6c2a19009f42/tox.ini#L6
-  checkInputs = [ coverage ];
-  checkPhase = ''
-    coverage run --branch simple_unit_tests.py
-    coverage run --branch unitTests.py
-  '';
+    # Not everything necessary to run the tests is included in the distribution
+    doCheck = false;
 
-  meta = with lib; {
-    homepage = "https://github.com/pyparsing/pyparsing";
-    description = "An alternative approach to creating and executing simple grammars, vs. the traditional lex/yacc approach, or the use of regular expressions";
-    license = licenses.mit;
-    maintainers = with maintainers; [
-      kamadorueda
-    ];
-  };
+    meta = with stdenv.lib; {
+      homepage = https://github.com/pyparsing/pyparsing;
+      description = "An alternative approach to creating and executing simple grammars, vs. the traditional lex/yacc approach, or the use of regular expressions";
+      license = licenses.mit;
+    };
 }
