bcd0b662d5cb4574e7360c20b3baa983670d2b64
Konrad Borowski <konrad@borowski.pw>
2020-02-18 13:55:56 +0100

    pythonPackages.decorator: fix Python 2 build
    (cherry picked from commit 686274ea62caf12ca5181e8979e3fc52233402ed)

--- pkgs/development/python-modules/decorator/default.nix
+++ pkgs/development/python-modules/decorator/default.nix
@@ -1,5 +1,6 @@
 { lib
 , buildPythonPackage
+, fetchpatch
 , fetchPypi
 }:
 
@@ -11,6 +12,13 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "1pi54wqj2p6ka13x7q8d5zgqg9bcf7m5d00l7x5bi204qmhn65c6";
   };
+
+  patches = [
+    (fetchpatch {
+      url = https://github.com/micheles/decorator/commit/3265f2755d16c0a3dfc9f1feee39722ddc11ee80.patch;
+      sha256 = "1q5nmff30vccqq5swf2ivm8cn7x3lhz8c9qpj0zddgs2y7fw8syz";
+    })
+  ];
 
   meta = with lib; {
     homepage = https://pypi.python.org/pypi/decorator;
