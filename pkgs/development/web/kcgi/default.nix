9f6a793db7a841f36c68956ea405453ecc058ca2
Will Dietz <w@wdtz.org>
2020-02-18 21:58:17 -0600

    kcgi: 0.10.8 -> 0.10.13, bmake/curl, tests

--- pkgs/development/web/kcgi/default.nix
+++ pkgs/development/web/kcgi/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, pkgconfig, fetchFromGitHub, libbsd }:
+{ stdenv, pkgconfig, fetchFromGitHub, libbsd, bmake, curl }:
 
 stdenv.mkDerivation rec {
   pname = "kcgi";
@@ -12,16 +12,35 @@ stdenv.mkDerivation rec {
     rev = "VERSION_${underscoreVersion}";
     sha256 = "18idyqa798y0yna462d0pd5j87810h7brqzsijw1jmnv8agk7xz6";
   };
-  patchPhase = ''substituteInPlace configure \
-    --replace /usr/local /
+  patchPhase = ''
+    substituteInPlace configure --replace /usr/local /
+    substituteInPlace Makefile --replace /bin/echo echo
   '';
   
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ ] ++ stdenv.lib.optionals stdenv.isLinux [ libbsd ] ;
+  nativeBuildInputs = [ bmake pkgconfig ];
+  buildInputs = [ curl ] ++ stdenv.lib.optionals stdenv.isLinux [ libbsd ] ;
+
+  buildPhase = ''
+    runHook preBuild
+    bmake
+    runHook postBuild
+  '';
+
+  doCheck = true;
+
+  checkPhase = ''
+    runHook preCheck
+    bmake regress
+    runHook postCheck
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    bmake DESTDIR=${placeholder "out"} install
+    runHook postInstall
+  '';
 
   dontAddPrefix = true;
-
-  installFlags = [ "DESTDIR=$(out)" ];
 
   meta = with stdenv.lib; {
     homepage = https://kristaps.bsd.lv/kcgi;
