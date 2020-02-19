52fac5735ef5526bd5f834e7cfa7017430157967
R. RyanTM <ryantm-bot@ryantm.com>
2020-02-19 10:58:07 +0000

    picard: 2.2.3 -> 2.3
    (cherry picked from commit 8df76db7390cef3db68b278fc18ad7bf8c75a1c6)

--- pkgs/applications/audio/picard/default.nix
+++ pkgs/applications/audio/picard/default.nix
@@ -4,13 +4,13 @@ let
   pythonPackages = python3Packages;
 in pythonPackages.buildPythonApplication rec {
   pname = "picard";
-  version = "2.2.1";
+  version = "2.3";
 
   src = fetchFromGitHub {
     owner = "metabrainz";
     repo = pname;
     rev = "release-${version}";
-    sha256 = "1g7pbicf65hswbqmhrwlba9jm4r2vnggy7vy75z4256y7qcpwdfd";
+    sha256 = "0msdnp6wh97lnm3m084jsvbp95h5d35y5fm71j64cgf4g39zp5ds";
   };
 
   nativeBuildInputs = [ gettext qt5.wrapQtAppsHook qt5.qtbase ];
