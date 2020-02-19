8b93e5fc53995487a66cb13c83be9e33d7b275bb
Emily <vcs@emily.moe>
2020-02-19 12:44:21 +0000

    gomuks: 2019-06-28 -> 2020-02-19
    (cherry picked from commit 3d3ba53897ef385ade917fa05b1923fc47d80b0f)

--- pkgs/applications/networking/instant-messengers/gomuks/default.nix
+++ pkgs/applications/networking/instant-messengers/gomuks/default.nix
@@ -2,24 +2,24 @@
 
 buildGoModule rec {
   pname = "gomuks";
-  version = "2019-07-26";
+  version = "2020-02-19";
 
   goPackagePath = "maunium.net/go/gomuks";
 
   src = fetchFromGitHub {
     owner = "tulir";
     repo = pname;
-    rev = "7012564d0ee8481c0e4c5ecb359f77d12c9f793b";
-    sha256 = "08yiyb5fd3407wb7jc51y75hs3mhjzqca8mq0jz0vdamkhiz4f21";
+    rev = "702592bf89dfcf1ec382c0a09d99318bce7a3943";
+    sha256 = "0g638q8ypkp6dbfy1s4hz798cpkld301f914il3yd70yf05vvysc";
   };
 
-  modSha256 = "1n53xxkgilq8bp9q9j23pcabdmys1j5yl06jjwi4abpp7d7h0a5b";
+  modSha256 = "03vbrh50pvx71rp6c23qc2sh0ir4jm1wl0gvi3z1c14ndzhsqky4";
 
   meta = with stdenv.lib; {
     homepage = "https://maunium.net/go/gomuks/";
     description = "A terminal based Matrix client written in Go";
     license = licenses.gpl3;
-    maintainers = with maintainers; [ tilpner ];
+    maintainers = with maintainers; [ tilpner emily ];
     platforms = platforms.unix;
   };
 }
