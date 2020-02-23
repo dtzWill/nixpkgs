bb43cc51510ca8ab06260bf477f3605d751f98da
Will Dietz <w@wdtz.org>
2020-02-22 23:41:32 -0600

    ipset: 7.5 -> 7.6

--- pkgs/os-specific/linux/ipset/default.nix
+++ pkgs/os-specific/linux/ipset/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "ipset";
-  version = "7.5";
+  version = "7.6";
 
   src = fetchurl {
     url = "http://ipset.netfilter.org/${pname}-${version}.tar.bz2";
-    sha256 = "02vangpxdyyk9z84vc0ba1vbjvfnd6zlniisc029xzkgmdafwym5";
+    sha256 = "1ny2spcm6bmpj8vnazssg99k59impr7n84jzkdmdjly1m7548z8f";
   };
 
   nativeBuildInputs = [ pkgconfig ];
