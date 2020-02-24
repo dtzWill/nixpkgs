480740344a808a128fad305d5de935790a86d4a3
Tim Steinbach <tim@nequissimus.com>
2020-02-24 08:16:18 -0500

    vivaldi: 2.11.1811.38-1 -> 2.11.1811.41-1
    (cherry picked from commit bd118f6375087411356df8e77da731301562f410)

--- pkgs/applications/networking/browsers/vivaldi/default.nix
+++ pkgs/applications/networking/browsers/vivaldi/default.nix
@@ -17,11 +17,11 @@ let
   vivaldiName = if isSnapshot then "vivaldi-snapshot" else "vivaldi";
 in stdenv.mkDerivation rec {
   pname = "vivaldi";
-  version = "2.11.1811.33-1";
+  version = "2.11.1811.41-1";
 
   src = fetchurl {
     url = "https://downloads.vivaldi.com/${branch}/vivaldi-${branch}_${version}_amd64.deb";
-    sha256 = "1z0cscvmhxh1wjs7r0sjdr3f3mcg8i6avpal6spp0ymkxp2rn0h3";
+    sha256 = "1d0n9il9jj2790npp1ppnkc9g1pdb5v0x5kgl45xyqw0fd5fry08";
   };
 
   unpackPhase = ''
