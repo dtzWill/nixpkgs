a858d66b5ec4ee6af7eceae06359b89344797b21
Will Dietz <w@wdtz.org>
2020-02-24 20:13:57 -0600

    smartmontools: 7.0 -> 7.1, default drivedb for now

--- pkgs/tools/system/smartmontools/default.nix
+++ pkgs/tools/system/smartmontools/default.nix
@@ -2,26 +2,28 @@
 , IOKit ? null , ApplicationServices ? null }:
 
 let
-  version = "7.0";
+  version = "7.1";
 
-  dbrev = "4883";
-  drivedbBranch = "RELEASE_${builtins.replaceStrings ["."] ["_"] version}_DRIVEDB";
-  driverdb = fetchurl {
-    url    = "https://sourceforge.net/p/smartmontools/code/${dbrev}/tree/branches/${drivedbBranch}/smartmontools/drivedb.h?format=raw";
-    sha256 = "07x3haz65jyhj579h4z17v6jkw6bbyid34442gl4qddmgv2qzvwx";
-    name   = "smartmontools-drivedb.h";
-  };
+  # Use builtin drivedb.h for now, no database-update branch for this new release yet
+  #dbrev = "5008";
+  #drivedbBranch = "RELEASE_${builtins.replaceStrings ["."] ["_"] version}_DRIVEDB";
+  #driverdb = fetchurl {
+  #  url    = "https://sourceforge.net/p/smartmontools/code/${dbrev}/tree/branches/${drivedbBranch}/smartmontools/drivedb.h?format=raw";
+  #  sha256 = "07x4haz65jyhj579h4z17v6jkw6bbyid34442gl4qddmgv2qzvwx";
+  #  name   = "smartmontools-drivedb.h";
+  #};
 
 in stdenv.mkDerivation rec {
-  name = "smartmontools-${version}";
+  pname = "smartmontools";
+  inherit version;
 
   src = fetchurl {
-    url = "mirror://sourceforge/smartmontools/${name}.tar.gz";
-    sha256 = "077nx2rn9szrg6isdh0938zbp7vr3dsyxl4jdyyzv1xwhqksrqg5";
+    url = "mirror://sourceforge/smartmontools/${pname}-${version}.tar.gz";
+    sha256 = "0imqb7ka4ia5573w8rnpck571pjjc9698pdjcapy9cfyk4n4swrz";
   };
 
   patches = [ ./smartmontools.patch ];
-  postPatch = "cp -v ${driverdb} drivedb.h";
+  # postPatch = "cp -v ${driverdb} drivedb.h";
 
   nativeBuildInputs = [ autoreconfHook ];
   buildInputs = [] ++ stdenv.lib.optionals stdenv.isDarwin [IOKit ApplicationServices];
