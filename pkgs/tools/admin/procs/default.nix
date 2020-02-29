97fc296d77c5d5eb351319d17c3e5afd3ca16895
Will Dietz <w@wdtz.org>
2020-02-28 19:53:29 -0600

    procs: 0.9.11 -> 0.9.12

--- pkgs/tools/admin/procs/default.nix
+++ pkgs/tools/admin/procs/default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "procs";
-  version = "0.9.11";
+  version = "0.9.12";
 
   src = fetchFromGitHub {
     owner = "dalance";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0nz06q1rdrqgprclgr30la2vp4bx0qgibh22vdlqpd4rzi3x0h2r";
+    sha256 = "1cz0l9zf60kvpgpsbdwcw8sfgdywbqsvqrk35z67abpraz8hvhac";
   };
 
   cargoSha256 = "1r87pyimbvqr0yjwpz6q7vqly9c0fk54aiafjsdzr6h4qbi0pq8w";
