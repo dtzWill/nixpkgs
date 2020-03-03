6b610f4f20ce32ed682abc3cab9edc446b0f4cf7
Will Dietz <w@wdtz.org>
2020-03-03 16:42:16 -0600

    pastel: 0.6.1 -> 0.7.1, cargohash wip

--- pkgs/applications/misc/pastel/default.nix
+++ pkgs/applications/misc/pastel/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "pastel";
-  version = "0.6.1";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "sharkdp";
     repo = pname;
     rev = "v${version}";
-    sha256 = "1c47bph1qraq3g0g5bp23jqlz7qdn4f8vh264y937jz17avvacx5";
+    sha256 = "1xrg25w175m2iz7q9v7c05a0p0v5rr71vd4m3v6p0lqvij3sih4s";
   };
 
-  cargoSha256 = "1pfhwqj9kxm9p0mpdw7qyvivgby2bmah05kavf0a5zhzvq4v4sg0";
+  cargoSha256 = "1pfhwqj9kxm9p1mpdw7qyvivgby2bmah05kavf0a5zhzvq4v4sg0";
 
   buildInputs = stdenv.lib.optional stdenv.isDarwin Security;
 
