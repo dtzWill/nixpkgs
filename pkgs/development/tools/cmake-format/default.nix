9ac9987f515ff13528ba823d8893334949430a39
Will Dietz <w@wdtz.org>
2020-03-04 06:47:22 -0600

    Revert "cmake-format: 0.6.8 -> 0.6.9" (deps, revisit)
    This reverts commit 2c1418b114e40e222aef0e7035348f25b974dfcf.

--- pkgs/development/tools/cmake-format/default.nix
+++ pkgs/development/tools/cmake-format/default.nix
@@ -10,12 +10,12 @@
 
 buildPythonApplication rec {
   pname = "cmake-format";
-  version = "0.6.9";
+  version = "0.6.5";
 
   src = fetchPypi {
     inherit version;
     pname = "cmake_format";
-    sha256 = "082d7949gsk5v72ap7k4p3vgmc126a0bfm195xpi4lb6khpbzy5j";
+    sha256 = "0fzfczf66df81szp488zwdz6phx6lcq6wkb0dzpzq6ni39r7kvw8";
   };
 
   propagatedBuildInputs = [ autopep8 flake8 jinja2 pylint pyyaml ];
