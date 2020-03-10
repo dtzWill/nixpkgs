dce9d4442ef3dddb7bc6a361ac0647406a715a8b
Will Dietz <w@wdtz.org>
2020-03-10 10:58:15 -0500

    gusb: python3 for script shebang

--- pkgs/development/libraries/gusb/default.nix
+++ pkgs/development/libraries/gusb/default.nix
@@ -1,6 +1,7 @@
 { stdenv, fetchurl, meson, ninja, pkgconfig, gettext, gobject-introspection
 , gtk-doc, docbook_xsl, docbook_xml_dtd_412, docbook_xml_dtd_44
 , glib, systemd, libusb1, vala, hwdata
+, python3
 }:
 stdenv.mkDerivation rec {
   pname = "gusb";
@@ -13,10 +14,15 @@ stdenv.mkDerivation rec {
     sha256 = "011f705wvh466p11hv2i2a9gskam1f0jywx0nawm8rj9297d47sq";
   };
 
+  postPatch = ''
+    patchShebangs contrib/generate-version-script.py
+  '';
+
   nativeBuildInputs = [
     meson ninja pkgconfig gettext
     gtk-doc docbook_xsl docbook_xml_dtd_412 docbook_xml_dtd_44
     gobject-introspection vala
+    python3
   ];
   buildInputs = [ systemd glib ];
 
