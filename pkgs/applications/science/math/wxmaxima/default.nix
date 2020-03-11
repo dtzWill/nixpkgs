1b6fe5b253177a1c3f1ebfd22bb022290dc509f2
xaverdh <xaverdh@users.noreply.github.com>
2020-03-11 16:07:53 +0000

    wxmaxima: 18.02.0 -> 20.02.4 (#82323)
    The commit description is 18.02.0 -> 20.02.4, because in the last version bump (1c6a193b3ed36dd08e35a7b0ea1d28b72133d6c6), the version string was changed, but the hash was not.
    
    Co-authored-by: Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
    (cherry picked from commit 5ac085c3cc690333885b1a142806c3de25f9ece2)

--- pkgs/applications/science/math/wxmaxima/default.nix
+++ pkgs/applications/science/math/wxmaxima/default.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation rec {
   pname = "wxmaxima";
-  version = "20.01.2";
+  version = "20.02.4";
 
   src = fetchFromGitHub {
     owner = "wxMaxima-developers";
     repo = "wxmaxima";
     rev = "Version-${version}";
-    sha256 = "0ji6d8cmgfyc42jbbqx8svcswi8235bxv1451f4j7rl5vyyhrswn";
+    sha256 = "106a7jrjwfmymzj70nsv44fm3jbxngr8pmkaghhpwy0ln38lhf54";
   };
 
   buildInputs = [ wxGTK maxima gnome3.adwaita-icon-theme ];
@@ -21,12 +21,10 @@ stdenv.mkDerivation rec {
     gappsWrapperArgs+=(--prefix PATH ":" ${maxima}/bin)
   '';
 
-  enableParallelBuilding = true;
-
   meta = with stdenv.lib; {
     description = "Cross platform GUI for the computer algebra system Maxima";
     license = licenses.gpl2;
-    homepage = https://wxmaxima-developers.github.io/wxmaxima/;
+    homepage = "https://wxmaxima-developers.github.io/wxmaxima/";
     platforms = platforms.linux;
     maintainers = [ maintainers.peti ];
   };
