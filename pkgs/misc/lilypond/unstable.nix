ffb5f152c2aba8537ba6cab9e048809eac83ddbb
Orivej Desh <orivej@gmx.fr>
2020-03-13 23:34:09 +0000

    lilypond: 2.18.2 -> 2.20.0
    (cherry picked from commit 2e099c7e53a2f36a5d05bfb8f6b823b164ecff3a)

--- pkgs/misc/lilypond/unstable.nix
+++ pkgs/misc/lilypond/unstable.nix
@@ -14,10 +14,4 @@ lilypond.overrideAttrs (oldAttrs: {
     rev = "release/${version}-1";
     sha256 = "15wxya18larvy0ya97gg1xlbcd03cbl9n47xq53w9wjamh498hkv";
   };
-
-  configureFlags = [
-    "--disable-documentation"
-    "--with-urwotf-dir=${ghostscript}/share/ghostscript/fonts"
-    "--with-texgyre-dir=${gyre-fonts}/share/fonts/truetype/"
-  ];
 })
