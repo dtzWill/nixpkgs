e4cfecaabc16e3154e1f50d2a36d7fdcc9b0a901
Pierre Bourdon <delroth@gmail.com>
2020-03-16 09:21:15 +0100

    libtxc_dxtn{,_s2tc}: remove from nixpkgs + hardware.opengl options
    Context: discussion in https://github.com/NixOS/nixpkgs/pull/82630
    
    Mesa has been supporting S3TC natively without requiring these libraries
    since the S3TC patent expired in December 2017.
    
    (cherry picked from commit 1b89bffcf47dc1e271bea128635e33efd9481b93)

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -215,6 +215,8 @@ mapAliases ({
   librecad2 = librecad;  # backwards compatibility alias, added 2015-10
   libsysfs = sysfsutils; # added 2018-04-25
   libtidy = html-tidy;  # added 2014-12-21
+  libtxc_dxtn = throw "removed 2020-03-16, now integrated in Mesa";
+  libtxc_dxtn_s2tc = throw "removed 2020-03-16, now integrated in Mesa";
   libudev = udev; # added 2018-04-25
   links = links2; # added 2016-01-31
   lttngTools = lttng-tools;  # added 2014-07-31
