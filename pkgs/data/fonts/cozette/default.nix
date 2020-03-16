11f0942b7a98475691ba1537b69286bc5ee970ba
Will Dietz <w@wdtz.org>
2020-03-16 06:58:25 -0500

    cozette: 1.6.1 -> 1.6.2

--- pkgs/data/fonts/cozette/default.nix
+++ pkgs/data/fonts/cozette/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, mkfontscale }:
 
 let
-  version = "1.6.1";
+  version = "1.6.2";
   releaseUrl =
     "https://github.com/slavfox/Cozette/releases/download/v.${version}";
 in stdenv.mkDerivation rec {
@@ -11,15 +11,15 @@ in stdenv.mkDerivation rec {
   srcs = map fetchurl [
     {
       url = "${releaseUrl}/cozette.otb";
-      sha256 = "1cbr55ppjpcc1d08scfbvk9kd8swb2g73n83xd8wca0gxbcqhx8z";
+      sha256 = "0hq86r3rxcnr6hkr8l30fwq81zq37f0n16jj4g3bifqk77md1s4v";
     }
     {
       url = "${releaseUrl}/CozetteVector.otf";
-      sha256 = "08rld9dkypm5djbjg0ncrap2fh3jh1c6cxs0lnrbrrzhp39d4i7i";
+      sha256 = "08sbbsl2y8ap0ailps4kbsiac1l0v28dygi34qk9qn9jy8q8hhvb";
     }
     {
       url = "${releaseUrl}/CozetteVector.ttf";
-      sha256 = "1cvb3811pzvk8f2127x54727yv9a73zybp255nbzyybqhykjjczb";
+      sha256 = "1mjh55hlb29qwswzyynpdqg0c6ni5svfsgy9pfjacrxn02ym9kdz";
     }
   ];
 
