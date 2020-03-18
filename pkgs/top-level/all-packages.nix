902772ae2edd9b1640da3ae63c5b36e4dbe246e1
Jonathan Ringer <jonringer117@gmail.com>
2020-03-18 12:53:54 -0700

    texworks: fix qt build
    (cherry picked from commit aaefb947eaa5f227feb99ea181bca6404c549d74)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6775,7 +6775,7 @@ in
 
   textadept = callPackage ../applications/editors/textadept { };
 
-  texworks = callPackage ../applications/editors/texworks { };
+  texworks = libsForQt5.callPackage ../applications/editors/texworks { };
 
   thc-hydra = callPackage ../tools/security/thc-hydra { };
 
