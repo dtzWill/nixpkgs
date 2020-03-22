04b37fd6a2dd0d625c0beb95a45f563f3879bfba
Will Dietz <w@wdtz.org>
2020-03-21 22:28:55 -0500

    linux: 4.19.111 -> 4.19.112

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.19.111";
+  version = "4.19.112";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0cjjf3wbvbkjy4mss8c74afx8ng31i22km66ydh9f9mz182piyy6";
+    sha256 = "0yiyqwgh6wcyshpdj98s7dc4ahyx47y6whvnww6sjmzdq0fb3hi4";
   };
 } // (args.argsOverride or {}))
