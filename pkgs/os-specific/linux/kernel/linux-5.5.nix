f5e9bc31d40196d07567ac4b278d2c6cb1224edd
Will Dietz <w@wdtz.org>
2020-03-21 22:29:45 -0500

    linux: 5.5.10 -> 5.5.11

--- pkgs/os-specific/linux/kernel/linux-5.5.nix
+++ pkgs/os-specific/linux/kernel/linux-5.5.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.5.10";
+  version = "5.5.11";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "08zw437i7zb4s90pciis843xvilgmbxbrkmq2alnyxnrswsqw7g3";
+    sha256 = "0bplsbjb3slx566assxdhp7rnmm9z2s8iv9hfar574jds77syix5";
   };
 } // (args.argsOverride or {}))
