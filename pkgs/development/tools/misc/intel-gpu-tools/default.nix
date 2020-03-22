1e90beb408a83effdc7af719cca7d966e5056b7f
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-22 15:36:46 +0000

    intel-gpu-tools: 1.24 -> 1.25
    (cherry picked from commit 77dca1403189b05d4a5988b01044e9ffaa833d29)

--- pkgs/development/tools/misc/intel-gpu-tools/default.nix
+++ pkgs/development/tools/misc/intel-gpu-tools/default.nix
@@ -3,12 +3,12 @@
 , procps, utilmacros, gtk-doc, openssl, peg, meson, ninja, elfutils, flex }:
 
 stdenv.mkDerivation rec {
-  pname = "intel-gpu-tools"; # upstream renamed to 'igt-gpu-tools', perhaps we should also?
-  version = "1.24";
+  pname = "intel-gpu-tools";
+  version = "1.25";
 
   src = fetchurl {
     url = "https://xorg.freedesktop.org/archive/individual/app/igt-gpu-tools-${version}.tar.xz";
-    sha256 = "1gr1m18w73hmh6n9w2f6gky21qc0pls14bgxkhy95z7azrr7qdap";
+    sha256 = "04fx7xclhick3k7fyk9c4mn8mxzf1253j1r0hrvj9sl40j7lsia0";
   };
 
   nativeBuildInputs = [ pkgconfig utilmacros meson ninja flex ];
@@ -34,7 +34,7 @@ stdenv.mkDerivation rec {
   enableParallelBuilding = true;
 
   meta = with stdenv.lib; {
-    homepage = https://01.org/linuxgraphics/;
+    homepage = "https://01.org/linuxgraphics/";
     description = "Tools for development and testing of the Intel DRM driver";
     license = licenses.mit;
     platforms = [ "x86_64-linux" "i686-linux" ];
