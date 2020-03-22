80cfc4278371eaf28168497a8bea7fe778c2a898
Will Dietz <w@wdtz.org>
2020-03-21 23:15:56 -0500

    slack-term: mod hashfix

--- pkgs/applications/networking/instant-messengers/slack-term/default.nix
+++ pkgs/applications/networking/instant-messengers/slack-term/default.nix
@@ -14,7 +14,7 @@ buildGoModule rec {
     sha256 = "1fbq7bdhy70hlkklppimgdjamnk0v059pg73xm9ax1f4616ki1m6";
   };
 
-  modSha256 = "1spzwqbiiy76yps482r4dyb5radliiwacz5zjgv24d0yfs6hqw4x";
+  modSha256 = "0h2w276g4y1sdpbgngg2nc82w7157gz0l8l4byzb8dlxkc8l530s";
 
   meta = with stdenv.lib; {
     description = "Slack client for your terminal";
