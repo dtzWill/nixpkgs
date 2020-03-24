61da441cb5d5a6c258cd3797fe71c4bd62081ef0
Will Dietz <w@wdtz.org>
2020-03-24 09:26:17 -0500

    onedrive: sync

--- pkgs/applications/networking/sync/onedrive/default.nix
+++ pkgs/applications/networking/sync/onedrive/default.nix
@@ -1,10 +1,4 @@
-{ stdenv
-, fetchFromGitHub
-, dmd
-, pkgconfig
-, curl
-, sqlite
-}:
+{ stdenv, fetchFromGitHub, dmd, pkgconfig, curl, sqlite, libnotify }:
 
 stdenv.mkDerivation rec {
   pname = "onedrive";
@@ -12,24 +6,22 @@ stdenv.mkDerivation rec {
 
   src = fetchFromGitHub {
     owner = "abraunegg";
-    repo = "onedrive";
+    repo = pname;
     rev = "v${version}";
     sha256 = "12cc1i6ygrky4dm42frlsysyjn74qjqg9w19m17fgblagdh66q04";
   };
 
-  nativeBuildInputs = [
-    dmd
-    pkgconfig
-  ];
-  buildInputs = [
-    curl
-    sqlite
-  ];
+  nativeBuildInputs = [ dmd pkgconfig ];
+
+  buildInputs = [ curl sqlite libnotify ];
+
+  configureFlags = [ "--enable-notifications" ];
+
   meta = with stdenv.lib; {
     description = "A complete tool to interact with OneDrive on Linux";
     homepage = "https://github.com/abraunegg/onedrive";
     license = licenses.gpl3;
-    maintainers = with maintainers; [ doronbehar ];
+    maintainers = with maintainers; [ srgom ianmjones ];
     platforms = platforms.linux;
   };
 }
