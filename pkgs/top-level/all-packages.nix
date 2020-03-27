8939d19955fe635212ba27f05b8746b64683c711
Orivej Desh <orivej@gmx.fr>
2020-03-27 23:57:06 +0000

    x42-autotune: remove
    It is a part of x42-plugins (under the name of x42-fat1).
    
    (cherry picked from commit 14753000ed1a48634ffb4ce78860db96ac29d8c6)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22508,8 +22508,6 @@ in
 
   x32edit = callPackage ../applications/audio/midas/x32edit.nix {};
 
-  x42-autotune = callPackage ../applications/audio/x42-autotune { };
-
   x42-plugins = callPackage ../applications/audio/x42-plugins { };
 
   xannotate = callPackage ../tools/X11/xannotate {};
