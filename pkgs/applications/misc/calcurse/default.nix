f85c15fd948c60fe81193278e2e166efaea9a5fa
R. RyanTM <ryantm-bot@ryantm.com>
2020-03-29 12:46:19 +0000

    calcurse: 4.5.1 -> 4.6.0
    (cherry picked from commit 725e7aec63a2c27af36170783e1b2058b0240c5b)

--- pkgs/applications/misc/calcurse/default.nix
+++ pkgs/applications/misc/calcurse/default.nix
@@ -3,14 +3,11 @@
 
 stdenv.mkDerivation rec {
   pname = "calcurse";
-  #version = "4.5.1";
-  version = "unstable-2020-02-04";
+  version = "4.6.0";
 
-  src = fetchFromGitHub {
-    owner = "lfos";
-    repo = pname;
-    rev = "ada0d98bde810d356d8272b476bc080fa37104f5";
-    sha256 = "01vdfq28y6g8xvixsq2b8yw2d2frfrxgah0g3ycnr9fy0rxsha4d";
+  src = fetchurl {
+    url = "https://calcurse.org/files/${pname}-${version}.tar.gz";
+    sha256 = "0hzhdpkkn75jlymanwzl69hrrf1pw29hrchr11wlxqjpl43h62gs";
   };
   #src = fetchurl {
   #  #url = "https://calcurse.org/files/${pname}-${version}.tar.gz";
@@ -57,7 +54,7 @@ stdenv.mkDerivation rec {
       customized to suit user needs and a very powerful set of command line options can
       be used to filter and format appointments, making it suitable for use in scripts.
     '';
-    homepage = http://calcurse.org/;
+    homepage = "http://calcurse.org/";
     license = licenses.bsd2;
     platforms = platforms.linux;
   };
