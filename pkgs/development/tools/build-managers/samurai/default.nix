d414c58a752d2eafdb89a4e8795fbfee69121428
Will Dietz <w@wdtz.org>
2020-04-02 10:33:08 -0500

    samurai: drop included patch

--- pkgs/development/tools/build-managers/samurai/default.nix
+++ pkgs/development/tools/build-managers/samurai/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, fetchpatch }:
+{ stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   pname = "samurai";
@@ -10,14 +10,6 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "0k0amxpi3v9v68a8vc69r4b86xs12vhzm0wxd7f11vap1pnqz2cz";
   };
-
-  # Add support for compdb tool (compilation database)!
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/michaelforney/${pname}/compare/${src.rev}..fe1b3ce210fc333ad0b38a61dc413f8d105013d4.patch";
-      sha256 = "1pbr7p829h8d9nx99s21qpb0qgj0y0hdz4wnx7h93mjfaagb8qlq";
-    })
-  ];
 
   makeFlags = [ "DESTDIR=" "PREFIX=${placeholder "out"}" ];
 
