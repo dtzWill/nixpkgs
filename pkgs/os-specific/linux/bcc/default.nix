d423317bf0d8f8bd7a438848587fc5cd6cd3f321
Will Dietz <w@wdtz.org>
2020-06-03 16:10:30 -0500

    linuxPackages.bcc: 0.13.0 -> 0.14.0

--- pkgs/os-specific/linux/bcc/default.nix
+++ pkgs/os-specific/linux/bcc/default.nix
@@ -4,12 +4,12 @@
 }:
 
 python.pkgs.buildPythonApplication rec {
-  version = "0.13.0";
+  version = "0.14.0";
   name = "bcc-${version}";
 
   src = fetchurl {
     url = "https://github.com/iovisor/bcc/releases/download/v${version}/bcc-src-with-submodule.tar.gz";
-    sha256 = "15xpwf17x2j1c1wcb84cgfs35dp5w0rjd9mllmddmdjvn303wffx";
+    sha256 = "1hw02bib06fjyw61as5pmhf0qxy0wv0nw8fff2i8a9d1zcd8xf3p";
   };
   format = "other";
 
