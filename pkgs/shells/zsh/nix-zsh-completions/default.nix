ffe56e39ae3252b58239da6dd83e438a64a239b7
Will Dietz <w@wdtz.org>
2020-06-05 08:22:35 -0500

    nix-zsh-completions: 0.4.4, sync w/nixpkgs-master

--- pkgs/shells/zsh/nix-zsh-completions/default.nix
+++ pkgs/shells/zsh/nix-zsh-completions/default.nix
@@ -1,18 +1,18 @@
 { stdenv, fetchFromGitHub }:
 
 let
-  version = "0.4.3";
+  version = "0.4.4";
 in
 
-stdenv.mkDerivation rec {
-  name = "nix-zsh-completions-${version}";
+stdenv.mkDerivation {
+  pname = "nix-zsh-completions";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "spwhitt";
     repo = "nix-zsh-completions";
-    #rev = "${version}";
-    rev = "23cbcf53fa259a23bb56d3641c70cc815f8e9b8f";
-    sha256 = "1zqa8ilwzisz8ic8bnyg6xxgrwaqdlw9cpfk7d4qxh49lfbx7p52";
+    rev = version;
+    sha256 = "1n9whlys95k4wc57cnz3n07p7zpkv796qkmn68a50ygkx6h3afqf";
   };
 
   installPhase = ''
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with stdenv.lib; {
-    homepage = https://github.com/spwhitt/nix-zsh-completions;
+    homepage = "https://github.com/spwhitt/nix-zsh-completions";
     description = "ZSH completions for Nix, NixOS, and NixOps";
     license = licenses.bsd3;
     platforms = platforms.all;
