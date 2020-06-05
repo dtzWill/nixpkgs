02738076921e0d4b9a2047f485f74175a48e0b7a
Will Dietz <w@wdtz.org>
2020-06-05 11:27:02 -0500

    steam: runtime 2020-04-17 -> 2020-05-05

--- pkgs/games/steam/runtime.nix
+++ pkgs/games/steam/runtime.nix
@@ -4,16 +4,16 @@ stdenv.mkDerivation rec {
 
   name = "steam-runtime";
   # from https://repo.steampowered.com/steamrt-images-scout/snapshots/
-  version = "0.20200417.0";
+  version = "0.20200505.0";
 
   src =
     if steamArch == "amd64" then fetchurl {
       url = "https://repo.steampowered.com/steamrt-images-scout/snapshots/${version}/com.valvesoftware.SteamRuntime.Platform-amd64,i386-scout-runtime.tar.gz";
-      sha256 = "0kps8i5v23sycqm69xz389n8k831jd7ncsmlrkky7nib2q91rbvj";
+      sha256 = "0mg1c2m9dnc9fl2wdyjwb87ma4qpxax9qax6ndp72yw7sgkdwrfj";
       name = "scout-runtime-${version}.tar.gz";
     } else fetchurl {
       url = "https://repo.steampowered.com/steamrt-images-scout/snapshots/${version}/com.valvesoftware.SteamRuntime.Platform-i386-scout-runtime.tar.gz";
-      sha256 = "03fhac1r25xf7ia2pd35wjw360v5pa9h4870yrhhygp9h7v4klzf";
+      sha256 = "0723lr0dz7i20ibcgh9lgjv3m723j9al0zki8kc9a477hjcf6z8k";
       name = "scout-runtime-i386-${version}.tar.gz";
     };
 
