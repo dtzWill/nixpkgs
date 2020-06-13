07724a8611e99d84a9563cb6a59e23a11632ab67
Euan Kemp <euank@euank.com>
2020-06-13 14:41:22 -0700

    steam: switch to correct runtime tarball (dtz: 20200505)
    In my previous commit (593c28b) I used the wrong upstream artifact for
    the runtime. After reading the documentation in the
    ValveSoftware/steam-runtime repo, I now know that the steam-runtime
    tarball is what I actually wanted.
    
    I also used 'diff' to compare the various artifacts with the old runtime
    this package used before, and the steam-runtime one is certainly the
    closest.
    
    Most importantly, switching to the right steam-runtime package
    reportedly fixes issues for other users (fixes #90229).
    
    This also entirely removes the amd64/i386 split from runtime.nix because
    the upstream package bundles both together, and if that's how upstream
    wants to distribute this, it seems best to follow their lead.
    
    (cherry picked from commit 727e7d8039ca11aff682e39bf3817f09012cf924)

--- pkgs/games/steam/runtime.nix
+++ pkgs/games/steam/runtime.nix
@@ -1,4 +1,4 @@
-{ stdenv, steamArch, fetchurl, }:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
 
@@ -6,20 +6,15 @@ stdenv.mkDerivation rec {
   # from https://repo.steampowered.com/steamrt-images-scout/snapshots/
   version = "0.20200505.0";
 
-  src =
-    if steamArch == "amd64" then fetchurl {
-      url = "https://repo.steampowered.com/steamrt-images-scout/snapshots/${version}/com.valvesoftware.SteamRuntime.Platform-amd64,i386-scout-runtime.tar.gz";
-      sha256 = "0mg1c2m9dnc9fl2wdyjwb87ma4qpxax9qax6ndp72yw7sgkdwrfj";
-      name = "scout-runtime-${version}.tar.gz";
-    } else fetchurl {
-      url = "https://repo.steampowered.com/steamrt-images-scout/snapshots/${version}/com.valvesoftware.SteamRuntime.Platform-i386-scout-runtime.tar.gz";
-      sha256 = "0723lr0dz7i20ibcgh9lgjv3m723j9al0zki8kc9a477hjcf6z8k";
-      name = "scout-runtime-i386-${version}.tar.gz";
-    };
+  src = fetchurl {
+    url = "https://repo.steampowered.com/steamrt-images-scout/snapshots/${version}/steam-runtime.tar.xz";
+    sha256 = "0cbnzx0qc3y8rg4gcmfxr6d5kw2xbg9v67vf6j0bbj8n5k6wzf8b";
+    name = "scout-runtime-${version}.tar.gz";
+  };
 
   buildCommand = ''
     mkdir -p $out
-    tar -C $out -x --strip=1 -f $src files/
+    tar -C $out --strip=1 -x -f $src
   '';
 
   meta = with stdenv.lib; {
