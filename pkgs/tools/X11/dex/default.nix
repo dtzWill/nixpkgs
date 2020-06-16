7c83ebe7f9110e54c3fdebb8bd98860fdb7255ac
Will Dietz <w@wdtz.org>
2020-06-16 11:01:10 -0500

    dex: remove included patches

--- pkgs/tools/X11/dex/default.nix
+++ pkgs/tools/X11/dex/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, python3, fetchpatch }:
+{ stdenv, fetchFromGitHub, python3 }:
 
 stdenv.mkDerivation rec {
   program = "dex";
@@ -15,24 +15,6 @@ stdenv.mkDerivation rec {
   buildInputs = [ python3 ];
   nativeBuildInputs = [ python3.pkgs.sphinx python3.pkgs.setuptools ];
   makeFlags = [ "PREFIX=${placeholder "out"}" "VERSION=${version}" ];
-
-  patches = [
-    (fetchpatch {
-      name = "fix-version-number.patch";
-      url = https://github.com/jceb/dex/commit/107358ddf5e1ca4fa56ef1a7ab161dc3b6adc45a.patch;
-      sha256 = "06dfkfzxp8199by0jc5wim8g8qw38j09dq9p6n9w4zaasla60pjq";
-    })
-    (fetchpatch {
-      name = "add-term-option.patch";
-      url = https://github.com/jceb/dex/commit/e183bc04f7afe719ba3fc137604f2cdf08e03c22.patch;
-      sha256 = "1krzlxpy6qwh0agmxn2k2jslzs9pl2dqwg6g0ap10ryw9zxdynz5";
-    })
-    (fetchpatch {
-      name = "add-wait-option.patch";
-      url = https://github.com/jceb/dex/commit/cfbb76cd2ff32b9c821b91b5372a1770d6ac300a.patch;
-      sha256 = "1ghmgzd9r1jg1hva5dxcxav9ynzsmyka3pnqndknpngkqb0vnf0s";
-    })
-  ];
 
   meta = with stdenv.lib; {
     description = "A program to generate and execute DesktopEntry files of the Application type";
