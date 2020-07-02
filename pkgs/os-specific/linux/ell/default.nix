5c51d4488f15361c51435760c8a05429c6ff2824
Will Dietz <w@wdtz.org>
2020-07-02 17:06:51 -0500

    ell: bump to fix iwd

--- pkgs/os-specific/linux/ell/default.nix
+++ pkgs/os-specific/linux/ell/default.nix
@@ -7,16 +7,16 @@
 
 stdenv.mkDerivation rec {
   pname = "ell";
-  version = "0.32";
-  #version = "unstable-2020-04-30";
+  #version = "0.32";
+  version = "unstable-2020-06-22";
 
   outputs = [ "out" "dev" ];
 
   src = fetchgit {
      url = "https://git.kernel.org/pub/scm/libs/${pname}/${pname}.git";
-     rev = version;
-     #rev = "87516f3e469c9e2fd34aab9363989946692081e6";
-     sha256 = "07hm9lrhhb5y53l13yja2kr3xmjgs0azk3x7w2si99cplwkgxak2";
+     #rev = version;
+     rev = "1fbac22c17d0ad9a93f7debb679af4974504f932";
+     sha256 = "0zg5zlfxdyp2zqfp0cvg223a6hmdwb9jhljdzm8hl6kkb7id4vv7";
   };
 
   patches = [
