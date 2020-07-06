6b7bea45c633cacd2b73ee11b00937e5ccf7008a
Will Dietz <w@wdtz.org>
2020-07-06 14:58:40 -0500

    Revert "google_api_core: use protobuf 3.12, default not new enough"
    This reverts commit e57e861a3f76bd82aa5fc136afd49e42ef221589.

--- pkgs/development/python-modules/google_api_core/default.nix
+++ pkgs/development/python-modules/google_api_core/default.nix
@@ -1,5 +1,5 @@
 { lib, buildPythonPackage, fetchPypi, pythonOlder, isPy27
-, google_auth, protobuf3_12, googleapis_common_protos, requests, setuptools, grpcio
+, google_auth, protobuf, googleapis_common_protos, requests, setuptools, grpcio
 , mock
 }:
 
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   };
 
   propagatedBuildInputs = [
-    googleapis_common_protos protobuf3_12
+    googleapis_common_protos protobuf
     google_auth requests setuptools grpcio
   ];
 
