c277d83d204cf11f00d700a4c019e2cecee2eb18
Will Dietz <w@wdtz.org>
2020-07-10 10:10:46 -0500

    jetbrains-mono: 1.0.6 -> 2.000
    https://github.com/JetBrains/JetBrainsMono/releases/tag/v2.000

--- pkgs/data/fonts/jetbrains-mono/default.nix
+++ pkgs/data/fonts/jetbrains-mono/default.nix
@@ -1,19 +1,19 @@
 { lib, fetchzip }:
 
 let
-  version = "1.0.6";
+  version = "2.000";
 in
 fetchzip rec {
   name = "JetBrainsMono-${version}";
 
   #url = "https://download.jetbrains.com/fonts/JetBrainsMono-${version}.zip";
-  url = "https://github.com/JetBrains/JetBrainsMono/releases/download/v${version}/JetBrainsMono-${version}.zip";
+  url = "https://github.com/JetBrains/JetBrainsMono/releases/download/v${version}/JetBrains.Mono.${version}.zip";
 
-  sha256 = "0vf0pzfryhmzddskpwn9k9iqf3fww547xvrjb2xc73ycxddac3f1";
+  sha256 = "1dc2m4f9vhddn0d8gcp1qa76gg1jqa0lqj739cr0ndx8ck5zq2rp";
 
   postFetch = ''
     unzip $downloadedFile
-    install -m444 -Dt $out/share/fonts/truetype ${name}/ttf/*.ttf
+    install -m444 -Dt $out/share/fonts/truetype "JetBrains Mono ${version}"/ttf{,/Variable}/*.ttf
   '';
 
   meta = with lib; {
