b7f5cfba23a2d009ad569ea67f1315addc21ab17
Will Dietz <w@wdtz.org>
2020-07-17 15:10:02 -0500

    notmuch: more cleanup

--- pkgs/applications/networking/mailreaders/notmuch/default.nix
+++ pkgs/applications/networking/mailreaders/notmuch/default.nix
@@ -20,18 +20,9 @@ stdenv.mkDerivation rec {
 
   passthru = {
     pythonSourceRoot = "${pname}-${version}/bindings/python";
-    #pythonSourceRoot = "${src}/bindings/python"; # lol
     inherit version;
   };
 
-  ## src = fetchgit {
-  ##   name = "${pname}-${version}"; # descriptive but exact name, used above in pythonSourceRoot
-  ##   url = git://git.notmuchmail.org/git/notmuch;
-  ##   #rev = "bc396c967c7cd8e7a109858e428d7bf97173f7a7";
-# ##    rev = "refs/tags/${version}";
-  ##   rev = "1fcf068e331b9b79e14f79c8b126711fc3d72cbb";
-  ##   sha256 = "1pf9xds5csw6vwkb4b15isrw29psdifx8gl8y61la7d1k7b6m517";
-  ## };
   src = fetchurl {
     url = "https://notmuchmail.org/releases/${pname}-${version}.tar.xz";
     sha256 = "1ylnj12f7xr18v3ckb1nwc2aw2rj3ghqnj5f4rzccr8xw5pslfsy";
@@ -43,6 +34,7 @@ stdenv.mkDerivation rec {
     pythonPackages.sphinx     # (optional) documentation -> doc/INSTALL
     texinfo                   # (optional) documentation -> doc/INSTALL
   ] ++ optional withEmacs [ emacs ];
+
   buildInputs = [
     gnupg                     # undefined dependencies
     xapian gmime talloc zlib  # dependencies described in INSTALL
@@ -65,12 +57,13 @@ stdenv.mkDerivation rec {
 
     substituteInPlace lib/Makefile.local \
       --replace '-install_name $(libdir)' "-install_name $out/lib"
+  '' + optionalString withEmacs ''
+    substituteInPlace emacs/notmuch-emacs-mua \
+      --replace 'EMACS:-emacs' 'EMACS:-${emacs}/bin/emacs' \
+      --replace 'EMACSCLIENT:-emacsclient' 'EMACSCLIENT:-${emacs}/bin/emacsclient'
   '';
 
   configureFlags = [
-    "--without-emacs"
-    #"--without-docs"
-    #"--without-api-docs"
     "--zshcompletiondir=${placeholder "out"}/share/zsh/site-functions"
     "--bashcompletiondir=${placeholder "out"}/share/bash-completion/completions"
     "--infodir=${placeholder "info"}/share/info"
@@ -94,22 +87,26 @@ stdenv.mkDerivation rec {
   in ''
     ln -s ${test-database} test/test-databases/database-v1.tar.xz
   '';
-  doCheck = !stdenv.hostPlatform.isDarwin && (versionAtLeast gmime.version "3.0");
+  doCheck = !stdenv.hostPlatform.isDarwin && (versionAtLeast gmime.version "3.0.3");
   checkTarget = "test";
   checkInputs = [
     which dtach openssl bash
-    gdb man
+    gdb man emacs
   ];
 
-  installTargets = [ "install" "install-man" ];
+  installTargets = [ "install" "install-man" "install-info" ];
+
+  postInstall = stdenv.lib.optionalString withEmacs ''
+    moveToOutput bin/notmuch-emacs-mua $emacs
+  '';
 
   dontGzipMan = true; # already compressed
 
   meta = {
     description = "Mail indexer";
-    homepage    = https://notmuchmail.org/;
+    homepage    = "https://notmuchmail.org/";
     license     = licenses.gpl3;
-    maintainers = with maintainers; [ flokli the-kenny ];
+    maintainers = with maintainers; [ flokli puckipedia ];
     platforms   = platforms.unix;
   };
 }
