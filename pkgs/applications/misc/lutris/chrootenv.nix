2161a7f1dbd712b6492cc515d460eb1383033d12
Will Dietz <w@wdtz.org>
2020-07-22 06:41:03 -0500

    lutris: libGL?

--- pkgs/applications/misc/lutris/chrootenv.nix
+++ pkgs/applications/misc/lutris/chrootenv.nix
@@ -91,7 +91,7 @@ in buildFHSUserEnv {
 
   multiPkgs = pkgs: with pkgs; [
     # Common
-    libsndfile libtheora libogg libvorbis libopus libGLU libpcap libpulseaudio
+    libsndfile libtheora libogg libvorbis libopus libGL libGLU libpcap libpulseaudio
     libao libevdev udev libgcrypt libxml2 libusb libpng libmpeg2 libv4l
     libjpeg libxkbcommon libass libcdio libjack2 libsamplerate libzip libmad libaio
     libcap libtiff libva libgphoto2 libxslt libsndfile giflib zlib glib
