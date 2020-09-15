b7aa7ac2cede5b539e01dacf6e0bc7c9d82fc30a
Will Dietz <w@wdtz.org>
2020-09-15 15:33:53 -0500

    iwd: git bump

--- pkgs/os-specific/linux/iwd/default.nix
+++ pkgs/os-specific/linux/iwd/default.nix
@@ -17,13 +17,13 @@ stdenv.mkDerivation rec {
   pname = "iwd";
 
   #version = "1.8";
-  version = "unstable-2020-08-28";
+  version = "unstable-2020-09-14";
 
   src = fetchgit {
     url = https://git.kernel.org/pub/scm/network/wireless/iwd.git;
     #rev = version;
-    rev = "4bc20a0979655a97b7776ab1a8778e82c0785860";
-    sha256 = "07rrizsldsj6v3168w5238kng5j02xm0cbih6hqs36vahqjhfz9z";
+    rev = "9acb89d6f4effcb9a2d5821334b6b1d97fcf0c49";
+    sha256 = "1595by63nwipmdlzmll4ngd1i5r5d8fzms87adlnwg8c42y726iv";
   };
 
   nativeBuildInputs = [
