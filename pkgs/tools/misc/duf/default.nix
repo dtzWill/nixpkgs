add0daaf47f6930893b6b71947c9cc897f99408b
Will Dietz <w@wdtz.org>
2020-09-26 03:25:30 -0500

    duf: 0.1.0 -> 0.3.1

--- pkgs/tools/misc/duf/default.nix
+++ pkgs/tools/misc/duf/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "duf";
-  version = "0.1.0";
+  version = "0.3.1";
 
   src = fetchFromGitHub {
     owner = "muesli";
     repo = "duf";
     rev = "v${version}";
-    sha256 = "1akziaa5wjszflyylvnw284pz1aqngl40civzfabjz94pvyjkp76";
+    sha256 = "1y27cc818099drccqg57nq4kcgl2zmg6akxv2k1c8rkz932q718f";
   };
 
   #vendorSha256 = "1aj7rxlylgvxdnnfnfzh20v2jvs8falvjjishxd8rdk1jgfpipl8";
   # old fetcher :(
-  modSha256 = "0m9dzkcli7kg8z72mvc86dil6d9hjzy6z5hi7gyz4x3lcay4lh5s";
+  modSha256 = "11xy3avalgrqxk6bgra509gxip7ls2gsx27fahk9fbvq80dyc9c3";
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/muesli/duf/";
