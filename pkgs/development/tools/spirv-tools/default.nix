b2c8af30a36cd2e1080e369393686ac75a286ef4
Doron Behar <doron.behar@gmail.com>
2020-10-15 09:53:35 +0300

    Merge pull request #94049 from CajuM/vulkan-up
    (cherry picked from commit 0826f2efaed5c64d3c574f87dd4dcf978207f576)

--- pkgs/development/tools/spirv-tools/default.nix
+++ pkgs/development/tools/spirv-tools/default.nix
@@ -1,15 +1,18 @@
 { stdenv, fetchFromGitHub, cmake, python3, spirv-headers }:
+let
+  # Update spirv-headers rev in lockstep according to DEPs file
+  version = "2020.2";
+in
 
 stdenv.mkDerivation rec {
   pname = "spirv-tools";
-  # Update spirv-headers rev in lockstep according to DEPs file
-  version = "2019.4";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "SPIRV-Tools";
     rev = "v${version}";
-    sha256 = "17bbvhk4p42x4jlvcr5p9903xiiryw57c8yyfxmqik10s8601an9";
+    sha256 = "00b7xgyrcb2qq63pp3cnw5q1xqx2d9rfn65lai6n6r89s1vh3vg6";
   };
   enableParallelBuilding = true;
 
