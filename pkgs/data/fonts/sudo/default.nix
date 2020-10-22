e1a7e4368d96fff2c9dba57c4626d0cea7478cb7
Will Dietz <w@wdtz.org>
2020-10-22 13:08:11 -0500

    sudo-font: 0.51 -> 0.52

--- pkgs/data/fonts/sudo/default.nix
+++ pkgs/data/fonts/sudo/default.nix
@@ -1,11 +1,11 @@
 { lib, fetchzip }:
 
 let
-  version = "0.51";
+  version = "0.52";
 in fetchzip {
   name = "sudo-font-${version}";
   url = "https://github.com/jenskutilek/sudo-font/raw/v${version}/dist/sudo.zip";
-  sha256 = "12jrh1jnhsyzjk6wn84yj35ji90iw0ap22l84mb60f6nq982mzq9";
+  sha256 = "1j5p7apclyy5gfj2kklmgcncdsp5iik4gd6mdl29anzijknd0kja";
 
   postFetch = ''
     mkdir -p $out/share/fonts/
