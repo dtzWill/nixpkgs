9f4d8f908e83e4c29872e43ba0f12202ffac8963
Zhaofeng Li <hello@zhaofeng.li>
2021-08-24 18:21:40 -0700

    [WIP] vtune: init at 2022.1.0-98

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11355,6 +11355,8 @@ with pkgs;
 
   vtm = callPackage ../tools/misc/vtm { };
 
+  vtune = callPackage ../development/tools/profiling/vtune { };
+
   witness = callPackage ../tools/security/witness { };
 
   openconnectPackages = callPackage ../tools/networking/openconnect { };
