cd64ca9edd206b92a30c80229daaaf58382ca4b3
Will Dietz <w@wdtz.org>
2022-01-31 10:07:35 -0600

    agave: test slashed-zero variants

--- pkgs/data/fonts/agave/default.nix
+++ pkgs/data/fonts/agave/default.nix
@@ -10,10 +10,10 @@ let
     name = "Agave-${name}.ttf";
   };
   fonts = [
-    (mkAg "Regular" "sha256-vX1VhEgqy9rQ7hPmAgBGxKyIs2QSAYqZC/mL/2BIOrA=")
-    (mkAg "Bold" "sha256-Ax/l/RKyc03law0ThiLac/7HHV4+YxibKzcZnjZs6VI=")
-    #(mkAg "Regular-slashed" "sha256-L3Yp5xp0A22kAGzq74RgU638j8woBBtLKVV29hjzB7A=")
-    #(mkAg "Bold-slashed" "sha256-FIMxuDPlYWpA2egCIMKHtfxhcsSUqMaEVe6MacVEUSU=")
+    #(mkAg "Regular" "sha256-vX1VhEgqy9rQ7hPmAgBGxKyIs2QSAYqZC/mL/2BIOrA=")
+    #(mkAg "Bold" "sha256-Ax/l/RKyc03law0ThiLac/7HHV4+YxibKzcZnjZs6VI=")
+    (mkAg "Regular-slashed" "sha256-L3Yp5xp0A22kAGzq74RgU638j8woBBtLKVV29hjzB7A=")
+    (mkAg "Bold-slashed" "sha256-FIMxuDPlYWpA2egCIMKHtfxhcsSUqMaEVe6MacVEUSU=")
   ];
 
 in stdenv.mkDerivation {
