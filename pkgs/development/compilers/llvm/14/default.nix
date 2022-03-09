fc2ad9cc1dc01e9a5eb6539bf7e41fbd79712663
Will Dietz <w@wdtz.org>
2022-03-09 13:07:07 -0600

    llvmPackages_14.mlir: init

--- pkgs/development/compilers/llvm/14/default.nix
+++ pkgs/development/compilers/llvm/14/default.nix
@@ -169,6 +169,10 @@ let
       inherit llvm_meta;
     };
 
+    mlir = callPackage ./mlir {
+      inherit llvm_meta;
+    };
+
     # Below, is the LLVM bootstrapping logic. It handles building a
     # fully LLVM toolchain from scratch. No GCC toolchain should be
     # pulled in. As a consequence, it is very quick to build different
