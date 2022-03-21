f924715b26761385b200116ad590ccfaa537fea3
Will Dietz <w@wdtz.org>
2022-03-21 09:03:32 -0500

    llvmPackages_{9,10,11,12,13,14,git}.libcxxabi: drop wasm patches
    See #165085.

--- pkgs/development/compilers/llvm/9/libcxxabi/default.nix
+++ pkgs/development/compilers/llvm/9/libcxxabi/default.nix
@@ -18,8 +18,6 @@ stdenv.mkDerivation {
     export TRIPLE=x86_64-apple-darwin
   '' + lib.optionalString stdenv.hostPlatform.isMusl ''
     patch -p1 -d $(ls -d libcxx-*) -i ${../../libcxx-0001-musl-hacks.patch}
-  '' + lib.optionalString stdenv.hostPlatform.isWasm ''
-    patch -p1 -d $(ls -d llvm-*) -i ${./wasm.patch}
   '';
 
   patches = [
