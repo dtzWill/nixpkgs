979b3a69e46c1ef0b1e08cae62bced00dbb44e8d
Will Dietz <w@wdtz.org>
2022-03-30 13:32:04 -0500

    ccls: fix c detection?

--- pkgs/development/tools/misc/ccls/default.nix
+++ pkgs/development/tools/misc/ccls/default.nix
@@ -12,13 +12,19 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-jipSipgD0avd7XODlpxnqjHK3s6nacaxbIQIddix7X8=";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake llvmPackages.llvm.dev ];
   buildInputs = with llvmPackages; [ libclang llvm rapidjson ];
 
   cmakeFlags = [ "-DCCLS_VERSION=${version}" ];
 
   preConfigure = ''
     cmakeFlagsArray+=(-DCMAKE_CXX_FLAGS="-fvisibility=hidden -fno-rtti")
+  '';
+
+  postPatch = ''
+    substituteInPlace CMakeLists.txt \
+      --replace "project(ccls LANGUAGES CXX)" \
+                "project(ccls LANGUAGES C CXX)"
   '';
 
   clang = llvmPackages.clang;
