3983d5d79747a36670b27e69d2fe3e30871d96ba
Will Dietz <w@wdtz.org>
2022-10-04 09:28:29 -0500

    llvmPackages_git.clang: patch fixup for gold path

--- pkgs/development/compilers/llvm/git/clang/default.nix
+++ pkgs/development/compilers/llvm/git/clang/default.nix
@@ -48,7 +48,7 @@ let
       ./gnu-install-dirs.patch
       ./add-nostdlibinc-flag.patch
       (substituteAll {
-        src = ../../clang-11-12-LLVMgold-path.patch;
+        src = ../../clang-16-LLVMgold-path.patch;
         libllvmLibdir = "${libllvm.lib}/lib";
       })
     ];
