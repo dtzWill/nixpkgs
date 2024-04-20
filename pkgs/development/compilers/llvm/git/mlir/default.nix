2b848dd0fd09930323e5ce044207a8f8329094de
Will Dietz <w@wdtz.org>
2024-04-20 10:42:49 -0500

    mlir: install mlir-cpu-runner too

--- pkgs/development/compilers/llvm/git/mlir/default.nix
+++ pkgs/development/compilers/llvm/git/mlir/default.nix
@@ -16,7 +16,7 @@ let
     "linalg-ods-yaml-gen" "tblgen" # needed for cross
     "lsp-server" "opt" "pdll" "reduce" "translate" # misc utilities
     "pdll-lsp-server"
-    "query"
+    "query" "cpu-runner"
   ]) ++ [ "tblgen-lsp-server" ];
 in
 stdenv.mkDerivation rec {
